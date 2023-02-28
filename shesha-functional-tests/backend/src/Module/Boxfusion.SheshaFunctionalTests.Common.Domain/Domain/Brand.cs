e552864ce9caf57a1ae3c294e1236442a56412e6
kudzait <kudzait@boxfusion.co.za>
2023-02-28 08:31:11 +0200

    Added test entities

---
+++ shesha-functional-tests/backend/src/Module/Boxfusion.SheshaFunctionalTests.Common.Domain/Domain/Brand.cs
@@ -0,0 +1,16 @@
+﻿using Abp.Domain.Entities;
+using Shesha.Domain.Attributes;
+using System;
+
+namespace Boxfusion.SheshaFunctionalTests.Common.Domain.Domain
+{
+    [Entity(TypeShortAlias = "Boxfusion.SheshaFunctionalTests.Domain.Brand")]
+    public class Brand : Entity<Guid>
+    {
+        public virtual string Name { get; set; }
+
+        public virtual string Description { get; set; }
+
+        public virtual string WebsiteUrl { get; set; }
+    }
+}
---
+++ shesha-functional-tests/backend/src/Module/Boxfusion.SheshaFunctionalTests.Common.Domain/Migrations/M20230228082800.cs
@@ -0,0 +1,44 @@
+﻿using FluentMigrator;
+using Shesha.FluentMigrator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boxfusion.SheshaFunctionalTests.Common.Domain.Migrations
+{
+    [Migration(20230228082800)]
+    public class M20230228082800 : Migration
+    {
+        public override void Down()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Up()
+        {
+            if (!Schema.Table("SheshaFunctionalTests_Brands").Exists())
+            {
+                Create.Table("SheshaFunctionalTests_Brands")
+                    .WithIdAsGuid()
+                    .WithColumn("Name").AsString().Nullable()
+                    .WithColumn("Description").AsString().Nullable()
+                    .WithColumn("WebsiteUrl").AsString().Nullable();
+            }
+
+            if (!Schema.Table("SheshaFunctionalTests_Employees").Exists())
+            {
+                Create.Table("SheshaFunctionalTests_Employees")
+                    .WithIdAsGuid()
+                    .WithColumn("FirstName").AsString().Nullable()
+                    .WithColumn("LastName").AsString().Nullable()
+                    .WithForeignKeyColumn("CompanyId", "Core_Organisations").Nullable();
+            }
+
+
+
+
+        }
+    }
+}
