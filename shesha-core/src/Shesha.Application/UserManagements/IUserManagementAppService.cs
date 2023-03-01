eab5de48f0a69eaf41611f15359bb01e5fa9bb21
Omolemo Lethuloe <omolemo.lethuloe@shesha.io>
2023-03-01 10:03:58 +0200

    adding create user with account for user management

---
+++ shesha-core/src/Shesha.Application/UserManagements/IUserManagementAppService.cs
@@ -0,0 +1,14 @@
+﻿using Abp.Application.Services;
+using Shesha.Application.Services.Dto;
+using Shesha.Persons;
+using System;
+
+namespace Shesha.UserManagements
+{
+    /// <summary>
+    /// User Management Application Service
+    /// </summary>
+    public interface IUserManagementAppService : IAsyncCrudAppService<PersonAccountDto, Guid, FilteredPagedAndSortedResultRequestDto, CreatePersonAccountDto, PersonAccountDto>
+    {
+    }
+}
---
+++ shesha-core/src/Shesha.Application/UserManagements/UserManagementDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Abp.Application.Services.Dto;
+using Abp.AutoMapper;
+
+namespace Shesha.UserManagements
+{
+    [AutoMap(typeof(Domain.Person))]
+    public class UserManagementDto: EntityDto<Guid>
+    {
+        public virtual string UserName { get; set; }
+        public virtual string Password { get; set; }
+
+        public virtual string FirstName { get; set; }
+
+        public virtual string LastName { get; set; }
+
+        public virtual string MobileNumber { get; set; }
+
+        public virtual string EmailAddress { get; set; }
+    }
+}
