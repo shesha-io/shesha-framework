146fc2f4f79c245ef1bc64db630e85ed3566d6f4
ivan <ivan@boxfusion.co.za>
2023-03-07 12:08:40 +0300

    IShaSettingManager: removed 'GetOrNull' method notifications: added INotificationPublicationContext, is used to collect statistic of the notification publication

---
+++ shesha-core/src/Shesha.Application/Notifications/NotificationPublicationContext.cs
@@ -0,0 +1,78 @@
+﻿using Abp.Dependency;
+using Abp.Runtime;
+using Shesha.NotificationMessages.Dto;
+using System;
+using System.Collections.Generic;
+
+namespace Shesha.Notifications
+{
+    /// <summary>
+    /// Notification publication context. Is used to pass parameters and collect statistics
+    /// </summary>
+    public class NotificationPublicationContext : INotificationPublicationContext, ITransientDependency
+    {
+        private const string ScopeKey = "sha-notification-publication";
+        private readonly IAmbientScopeProvider<NotificationPublicationStatistics> _scopeProvider;
+
+        public NotificationPublicationContext(IAmbientScopeProvider<NotificationPublicationStatistics> scopeProvider)
+        {
+            _scopeProvider = scopeProvider;
+        }
+
+        /// inheritedDoc
+        public IDisposable BeginScope()
+        {
+            var state = new NotificationPublicationStatistics();
+            
+            return _scopeProvider.BeginScope(ScopeKey, state);
+        }
+
+        /// inheritedDoc
+        public NotificationPublicationStatistics Statistics
+        {
+            get
+            {
+                return _scopeProvider.GetValue(ScopeKey);
+            }
+        }
+
+        /// inheritedDoc
+        public void NotificationMessageCreated(NotificationMessageDto notificationMessageDto) 
+        {
+            var statistics = Statistics;
+            if (statistics != null)
+                statistics.NotificationMessages.Add(notificationMessageDto);
+        }
+    }
+
+    public class NotificationPublicationStatistics 
+    {
+        /// <summary>
+        /// List of created notification messages
+        /// </summary>
+        public List<NotificationMessageDto> NotificationMessages { get; private set; } = new List<NotificationMessageDto>();
+    }
+
+    /// <summary>
+    /// Notification publication context. Is used to pass parameters and collect statistics
+    /// </summary>
+    public interface INotificationPublicationContext 
+    {
+        /// <summary>
+        /// Begin new scope. All statistics will be collected as part of this scope
+        /// </summary>
+        /// <returns></returns>
+        IDisposable BeginScope();
+
+        /// <summary>
+        /// Statistics
+        /// </summary>
+        NotificationPublicationStatistics Statistics { get; }
+
+        /// <summary>
+        /// Register createtion of notification message
+        /// </summary>
+        /// <param name="notificationMessageDto"></param>
+        void NotificationMessageCreated(NotificationMessageDto notificationMessageDto);
+    }
+}
