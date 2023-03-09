954bc657da37e87a47743183957b2d4c273ac6f4
Luke <Lukeybooi@gmail.com>
2023-03-09 15:57:32 +0200

    feat(childEntityTagGroup): added deleteConfirmationTitle, deleteConfirmationBody, modalTitle, capturedProperties to models

--- shesha-reactjs/src/components/formDesigner/components/childEntitiesTagGroup/models.ts
+++ shesha-reactjs/src/components/formDesigner/components/childEntitiesTagGroup/models.ts
@@ -1,17 +1,17 @@
 import { IConfigurableFormComponent } from '../../../../providers';
 
 export interface IChildEntitiesTagGroupProps extends IConfigurableFormComponent {
+  capturedProperties?: string[];
   formId?: string;
   labelFormat?: string;
+  modalTitle?: string;
   modalWidth?: '100%' | '80%' | '60%' | '40%';
+  deleteConfirmationTitle?: string;
+  deleteConfirmationBody?: string;
 }
 
 export interface IChildEntitiesTagGroupSelectOptions {
   label?: string;
   value?: string;
   metadata?: any;
-}
-
-export interface IChildEntitiesTagGroupPayload<T = any> {
-  value: T;
 }
