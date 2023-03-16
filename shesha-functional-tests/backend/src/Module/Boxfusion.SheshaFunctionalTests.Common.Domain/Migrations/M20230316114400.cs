11f1b709c0e99f0b17356c2870237da645839acd
kudzait <kudzait@boxfusion.co.za>
2023-03-16 11:56:37 +0200

    Added extra settings for testing purposes

---
+++ shesha-functional-tests/backend/src/Module/Boxfusion.SheshaFunctionalTests.Common.Domain/Migrations/M20230316114400.cs
@@ -0,0 +1,29 @@
+﻿using FluentMigrator;
+using Shesha.FluentMigrator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boxfusion.SheshaFunctionalTests.Common.Domain.Migrations
+{
+    [Migration(20230316114400)]
+    public class M20230316114400 : AutoReversingMigration
+    {
+        public override void Up()
+        {
+
+            // Create two settings
+            this.Shesha().SettingCreate("Greeting", "Login greeting template")
+               .WithCategory("Logon") // set category, is unsed in the generic settings UI
+               .WithDescription("I am testing a useful setting") // mark setting as application specific 
+               .AsString(); // set datatype of the setting
+
+            this.Shesha().SettingCreate("IsAllowedTo", "Logged in user is allowed to be awesome")
+                .WithCategory("Logon")
+                .WithDescription("Is the logged in user allowed to be awesome?")
+                .AsBoolean();
+        }
+    }
+}
