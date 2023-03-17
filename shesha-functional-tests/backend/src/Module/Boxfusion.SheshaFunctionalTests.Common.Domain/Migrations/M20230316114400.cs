8c8e15b82eebe4f405ad28782b1d588747c0d4e2
ivan <ivan@boxfusion.co.za>
2023-03-17 09:59:29 +0300

    fixed settings migration

--- shesha-functional-tests/backend/src/Module/Boxfusion.SheshaFunctionalTests.Common.Domain/Migrations/M20230316114400.cs
+++ shesha-functional-tests/backend/src/Module/Boxfusion.SheshaFunctionalTests.Common.Domain/Migrations/M20230316114400.cs
@@ -18,11 +18,13 @@ namespace Boxfusion.SheshaFunctionalTests.Common.Domain.Migrations
             this.Shesha().SettingCreate("Greeting", "Login greeting template")
                .WithCategory("Logon") // set category, is unsed in the generic settings UI
                .WithDescription("I am testing a useful setting") // mark setting as application specific 
+               .OnModule("Shesha")
                .AsString(); // set datatype of the setting
 
             this.Shesha().SettingCreate("IsAllowedTo", "Logged in user is allowed to be awesome")
                 .WithCategory("Logon")
                 .WithDescription("Is the logged in user allowed to be awesome?")
+                .OnModule("Shesha")
                 .AsBoolean();
         }
     }
