c7223000b20740b71d727beb5234d9d16128fcaf
kudzait <kudzait@boxfusion.co.za>
2023-03-20 11:52:12 +0200

    Tested shesha settings migration helper functions

---
+++ shesha-functional-tests/backend/src/Module/Boxfusion.SheshaFunctionalTests.Common.Domain/Migrations/M20230320114500.cs
@@ -0,0 +1,24 @@
+﻿using FluentMigrator;
+using System;
+using Shesha.FluentMigrator;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boxfusion.SheshaFunctionalTests.Common.Domain.Migrations
+{
+    [Migration(20230320114500)]
+    public class M20230320114500 : AutoReversingMigration
+    {
+        public override void Up()
+        {
+            this.Shesha().SettingUpdate("Greeting")
+                        .SetValue("Hello world");
+
+            this.Shesha().SettingUpdate("IsAllowedTo")
+                        .SetValue(true.ToString());
+                 
+        }
+    }
+}
