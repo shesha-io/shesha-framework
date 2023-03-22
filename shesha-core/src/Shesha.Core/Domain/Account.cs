a4dec445fb7f7f3bf6ed4d07c50a50ec9254c6bb
Yoland Magxagxa <yoland.magxagxa@boxfusion.io>
2023-03-22 17:48:26 +0200

    added Site, Account Entities and updated the Person Entity

---
+++ shesha-core/src/Shesha.Core/Domain/Account.cs
@@ -0,0 +1,22 @@
+﻿using Abp.Domain.Entities.Auditing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shesha.Domain
+{
+    public class Account : FullAuditedEntity<Guid>
+    {
+        public virtual string Name { get; set; }
+
+        public virtual Person PrimaryContact{ get; set; }
+
+        public virtual Organisation Organisation { get; set; }
+
+        public virtual Account Parent { get; set; }
+
+        public virtual Site PrimarySite { get; set; }
+    }
+}
