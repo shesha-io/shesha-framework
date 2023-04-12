6b0f4673f1499114f27531eacb6f3742ceac3c68
Yoland Magxagxa <yoland.magxagxa@boxfusion.io>
2023-04-12 10:30:41 +0200

    updated discriminator column to have a default value

---
+++ shesha-core/src/Shesha.NHibernate/Migrations/M20230412095600.cs
@@ -0,0 +1,23 @@
+﻿using FluentMigrator;
+using Shesha.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shesha.Migrations
+{
+    [Migration(20230412095600)]
+    public class M20230412095600 : Migration
+    {
+        public override void Up()
+        {
+            Alter.Column("Frwk_Discriminator").OnTable("Core_Accounts").AsString(SheshaDatabaseConsts.DiscriminatorMaxSize).NotNullable().WithDefaultValue("");
+        }
+
+        public override void Down()
+        {
+        }
+    }
+}
