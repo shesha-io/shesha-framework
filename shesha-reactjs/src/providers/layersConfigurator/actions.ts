3009372c365f71e4f80ea6dd2a563b0083a01fe0
Luke <Lukeybooi@gmail.com>
2023-05-14 00:34:45 +0200

    refactor: formatted code

--- shesha-reactjs/src/providers/layersConfigurator/actions.ts
+++ shesha-reactjs/src/providers/layersConfigurator/actions.ts
@@ -2,26 +2,25 @@ import { createAction } from 'redux-actions';
 import { IUpdateChildItemsPayload, IUpdateItemSettingsPayload } from './contexts';
 
 export enum LayerGroupActionEnums {
-    AddLayer = 'ADD_LAYER',
-    DeleteLayer = 'DELETE_LAYER',
-    UpdateItem = 'UPDATE_ITEM',
-    SelectItem = 'SELECT_ITEM',
-    UpdateChildItems = 'UPDATE_CHILD_ITEMS',
+  AddLayer = 'ADD_LAYER',
+  DeleteLayer = 'DELETE_LAYER',
+  UpdateItem = 'UPDATE_ITEM',
+  SelectItem = 'SELECT_ITEM',
+  UpdateChildItems = 'UPDATE_CHILD_ITEMS',
 }
 
 export const addLayerAction = createAction(LayerGroupActionEnums.AddLayer);
 
-export const deleteLayerAction = createAction<string, string>(LayerGroupActionEnums.DeleteLayer, p => p);
+export const deleteLayerAction = createAction<string, string>(LayerGroupActionEnums.DeleteLayer, (p) => p);
 
-export const selectItemAction = createAction<string, string>(LayerGroupActionEnums.SelectItem, p => p);
+export const selectItemAction = createAction<string, string>(LayerGroupActionEnums.SelectItem, (p) => p);
 
 export const updateItemAction = createAction<IUpdateItemSettingsPayload, IUpdateItemSettingsPayload>(
-    LayerGroupActionEnums.UpdateItem,
-    p => p
+  LayerGroupActionEnums.UpdateItem,
+  (p) => p
 );
 
 export const updateChildItemsAction = createAction<IUpdateChildItemsPayload, IUpdateChildItemsPayload>(
-    LayerGroupActionEnums.UpdateChildItems,
-    p => p
+  LayerGroupActionEnums.UpdateChildItems,
+  (p) => p
 );
-
--- shesha-reactjs/src/providers/layersConfigurator/utils.ts
+++ shesha-reactjs/src/providers/layersConfigurator/utils.ts
@@ -1,32 +1,31 @@
-import { ILayerGroup, LayerGroupItemProps } from "./models";
+import { ILayerGroup, LayerGroupItemProps } from './models';
 
 export interface IItemPosition {
-    ownerArray: LayerGroupItemProps[];
-    index: number;
+  ownerArray: LayerGroupItemProps[];
+  index: number;
 }
 
 export const getItemById = (items: LayerGroupItemProps[], id: string): LayerGroupItemProps => {
-    const position = getItemPositionById(items, id);
-    return position ? position.ownerArray[position.index] : null;
+  const position = getItemPositionById(items, id);
+  return position ? position.ownerArray[position.index] : null;
 };
 
-
 export const getItemPositionById = (items: LayerGroupItemProps[], id: string): IItemPosition => {
-    for (let index = 0; index < items.length; index++) {
-        const item = items[index];
-        if (item.id === id)
-            return {
-                ownerArray: items,
-                index,
-            };
+  for (let index = 0; index < items.length; index++) {
+    const item = items[index];
+    if (item.id === id)
+      return {
+        ownerArray: items,
+        index,
+      };
 
-        const children = (item as ILayerGroup)?.childItems;
+    const children = (item as ILayerGroup)?.childItems;
 
-        if (children) {
-            const itemPosition = getItemPositionById(children, id);
-            if (itemPosition) return itemPosition;
-        }
+    if (children) {
+      const itemPosition = getItemPositionById(children, id);
+      if (itemPosition) return itemPosition;
     }
+  }
 
-    return null;
+  return null;
 };
