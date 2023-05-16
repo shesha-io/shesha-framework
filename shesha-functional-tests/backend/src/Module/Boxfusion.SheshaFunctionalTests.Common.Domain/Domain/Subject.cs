1d077bc5322f168209f2e1c56897f5ba5cdc7f63
Omolemo Lethuloe <omolemo.lethuloe@shesha.io>
2023-05-16 14:03:48 +0200

    Adding entities and app services to test data table and map component

---
+++ shesha-functional-tests/backend/src/Module/Boxfusion.SheshaFunctionalTests.Common.Domain/Domain/Subject.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Abp.Domain.Entities.Auditing;
+using Intent.RoslynWeaver.Attributes;
+using Shesha.Domain.Attributes;
+
+namespace Boxfusion.SheshaFunctionalTests.Common.Domain.Domain
+{
+    /// <summary>
+    /// 
+    /// </summary>
+
+    [Discriminator]
+    public class Subject : FullAuditedEntity<Guid>
+    {
+        /// <summary> 
+        /// 
+        /// </summary>
+        public virtual string Name { get; set; }
+
+        /// <summary> 
+        /// 
+        /// </summary>
+        public virtual string Description { get; set; }
+
+        /// <summary> 
+        /// 
+        /// </summary>
+        public virtual decimal? Total { get; set; }
+
+        /// <summary> 
+        /// 
+        /// </summary>
+        public virtual Book Book { get; set; }
+
+        /// <summary> 
+        /// 
+        /// </summary>
+        public virtual School School { get; set; }
+    }
+}
