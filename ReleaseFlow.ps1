bfb139b2b1f8f2b563181efab5dd353a66463889
box-pulumi-templates <104182287+box-jsmoolman@users.noreply.github.com>
2023-05-22 13:34:50 +0200

    Update ReleaseFlow.ps1

--- ReleaseFlow.ps1
+++ ReleaseFlow.ps1
@@ -43,7 +43,7 @@ elseif ("$PipeSourceBranch" -like "*/releases/*"){
     $version = Split-Path $path -Leaf
     Write-Host $version
 
-    write-host "##vso[task.setvariable variable=versionNo]$version.$build"
+    write-host "##vso[task.setvariable variable=versionNo]$version.$PipeBuildNumber"
 }
 elseif ("$PipeSourceBranch" -like "*/tags/release-*"){
     write-host "Source Branch should be tags here: $PipeSourceBranch"
