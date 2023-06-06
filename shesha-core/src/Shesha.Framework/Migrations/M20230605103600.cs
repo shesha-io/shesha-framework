811ca81a3374b35d1bfc30f610f0e2d5903a1338
holeng-boxfusion-io <135592163+holeng-boxfusion-io@users.noreply.github.com>
2023-06-06 12:15:12 +0200

    Added SenderText, Direction and CCs fields to notification message table

---
+++ shesha-core/src/Shesha.Framework/Migrations/M20230605103600.cs
@@ -0,0 +1,16 @@
+﻿using FluentMigrator;
+using Shesha.FluentMigrator;
+
+namespace Shesha.Migrations
+{
+    [Migration(20230605103600)]
+    public class M20230605103600 : OneWayMigration
+    {
+        public override void Up()
+        {
+            Alter.Table("Core_NotificationMessages").AddColumn("SenderText").AsString().Nullable();
+            Alter.Table("Core_NotificationMessages").AddColumn("DirectionLkp").AsInt64().Nullable();
+            Alter.Table("Core_NotificationMessages").AddColumn("Cc").AsString().Nullable();
+        }
+    }
+}
