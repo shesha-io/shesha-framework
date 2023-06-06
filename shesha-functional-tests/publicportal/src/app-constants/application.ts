1315574e4aa501d835ffd3171e915284956b7c85
Luke <Lukeybooi@gmail.com>
2023-06-06 22:22:08 +0200

    refactor: changed template logo

--- shesha-functional-tests/publicportal/src/app-constants/application.ts
+++ shesha-functional-tests/publicportal/src/app-constants/application.ts
@@ -1,4 +1,4 @@
 export const LOGO = {
-  src: "https://www.gauteng.gov.za/images/GPGLogo_3x_op.png",
+  src: "https://uploads-ssl.webflow.com/61423df7fefcfd4536b14230/62b0726b03457b202b625d0d_Boxfusion_logo.png",
   width: 200,
 };
