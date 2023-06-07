68c2ba9f34b0e0cd27f70cbd6264271cd90b123e
holeng-boxfusion-io <135592163+holeng-boxfusion-io@users.noreply.github.com>
2023-06-07 11:53:26 +0200

    Added incoming emails settings

--- shesha-core/src/Shesha.Framework/Migrations/M20230605103600.cs
+++ shesha-core/src/Shesha.Framework/Migrations/M20230605103600.cs
@@ -11,6 +11,13 @@ namespace Shesha.Migrations
             Alter.Table("Core_NotificationMessages").AddColumn("SenderText").AsString().Nullable();
             Alter.Table("Core_NotificationMessages").AddColumn("DirectionLkp").AsInt64().Nullable();
             Alter.Table("Core_NotificationMessages").AddColumn("Cc").AsString().Nullable();
+
+            Alter.Table("Core_NotificationMessages").AddColumn(DatabaseConsts.ExtSysFirstSyncDate).AsDateTime().Nullable();
+            Alter.Table("Core_NotificationMessages").AddColumn(DatabaseConsts.ExtSysId).AsString(50).Nullable();
+            Alter.Table("Core_NotificationMessages").AddColumn(DatabaseConsts.ExtSysLastSyncDate).AsDateTime().Nullable();
+            Alter.Table("Core_NotificationMessages").AddColumn(DatabaseConsts.ExtSysSource).AsString(50).Nullable();
+            Alter.Table("Core_NotificationMessages").AddColumn(DatabaseConsts.ExtSysSyncError).AsStringMax().Nullable();
+            Alter.Table("Core_NotificationMessages").AddColumn(DatabaseConsts.ExtSysSyncStatusLkp).AsInt32().Nullable();
         }
     }
 }
