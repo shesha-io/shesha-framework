43f33a93cda62684dfc305f5932d3f7f7e7c099d
Jonathan Mavunda <jonathan.mavunda@boxfusion.io>
2023-06-29 20:33:08 +0200

    made changes on Core_ShaRoleAppointments added new colunms and reflist

---
+++ shesha-core/src/Shesha.Core/Domain/Enums/RefListRoleAppointmentStatus.cs
@@ -0,0 +1,12 @@
+﻿using Shesha.Domain.Attributes;
+
+namespace Shesha.Domain.Enums
+{
+    [ReferenceList("Shesha.Core", "RoleAppointmentStatus")]
+    public enum RefListRoleAppointmentStatus
+    {
+        ViewOnly = 1,
+        Submitter = 2,
+        Admin = 3
+    }
+}
--- shesha-core/src/Shesha.Core/Domain/ShaRoleAppointment.cs
+++ shesha-core/src/Shesha.Core/Domain/ShaRoleAppointment.cs
@@ -2,6 +2,7 @@
 using Abp.Domain.Entities;
 using Abp.Domain.Entities.Auditing;
 using Shesha.Domain.Attributes;
+using Shesha.EntityReferences;
 
 namespace Shesha.Domain
 {
@@ -11,5 +12,17 @@ namespace Shesha.Domain
     {
         public virtual ShaRole Role { get; set; }
         public virtual int? TenantId { get; set; }
+
+        public GenericEntityReference PermissionedEntity1 { get; set; }
+        public GenericEntityReference PermissionedEntity2 { get; set; }
+        public GenericEntityReference PermissionedEntity3 { get; set; }
+
+        public virtual DateTime? FromDate { get; set; }
+        public virtual DateTime? ToDate   { get; set; }
+
+        [ReferenceList("Shesha.Core", "RoleAppointmentStatus")]
+        public virtual int? Status { get; set; }
+        
+            
     }
 }
