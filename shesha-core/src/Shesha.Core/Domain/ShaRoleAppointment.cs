0e049afca0c61bee65b10c2ae3f7a2b8fe1467e2
Jonathan Mavunda <jonathan.mavunda@boxfusion.io>
2023-06-29 20:33:52 +0200

    Revert "made changes on Core_ShaRoleAppointments added new colunms and reflist"
    This reverts commit 43f33a93cda62684dfc305f5932d3f7f7e7c099d.

--- shesha-core/src/Shesha.Core/Domain/ShaRoleAppointment.cs
+++ shesha-core/src/Shesha.Core/Domain/ShaRoleAppointment.cs
@@ -2,7 +2,6 @@
 using Abp.Domain.Entities;
 using Abp.Domain.Entities.Auditing;
 using Shesha.Domain.Attributes;
-using Shesha.EntityReferences;
 
 namespace Shesha.Domain
 {
@@ -12,17 +11,5 @@ namespace Shesha.Domain
     {
         public virtual ShaRole Role { get; set; }
         public virtual int? TenantId { get; set; }
-
-        public GenericEntityReference PermissionedEntity1 { get; set; }
-        public GenericEntityReference PermissionedEntity2 { get; set; }
-        public GenericEntityReference PermissionedEntity3 { get; set; }
-
-        public virtual DateTime? FromDate { get; set; }
-        public virtual DateTime? ToDate   { get; set; }
-
-        [ReferenceList("Shesha.Core", "RoleAppointmentStatus")]
-        public virtual int? Status { get; set; }
-        
-            
     }
 }
