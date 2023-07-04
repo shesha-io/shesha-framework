5b246601700b632d7ab7ae1dc3c9d53228cb88ee
admin-shesha <122363505+admin-shesha@users.noreply.github.com>
2023-07-04 11:43:54 +0200

    Update ReleaseFlow.ps1

--- ReleaseFlow.ps1
+++ ReleaseFlow.ps1
@@ -18,9 +18,9 @@ $major = $parts[0].Substring(0,4)
 $minor = $parts[0].Substring(4,2)
 $patch = $parts[0].Substring(6,2)
 $build = $parts[1]
-$mainVersion = "0.0.$patchNumber-build"
+$mainVersion = "0.0.$patchNumber"
 
-Write-Host "Version number for main: 0.0.$patchNumber-build"
+Write-Host "Version number for main: 0.0.$patchNumber"
 
 write-host "The build number formatted is: $patchNumber"
 
