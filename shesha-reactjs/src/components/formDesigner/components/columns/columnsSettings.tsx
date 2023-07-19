ab9b77a9c1c9b5d4f94ecc082fcaeb31aa851109
Phil <phillymza@gmail.com>
2023-07-20 01:53:14 +0200

    wip(styles): Added style and customVisibility properties to the columns component

--- shesha-reactjs/src/components/formDesigner/components/columns/columnsSettings.tsx
+++ shesha-reactjs/src/components/formDesigner/components/columns/columnsSettings.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { Form, Input, InputNumber } from 'antd';
 import { IColumnsComponentProps } from './interfaces';
 import ColumnsList from './columnsList';
+import { EXPOSED_VARIABLES } from './exposedVariables';
+import CodeEditor from '../codeEditor/codeEditor';
+import SectionSeparator from 'components/sectionSeparator';
 
 export interface IProps {
   readOnly: boolean;
@@ -17,23 +20,55 @@ function ColumnsSettings({ readOnly, onSave, model, onValuesChange }: IProps) {
   return (
     <Form form={form} onFinish={onSave} layout="vertical" onValuesChange={onValuesChange}>
       <Form.Item name="name" label="Name" rules={[{ required: true }]} initialValue={model?.name}>
-        <Input readOnly={readOnly}/>
+        <Input readOnly={readOnly} />
       </Form.Item>
 
       <Form.Item name="gutterX" label="Gutter X" initialValue={model?.gutterX}>
-        <InputNumber min={1} max={48} step={4} readOnly={readOnly}/>
+        <InputNumber min={1} max={48} step={4} readOnly={readOnly} />
       </Form.Item>
 
       <Form.Item name="gutterY" label="Gutter Y" initialValue={model?.gutterX}>
-        <InputNumber min={1} max={48} step={4} readOnly={readOnly}/>
+        <InputNumber min={1} max={48} step={4} readOnly={readOnly} />
       </Form.Item>
 
       <Form.Item name="label" label="Label" initialValue={model?.label}>
-        <Input readOnly={readOnly}/>
+        <Input readOnly={readOnly} />
       </Form.Item>
 
       <Form.Item name="columns" label="Columns" initialValue={model?.columns || []}>
-        <ColumnsList readOnly={readOnly}/>
+        <ColumnsList readOnly={readOnly} />
+      </Form.Item>
+
+      <SectionSeparator title="Style" />
+
+      <Form.Item name="style" label="Style" initialValue={model?.style}>
+        <CodeEditor
+          id={''}
+          name="style"
+          readOnly={readOnly}
+          mode="dialog"
+          label="Style"
+          setOptions={{ minLines: 20, maxLines: 500, fixedWidthGutter: true }}
+          type={''}
+          description="A script that returns the style of the element as an object. This should conform to CSSProperties"
+          exposedVariables={EXPOSED_VARIABLES}
+        />
+      </Form.Item>
+
+      <SectionSeparator title="Visibility" />
+
+      <Form.Item name="customVisibility" label="Custom Visibility" initialValue={model?.customVisibility}>
+        <CodeEditor
+          id={''}
+          name="customVisibility"
+          readOnly={readOnly}
+          mode="dialog"
+          label="Custom Visibility"
+          setOptions={{ minLines: 20, maxLines: 500, fixedWidthGutter: true }}
+          type={''}
+          description="Enter custom visibility code.  You must return true to show the component. The global variable data is provided, and allows you to access the data of any form component, by using its API key."
+          exposedVariables={EXPOSED_VARIABLES}
+        />
       </Form.Item>
     </Form>
   );
--- shesha-reactjs/src/components/formDesigner/components/columns/interfaces.ts
+++ shesha-reactjs/src/components/formDesigner/components/columns/interfaces.ts
@@ -1,16 +1,18 @@
 import { IConfigurableFormComponent } from '../../../../providers/form/models';
 
 export interface IColumnProps {
-    id: string;
-    flex: number;
-    offset: number;
-    push: number;
-    pull: number;
-    components: IConfigurableFormComponent[];
+  id: string;
+  flex: number;
+  offset: number;
+  push: number;
+  pull: number;
+  components: IConfigurableFormComponent[];
 }
 
 export interface IColumnsComponentProps extends IConfigurableFormComponent {
-    columns: IColumnProps[];
-    gutterX?: number;
-    gutterY?: number;
+  columns: IColumnProps[];
+  gutterX?: number;
+  gutterY?: number;
+  style?: string;
+  customVisibility?: string;
 }
