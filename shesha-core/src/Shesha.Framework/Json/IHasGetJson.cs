64ef4007b8b6cdcac4bc1f51bd5f344ff619e457
ivan <ivan@boxfusion.co.za>
2023-08-01 16:25:14 +0300

    feat: implemented support of spatial types ion the GraphQL and dynamic APIs

---
+++ shesha-core/src/Shesha.Framework/Json/IHasGetJson.cs
@@ -0,0 +1,14 @@
+﻿namespace Shesha.Json
+{
+    /// <summary>
+    /// Represents an object that uses it's own logic of Json conversion
+    /// </summary>
+    public interface IHasGetJson
+    {
+        /// <summary>
+        /// Convert object to Json representation
+        /// </summary>
+        /// <returns></returns>
+        string GetJson();
+    }
+}
---
+++ shesha-core/src/Shesha.Framework/Json/JsonStringConverter.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace Shesha.Json
+{
+    public class JsonStringConverter : JsonConverter
+    {
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            var jsonContent = (value as JsonString)?.GetJson();
+
+            if (!string.IsNullOrWhiteSpace(jsonContent))
+                writer.WriteRawValue(jsonContent);
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType.IsAssignableTo(typeof(JsonString));
+        }
+
+        public override bool CanRead => false;
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
