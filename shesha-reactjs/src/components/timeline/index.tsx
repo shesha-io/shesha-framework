de4cd75b8808640e94da58dbcdb137ba0543ba04
Pholosho Seloane <35956639+pholoshos@users.noreply.github.com>
2023-09-06 15:15:17 +0200

    id clean up

--- shesha-reactjs/src/components/timeline/index.tsx
+++ shesha-reactjs/src/components/timeline/index.tsx
@@ -16,7 +16,7 @@ export const ShaTimeline: FC<ITimelineProps> = ({ properties, ownerId, entityTyp
   //timeline icons
   const { globalState } = useGlobalState();
   const getAllProps =
-    apiSource === 'custom' ? { path: customApiUrl + `?id=${ownerId + 'sds'}` || '', lazy: true } : { lazy: true };
+    apiSource === 'custom' ? { path: customApiUrl + `?id=${ownerId}` || '', lazy: true } : { lazy: true };
   const { refetch: fetchAllEntities, loading: isFetchingEntities, data } = useGetAll(getAllProps as any);
 
   const fetchEntities = (params: object) => {
