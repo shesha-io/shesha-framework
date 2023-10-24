42c026f5fec90934d4b678669f409d56806a8c5c
IvanIlyichev <ivan.Ilyichev@shesha.io>
2023-10-24 12:15:22 +0300

    -feat: implemented generic entity reorderer by OrderIndex (User Story 45707)

---
+++ shesha-core/src/Shesha.Framework/Utilities/TypeBuilderHelper.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace Shesha.Utilities
+{
+    /// <summary>
+    /// TypeBuilder helper
+    /// </summary>
+    public static class TypeBuilderHelper
+    {
+        public static FieldBuilder CreateAutoProperty(TypeBuilder tb, string propertyName, Type propertyType)
+        {
+            var fieldBuilder = tb.DefineField("_" + propertyName, propertyType, FieldAttributes.Private);
+
+            var propertyBuilder = tb.DefineProperty(propertyName, PropertyAttributes.HasDefault, propertyType, null);
+            var getPropMthdBldr = tb.DefineMethod("get_" + propertyName,
+                MethodAttributes.Public |
+                MethodAttributes.SpecialName |
+                MethodAttributes.HideBySig |
+                MethodAttributes.Virtual,
+                propertyType,
+                Type.EmptyTypes);
+            ILGenerator getIl = getPropMthdBldr.GetILGenerator();
+
+            getIl.Emit(OpCodes.Ldarg_0);
+            getIl.Emit(OpCodes.Ldfld, fieldBuilder);
+            getIl.Emit(OpCodes.Ret);
+
+            var setPropMthdBldr =
+                tb.DefineMethod("set_" + propertyName,
+                  MethodAttributes.Public |
+                  MethodAttributes.SpecialName |
+                  MethodAttributes.HideBySig |
+                  MethodAttributes.Virtual,
+                  null, new[] { propertyType });
+
+            ILGenerator setIl = setPropMthdBldr.GetILGenerator();
+            var modifyProperty = setIl.DefineLabel();
+            var exitSet = setIl.DefineLabel();
+
+            setIl.MarkLabel(modifyProperty);
+            setIl.Emit(OpCodes.Ldarg_0);
+            setIl.Emit(OpCodes.Ldarg_1);
+            setIl.Emit(OpCodes.Stfld, fieldBuilder);
+
+            setIl.Emit(OpCodes.Nop);
+            setIl.MarkLabel(exitSet);
+            setIl.Emit(OpCodes.Ret);
+
+            propertyBuilder.SetGetMethod(getPropMthdBldr);
+            propertyBuilder.SetSetMethod(setPropMthdBldr);
+
+            return fieldBuilder;
+        }
+    }
+}
