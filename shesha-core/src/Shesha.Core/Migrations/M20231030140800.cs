83c32e99cc91ffd1e0d70b6c77543be027e60d35
Ernest Nyumbu <ernest.nyumbu@boxfusion.io>
2023-10-30 18:49:31 +0200

    feat (Person) : Added TargetingFlag property

---
+++ shesha-core/src/Shesha.Core/Migrations/M20231030140800.cs
@@ -0,0 +1,18 @@
+﻿using FluentMigrator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shesha.Migrations
+{
+    [Migration(20231030140800), MsSqlOnly]
+    public class M20231030140800 : AutoReversingMigration
+    {
+        public override void Up()
+        {
+            Alter.Table("Core_Persons").AddColumn("TargetingFlag").AsInt64().Nullable();
+        }
+    }
+}
