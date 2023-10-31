5a8f5ec3282080cc3f8fef1cb619ed6ee66e9e32
MachoeneMoichela <Machoene.Moichela@shesha.io>
2023-10-31 16:04:35 +0200

    Added discriminator column on mobile device

---
+++ shesha-core/src/Shesha.Framework/Migrations/M20231031153500.cs
@@ -0,0 +1,15 @@
+﻿using FluentMigrator;
+using Shesha.FluentMigrator;
+
+namespace Shesha.Migrations
+{
+    [Migration(20231031153500)]
+    public class M20231031153500 : OneWayMigration
+    {
+        public override void Up()
+        {
+            Alter.Table("Frwk_MobileDevices").AddColumn("Frwk_Discriminator").AsString().WithDefaultValue("");
+
+        }
+    }
+}
