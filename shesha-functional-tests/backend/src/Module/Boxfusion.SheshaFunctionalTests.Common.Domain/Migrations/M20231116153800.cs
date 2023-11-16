b088956e9c16f4ab91bb340600faa3f9a4c8db21
Omolemo Lethuloe <omolemo.lethuloe@shesha.io>
2023-11-16 15:40:04 +0200

    chore: adding timespan property on school and book

---
+++ shesha-functional-tests/backend/src/Module/Boxfusion.SheshaFunctionalTests.Common.Domain/Migrations/M20231116153800.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using FluentMigrator;
+using Shesha.Domain;
+using Shesha.FluentMigrator;
+
+namespace Boxfusion.SheshaFunctionalTests.Common.Domain.Migrations
+{
+    [Migration(20231116153800), MsSqlOnly]
+    public class M20231116153800 : Migration
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public override void Up()
+        {
+            Alter.Table("SheshaFunctionalTests_Books").AddColumn("TimeTicks").AsInt64().Nullable();
+            Alter.Table("SheshaFunctionalTests_Schools").AddColumn("TimeTicks").AsInt64().Nullable();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public override void Down()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
