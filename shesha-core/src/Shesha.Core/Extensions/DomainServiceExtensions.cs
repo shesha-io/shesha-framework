30b33fe9ea93f4e70c2d60f1698ccc90db23e22f
Yoland Magxagxa <yoland.magxagxa@boxfusion.io>
2023-11-20 16:17:01 +0200

    Added DomainServiceExtensions on Domain Layer Extensions

---
+++ shesha-core/src/Shesha.Core/Extensions/DomainServiceExtensions.cs
@@ -0,0 +1,62 @@
+﻿using Abp.Dependency;
+using Abp.Domain.Entities;
+using Abp.Domain.Repositories;
+using Abp.Domain.Services;
+using Abp.Runtime.Session;
+using Abp.UI;
+using Shesha.Services;
+using System;
+using System.Threading.Tasks;
+
+namespace Shesha.Domain
+{
+    public static class DomainServiceExtensions
+    {
+
+        public static async Task<Person> GetCurrentPerson(this DomainService service)
+        {
+            var personRepository = IocManager.Instance.Resolve<IRepository<Person, Guid>>();
+            var _session = StaticContext.IocManager.Resolve<IAbpSession>();
+            var person = await personRepository.FirstOrDefaultAsync(p => p.User.Id == _session.GetUserId());
+            return person;
+        }
+
+        /// <summary>
+        /// Returns entity of the specified type with the specified <paramref name="id"/>
+        /// </summary>
+        /// <typeparam name="T">Type of entity</typeparam>
+        /// <param name="service"></param>
+        /// <param name="id">Id of the entity</param>
+        /// <param name="throwException">Throw exception if entity not found</param>
+        /// <returns></returns>
+        public static async Task<T> GetEntityAsync<T>(this DomainService service, Guid id, bool throwException = true) where T : class, IEntity<Guid>
+        {
+            return await GetEntityAsync<T, Guid>(service, id, throwException);
+        }
+
+        /// <summary>
+        /// Returns entity of the specified type with the specified <paramref name="id"/>
+        /// </summary>
+        /// <typeparam name="T">Type of entity</typeparam>
+        /// <typeparam name="TId">Id type</typeparam>
+        /// <param name="service"></param>
+        /// <param name="id">Id of the entity</param>
+        /// <param name="throwException">Throw exception if entity not found</param>
+        /// <returns></returns>
+        public static async Task<T> GetEntityAsync<T, TId>(this DomainService service, TId id, bool throwException = true) where T : class, IEntity<TId>
+        {
+            var dynamicRepo = StaticContext.IocManager.Resolve<IDynamicRepository>();
+
+            var item = await dynamicRepo.GetAsync(typeof(T), id.ToString());
+
+            if (item != null)
+                return (T)item;
+
+            if (throwException)
+                throw new UserFriendlyException($"{typeof(T).Name} with the specified id `{id}` not found");
+
+            return null;
+        }
+
+    }
+}
