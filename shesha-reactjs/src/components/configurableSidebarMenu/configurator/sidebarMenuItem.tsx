e45ca8f0d8517f6b40f3fbe47b8c03a5ef2b489a
IvanIlyichev <ivan.Ilyichev@shesha.io>
2023-12-13 17:05:37 +0300

    refactor: reviewed divider in the sidebar menu

--- shesha-reactjs/src/components/configurableSidebarMenu/configurator/sidebarMenuItem.tsx
+++ shesha-reactjs/src/components/configurableSidebarMenu/configurator/sidebarMenuItem.tsx
@@ -1,11 +1,13 @@
 import React, { FC } from 'react';
-import { Button, Tooltip } from 'antd';
+import { Button, Tooltip, Typography } from 'antd';
 import { DeleteFilled, QuestionCircleOutlined } from '@ant-design/icons';
 import { useSidebarMenuConfigurator } from '@/providers/sidebarMenuConfigurator';
 import DragHandle from './dragHandle';
 import ShaIcon, { IconType } from '@/components/shaIcon';
 import classNames from 'classnames';
 import { ISidebarMenuItem } from '@/interfaces/sidebar';
+
+const { Text } = Typography;
 
 export interface IProps extends ISidebarMenuItem {
   index: number[];
@@ -32,14 +34,19 @@ export const SidebarMenuItem: FC<IProps> = props => {
     <div className={classNames('sha-sidebar-item', { selected: selectedItemId === props.id })}>
       <div className="sha-sidebar-item-header">
         <DragHandle id={props.id} />
-        {renderedIcon}
-        <span className="sha-sidebar-item-name">{props.title}</span>
-
-        {props.tooltip && (
-          <Tooltip title={props.tooltip}>
-            <QuestionCircleOutlined className="sha-help-icon" />
-          </Tooltip>
+        {props.itemType === 'button' && (
+          <>
+            {renderedIcon}
+            <span className="sha-sidebar-item-name">{props.title}</span>
+            {props.tooltip && (
+              <Tooltip title={props.tooltip}>
+                <QuestionCircleOutlined className="sha-help-icon" />
+              </Tooltip>
+            )}
+          </>
         )}
+        {props.itemType === 'divider' && (<Text type="secondary">— divider —</Text>)}
+
         <div className="sha-sidebar-item-controls">
           <Button icon={<DeleteFilled color="red" />} onClick={onDeleteClick} size="small" danger />
         </div>
