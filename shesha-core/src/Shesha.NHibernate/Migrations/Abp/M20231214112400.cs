ca1e9f3251acaa281255f2263053421b85ecf5cc
Maseru Kganyago <maseru.kganyago@boxfusion.io>
2023-12-14 11:28:42 +0200

    Added new migration of M20231204141400 copy, and reverted apsettings configs

---
+++ shesha-core/src/Shesha.NHibernate/Migrations/Abp/M20231214112400.cs
@@ -0,0 +1,15 @@
+﻿using FluentMigrator;
+using Shesha.FluentMigrator;
+
+namespace Shesha.Migrations.Abp
+{
+    [Migration(20231214112400)]
+    public class M20231214112400 : OneWayMigration
+    {
+        public override void Up()
+        {
+            Create.Column("TargetNotifiers").OnTable("AbpUserNotifications").AsString(1024).Nullable();
+            Create.Column("TargetNotifiers").OnTable("AbpNotifications").AsString(1024).Nullable();
+        }
+    }
+}
