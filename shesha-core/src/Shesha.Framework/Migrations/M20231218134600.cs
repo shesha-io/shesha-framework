df60d434dc3490bd25e1f2c166193b179d0276d6
Omolemo Lethuloe <omolemo.lethuloe@shesha.io>
2023-12-18 14:03:59 +0200

    Added entity change audigt log view to allow Entity data changes Audit trail to viewed by an admin. Github issue: https://github.com/shesha-io/shesha-framework/issues/768

---
+++ shesha-core/src/Shesha.Framework/Migrations/M20231218134600.cs
@@ -0,0 +1,48 @@
+﻿using FluentMigrator;
+using Shesha.FluentMigrator;
+using System;
+
+namespace Shesha.Migrations
+{
+    [Migration(20231218134600)]
+    public class M20231218134600 : Migration
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public override void Up()
+        {
+            Execute.Sql(@"
+		            CREATE OR ALTER VIEW [dbo].[vw_Frwk_EntityChangeAuditLogs]
+			            AS
+                        SELECT 
+                          ec.EntityId as Id,
+                          epc.PropertyName,
+                          epc.OriginalValue,
+                          epc.NewValue,
+                          ec.EntityTypeFullName,
+                          ecs.CreationTime,
+                          Concat(abpU.Name,' ',abpU.Surname)  as ActionedBy,
+                          CASE
+                              WHEN ChangeType = 0 THEN 'Created'
+                              WHEN ChangeType = 1 THEN 'Updated'
+                              WHEN ChangeType = 2 THEN 'Deleted'
+                              ELSE 'Other'
+                          END AS ChangeType
+                        FROM [AbpEntityChanges] ec
+                        LEFT JOIN [AbpEntityPropertyChanges] epc on epc.EntityChangeId = ec.Id
+                        LEFT JOIN [AbpEntityChangeSets] ecs on ecs.Id = ec.EntityChangeSetId
+                        INNER JOIN [AbpUsers] abpU on abpU.Id = ecs.UserId
+                    GO
+            ");
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public override void Down()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
