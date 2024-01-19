97adbf07fbb7364eaa64e0cc92b6fe2478c79102
nthangeniphumudzo <60381173+nthangeniphumudzo@users.noreply.github.com>
2024-01-19 11:40:49 +0200

    testing

--- shesha-reactjs/src/components/dataTable/index.tsx
+++ shesha-reactjs/src/components/dataTable/index.tsx
@@ -19,27 +19,11 @@ import { DataTableFullInstance, IColumnWidth } from '@/providers/dataTable/conte
 import { removeUndefinedProperties } from '@/utils/array';
 import { camelcaseDotNotation, toCamelCase } from '@/utils/string';
 import { ReactTable } from '@/components/reactTable';
-import {
-  IColumnResizing,
-  IReactTableProps,
-  OnRowsRendering,
-  OnRowsReorderedArgs,
-  RowDataInitializer,
-  RowRenderer,
-} from '../reactTable/interfaces';
+import { IColumnResizing, IReactTableProps, OnRowsRendering, OnRowsReorderedArgs, RowDataInitializer, RowRenderer } from '../reactTable/interfaces';
 import { getCellRenderer } from './cell';
-import {
-  BackendRepositoryType,
-  ICreateOptions,
-  IDeleteOptions,
-  IUpdateOptions,
-} from '@/providers/dataTable/repository/backendRepository';
+import { BackendRepositoryType, ICreateOptions, IDeleteOptions, IUpdateOptions } from '@/providers/dataTable/repository/backendRepository';
 import { isDataColumn, ITableDataColumn } from '@/providers/dataTable/interfaces';
-import {
-  IColumnEditorProps,
-  IFieldComponentProps,
-  standardCellComponentTypes,
-} from '@/providers/datatableColumnsConfigurator/models';
+import { IColumnEditorProps, IFieldComponentProps, standardCellComponentTypes } from '@/providers/datatableColumnsConfigurator/models';
 import { useFormDesignerComponents } from '@/providers/form/hooks';
 import { executeScriptSync } from '@/providers/form/utils';
 import moment from 'moment';
@@ -47,11 +31,10 @@ import { axiosHttp } from '@/utils/fetchers';
 import { IAnyObject } from '@/interfaces';
 import { DataTableColumn, IShaDataTableProps, OnSaveHandler, OnSaveSuccessHandler, YesNoInheritJs } from './interfaces';
 import { ValueRenderer } from '../valueRenderer/index';
-import { isEqual } from 'lodash';
+import { isEqual } from "lodash";
 import { Collapse, Typography } from 'antd';
 import { RowsReorderPayload } from '@/providers/dataTable/repository/interfaces';
 import { adjustWidth, getCruadActionConditions } from './cell/utils';
-import { useStyles } from './styles/styles';
 
 export interface IIndexTableOptions {
   omitClick?: boolean;
@@ -89,8 +72,8 @@ export const DataTable: FC<Partial<IIndexTableProps>> = ({
   customCreateUrl,
   customUpdateUrl,
   customDeleteUrl,
-  onRowSave,
   inlineEditMode,
+  onRowSave,
   onRowSaveSuccessAction: onRowSaveSuccess,
   ...props
 }) => {
@@ -128,34 +111,6 @@ export const DataTable: FC<Partial<IIndexTableProps>> = ({
     setColumnWidths,
   } = store;
 
-  const evaluateYesNoInheritJs = (
-    value: YesNoInheritJs,
-    jsExpression: string,
-    formMode: FormMode,
-    formData: any,
-    globalState: IAnyObject
-  ): boolean => {
-    switch (value) {
-      case 'yes':
-        return true;
-      case 'no':
-        return false;
-      case 'inherit':
-        return formMode === 'edit';
-      case 'js': {
-        return (
-          jsExpression &&
-          executeScriptSync<boolean>(jsExpression, {
-            formData: formData,
-            globalState: globalState,
-            moment: moment,
-          })
-        );
-      }
-    }
-    return false;
-  };
-
   const onSelectRowLocal = (index: number, row: any) => {
     if (onSelectRow) {
       onSelectRow(index, row);
@@ -164,8 +119,10 @@ export const DataTable: FC<Partial<IIndexTableProps>> = ({
     if (setSelectedRow) {
       const rowId = row?.id;
       const currentId = store.selectedRow?.id;
-      if (rowId !== currentId) setSelectedRow(index, row);
-      else setSelectedRow(null, null);
+      if (rowId !== currentId)
+        setSelectedRow(index, row);
+      else
+        setSelectedRow(null, null);
     }
   };
 
@@ -220,7 +177,6 @@ export const DataTable: FC<Partial<IIndexTableProps>> = ({
       onRowsChanged(tableData);
     }
   }, [tableData]);
-  const { styles } = useStyles();
 
   const metadata = useMetadata(false)?.metadata;
 
@@ -237,17 +193,45 @@ export const DataTable: FC<Partial<IIndexTableProps>> = ({
   const onNewRowInitialize = useMemo<RowDataInitializer>(() => {
     const result: RowDataInitializer = props.onNewRowInitialize
       ? () => {
-          // todo: replace formData and globalState with accessors (e.g. refs) and remove hooks to prevent unneeded re-rendering
-          //return onNewRowInitializeExecuter(formData, globalState);
-          const result = onNewRowInitializeExecuter(formData ?? {}, globalState, axiosHttp(backendUrl), moment);
-          return Promise.resolve(result);
-        }
+        // todo: replace formData and globalState with accessors (e.g. refs) and remove hooks to prevent unneeded re-rendering
+        //return onNewRowInitializeExecuter(formData, globalState);
+        const result = onNewRowInitializeExecuter(formData ?? {}, globalState, axiosHttp(backendUrl), moment);
+        return Promise.resolve(result);
+      }
       : () => {
-          return Promise.resolve({});
-        };
+        return Promise.resolve({});
+      };
 
     return result;
   }, [onNewRowInitializeExecuter, formData, globalState]);
+
+  const evaluateYesNoInheritJs = (
+    value: YesNoInheritJs,
+    jsExpression: string,
+    formMode: FormMode,
+    formData: any,
+    globalState: IAnyObject
+  ): boolean => {
+    switch (value) {
+      case 'yes':
+        return true;
+      case 'no':
+        return false;
+      case 'inherit':
+        return formMode === 'edit';
+      case 'js': {
+        return (
+          jsExpression &&
+          executeScriptSync<boolean>(jsExpression, {
+            formData: formData,
+            globalState: globalState,
+            moment: moment,
+          })
+        );
+      }
+    }
+    return false;
+  };
 
   const prevCrudOptions = usePrevious({
     canDelete: evaluateYesNoInheritJs(
@@ -285,29 +269,19 @@ export const DataTable: FC<Partial<IIndexTableProps>> = ({
         formData,
         globalState
       ),
-      inlineEditMode,
       canAdd: evaluateYesNoInheritJs(props.canAddInline, props.canAddInlineExpression, formMode, formData, globalState),
       onNewRowInitialize,
-      formMode,
     };
     return {
       ...result,
       enabled: result.canAdd || result.canDelete || result.canEdit,
     };
-  }, [
-    props.canEditInline,
-    inlineEditMode,
-    props.canAddInline,
-    props?.canDeleteInline,
-    formMode,
-    formData,
-    globalState,
-  ]);
+  }, [props.canDeleteInline,inlineEditMode, props.canEditInline, props.canAddInline, formMode, formData, globalState]);
 
   const widthOptions = useMemo(() => {
     return getCruadActionConditions(crudOptions, prevCrudOptions);
   }, [crudOptions, prevCrudOptions]);
-
+  
   const preparedColumns = useMemo(() => {
     const localPreparedColumns = columns
       .map((column) => {
@@ -368,8 +342,9 @@ export const DataTable: FC<Partial<IIndexTableProps>> = ({
   ]);
 
   // sort
-  const defaultSorting =
-    sortMode === 'standard' ? userSorting?.map<SortingRule<string>>((c) => ({ id: c.id, desc: c.desc })) : undefined;
+  const defaultSorting = sortMode === 'standard'
+    ? userSorting?.map<SortingRule<string>>((c) => ({ id: c.id, desc: c.desc }))
+    : undefined;
 
   // http, moment, setFormData
   const performOnRowSave = useMemo<OnSaveHandler>(() => {
@@ -418,7 +393,7 @@ export const DataTable: FC<Partial<IIndexTableProps>> = ({
           : undefined;
 
       return repository.performUpdate(rowIndex, preparedData, options).then((response) => {
-        setRowData(rowIndex, preparedData /*, response*/);
+        setRowData(rowIndex, preparedData/*, response*/);
         performOnRowSaveSuccess(preparedData, formData ?? {}, globalState, setGlobalState, setFormData);
         return response;
       });
@@ -549,15 +524,15 @@ export const DataTable: FC<Partial<IIndexTableProps>> = ({
       currentGroup: null,
       propertyName: g.propertyName,
       index: index,
-      propertyPath: g.propertyName.split('.'),
+      propertyPath: g.propertyName.split('.')
     }));
 
     const getValue = (container: object, path: string[]) => {
-      return path.reduce((prev, part) => (prev ? prev[part] : undefined), container);
+      return path.reduce((prev, part) => prev ? prev[part] : undefined, container);
     };
 
     const result: RowsGroup[] = [];
-    rows.forEach((row) => {
+    rows.forEach(row => {
       let parent: RowOrGroup[] = result;
       let differenceFound = false;
       groupLevels.forEach((g, index) => {
@@ -567,7 +542,7 @@ export const DataTable: FC<Partial<IIndexTableProps>> = ({
           g.currentGroup = {
             index: index,
             value: groupValue,
-            $childs: [],
+            $childs: []
           };
           parent.push(g.currentGroup);
           differenceFound = true;
@@ -580,8 +555,9 @@ export const DataTable: FC<Partial<IIndexTableProps>> = ({
   };
 
   const renderGroupTitle = (value: any, propertyName: string) => {
-    if (!Boolean(value) && value !== false) return <Typography.Text type="secondary">(empty)</Typography.Text>;
-    const column = groupingColumns.find((c) => isDataColumn(c) && c.propertyName === propertyName);
+    if (!Boolean(value) && value !== false)
+      return <Typography.Text type='secondary'>(empty)</Typography.Text>;
+    const column = groupingColumns.find(c => isDataColumn(c) && c.propertyName === propertyName);
     const propertyMeta = isDataColumn(column) ? column.metadata : null;
 
     return <ValueRenderer value={value} meta={propertyMeta} />;
@@ -593,12 +569,14 @@ export const DataTable: FC<Partial<IIndexTableProps>> = ({
       <Collapse
         key={key}
         defaultActiveKey={['1']}
-        expandIconPosition="start"
+        expandIconPosition='start'
         className={`sha-group-level-${group.index}`}
       >
         <Collapse.Panel header={<>{title}</>} key="1">
           {group.$childs.map((child, index) => {
-            return isGroup(child) ? renderGroup(child, index, rowRenderer) : rowRenderer(child, index);
+            return isGroup(child)
+              ? renderGroup(child, index, rowRenderer)
+              : rowRenderer(child, index);
           })}
         </Collapse.Panel>
       </Collapse>
@@ -607,14 +585,16 @@ export const DataTable: FC<Partial<IIndexTableProps>> = ({
 
   const onRowsRenderingWithGrouping: OnRowsRendering = ({ rows, defaultRender }) => {
     const groupped = convertRowsToGroups(rows);
-    return <>{groupped.map((group, index) => renderGroup(group, index, defaultRender))}</>;
+    return (
+      <>
+        {groupped.map((group, index) => renderGroup(group, index, defaultRender))}
+      </>
+    );
   };
 
   const repository = store.getRepository();
   const reorderingAvailable = useMemo<boolean>(() => {
-    return (
-      repository && repository.supportsReordering && repository.supportsReordering({ sortMode, strictSortBy }) === true
-    );
+    return repository && repository.supportsReordering && repository.supportsReordering({ sortMode, strictSortBy }) === true;
   }, [repository, sortMode, strictSortBy]);
 
   const groupingAvailable = useMemo<boolean>(() => {
@@ -623,25 +603,24 @@ export const DataTable: FC<Partial<IIndexTableProps>> = ({
 
   const handleRowsReordered = (payload: OnRowsReorderedArgs): Promise<void> => {
     const repository = store.getRepository();
-    if (!repository) return Promise.reject('Repository is not specified');
+    if (!repository)
+      return Promise.reject('Repository is not specified');
 
     const supported = repository.supportsReordering && repository.supportsReordering({ sortMode, strictSortBy });
-    if (supported === true) {
+    if (supported === true){
       const reorderPayload: RowsReorderPayload = {
         ...payload,
         propertyName: strictSortBy,
       };
-
+  
       return repository.reorder(reorderPayload);
-    } else return Promise.reject(typeof supported === 'string' ? supported : 'Reordering is not supported');
+    } else
+      return Promise.reject(typeof(supported) === 'string' ? supported : 'Reordering is not supported');
   };
 
   const onResizedChange = (columns: ColumnInstance[], _columnSizes: IColumnResizing) => {
-    const widths = columns.map<IColumnWidth>((c) => ({
-      id: c.id,
-      width: typeof c.width === 'number' ? c.width : undefined,
-    }));
-
+    const widths = columns.map<IColumnWidth>(c => ({ id: c.id, width: typeof(c.width) === 'number' ? c.width : undefined }));
+    
     setColumnWidths(widths);
   };
 
@@ -680,14 +659,14 @@ export const DataTable: FC<Partial<IIndexTableProps>> = ({
     newRowCapturePosition: props.newRowCapturePosition,
     createAction: creater,
     newRowInitData: crudOptions.onNewRowInitialize,
-    inlineEditMode,
+    inlineEditMode: props.inlineEditMode,
     inlineSaveMode: props.inlineSaveMode,
     inlineEditorComponents,
     inlineCreatorComponents,
     inlineDisplayComponents,
     minHeight: props.minHeight,
     maxHeight: props.maxHeight,
-
+    
     allowReordering: allowReordering && reorderingAvailable,
     onRowsReordered: handleRowsReordered,
 
@@ -697,7 +676,7 @@ export const DataTable: FC<Partial<IIndexTableProps>> = ({
 
   return (
     <Fragment>
-      <div className={styles.shaChildTableErrorContainer}>
+      <div className="sha-child-table-error-container">
         {exportToExcelError && <ValidationErrors error={'Error occurred while exporting to excel'} />}
       </div>
 
