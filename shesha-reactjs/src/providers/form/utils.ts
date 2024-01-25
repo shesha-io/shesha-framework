bed67fd15d2d3053c964eaf98f72cd97f309fdbf
AlexS <alex.stepantsov@shesha.io>
2024-01-25 12:55:53 +0300

    The new components added from Quick Edit does not show after saving #952

--- shesha-reactjs/src/providers/form/utils.ts
+++ shesha-reactjs/src/providers/form/utils.ts
@@ -768,7 +768,8 @@ export const getVisibleComponentIds = (
       const component = components[key] as IConfigurableFormComponent;
 
       if (filteredComponents?.includes(component.id)
-        && !getActualPropertyValue(component, allData, 'hidden')?.hidden)
+        && !getActualPropertyValue(component, allData, 'hidden')?.hidden
+        && (component.visibilityFunc == null || component.visibilityFunc(allData.data, allData.globalState, allData.formMode)))
         visibleComponents.push(key);
     }
   }
@@ -780,7 +781,6 @@ export const getVisibleComponentIds = (
  */
 export const getFilteredComponentIds = (
   components: IComponentsDictionary,
-  allData: IApplicationContext,
   propertyFilter?: (name: string) => boolean
 ): string[] => {
   const visibleComponents: string[] = [];
@@ -788,7 +788,7 @@ export const getFilteredComponentIds = (
     if (components.hasOwnProperty(key)) {
       const component = components[key] as IConfigurableFormComponent;
 
-      if (isComponentFiltered(component, allData, propertyFilter))
+      if (isComponentFiltered(component, propertyFilter))
         visibleComponents.push(key);
     }
   }
@@ -797,15 +797,13 @@ export const getFilteredComponentIds = (
 
 const isComponentFiltered = (
   component: IConfigurableFormComponent,
-  allData: IApplicationContext,
   propertyFilter?: (name: string) => boolean
 ): boolean => {
   if (propertyFilter && component.propertyName) {
     const filteredOut = propertyFilter(component.propertyName);
     if (filteredOut === false) return false;
   }
-
-  return (component.visibilityFunc == null || component.visibilityFunc(allData.data, allData.globalState, allData.formMode));
+  return true;
 };
 
 /**
