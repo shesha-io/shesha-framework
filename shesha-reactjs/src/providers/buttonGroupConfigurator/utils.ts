0f69581f41e573e671ab3c1d536522d68fe8fe69
AlexS <alex.stepantsov@shesha.io>
2024-02-01 19:02:37 +0300

    When the tab component is configured as editable, the inherited components are not set to editable #1016 Update the list of components to have 'Edit Mode instead of 'disabled' property #808 Review buttonGroup component

--- shesha-reactjs/src/providers/buttonGroupConfigurator/utils.ts
+++ shesha-reactjs/src/providers/buttonGroupConfigurator/utils.ts
@@ -29,3 +29,25 @@ export const getItemById = (items: ButtonGroupItemProps[], id: string): ButtonGr
   const position = getItemPositionById(items, id);
   return position ? position.ownerArray[position.index] : null;
 };
+
+export const updateBranch = (items: ButtonGroupItemProps[], payload: {id: any; settings: any}) => {
+  for (let index = 0; index < items.length; index++) {
+    const item = items[index];
+    if (item.id === payload.id) {
+      items[index] = {...items[index], ...payload.settings};
+      return [...items];
+    }
+
+    const parent = (item as IButtonGroup);
+
+    if (parent.childItems?.length > 0) {
+      const array = updateBranch(parent.childItems, payload);
+      if (!!array) {
+        parent.childItems = array;
+        return [...items];
+      }
+    }
+  };
+
+  return null;
+};
