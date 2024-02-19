393c60354d025bb68c59f9c64112f6e746454f5a
Omolemo Lethuloe <omolemo.lethuloe@shesha.io>
2024-02-19 16:24:10 +0200

    chore: removing ApplicationInfo from GetCurrentLogin endpoint, and adding properties to test new RawValues functionality

--- shesha-core/src/Shesha.Application/Sessions/SessionAppService.cs
+++ shesha-core/src/Shesha.Application/Sessions/SessionAppService.cs
@@ -28,15 +28,7 @@ namespace Shesha.Sessions
         //[DisableAuditing]
         public async Task<GetCurrentLoginInfoOutput> GetCurrentLoginInfo()
         {
-            var output = new GetCurrentLoginInfoOutput
-            {
-                Application = new ApplicationInfoDto
-                {
-                    Version = AppVersionHelper.Version,
-                    ReleaseDate = AppVersionHelper.ReleaseDate,
-                    Features = new Dictionary<string, bool>()
-                }
-            };
+            var output = new GetCurrentLoginInfoOutput {};
 
             if (AbpSession.TenantId.HasValue)
             {
---
+++ shesha-functional-tests/backend/src/Module/Boxfusion.SheshaFunctionalTests.Common.Domain/Domain/Enum/RefListPaymentType.cs
@@ -0,0 +1,33 @@
+﻿using Shesha.Domain.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boxfusion.SheshaFunctionalTests.Common.Domain.Domain.Enum
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [ReferenceList("SheshaFunctionalTests", "PaymentType")]
+    public enum RefListPaymentType : long
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        Cash = 1,
+        /// <summary>
+        ///             
+        /// </summary>
+        CreditCard = 2,
+        /// <summary>
+        ///     
+        /// </summary>
+        DebitCard = 3,
+        /// <summary>
+        ///               
+        /// </summary>
+        EFT = 4
+    }
+}
