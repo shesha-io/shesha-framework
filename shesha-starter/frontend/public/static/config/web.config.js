dd0a6ae9c81f7c245e8b210424cd2c24b3aff5ee
Omolemo Lethuloe <omolemo.lethuloe@shesha.io>
2024-03-04 16:31:27 +0200

    fix: adding web config files on starter

---
+++ shesha-starter/frontend/public/static/config/web.config.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line no-constant-condition
+if (typeof window) {
+    window.__APP_CONFIG__ = {
+        appInsightsInstrumentationKey: null,
+        baseUrl: 'https://starter-api-test.shesha.dev',
+        googleMapsApiKey: null,
+    };
+}
