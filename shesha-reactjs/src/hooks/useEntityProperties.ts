744e5cb85f3debd56ad7b84da18aafc6f2dfc9e5
nthangeniphumudzo <phumudzo.nthangeni@boxfusion.io>
2024-04-23 20:41:30 +0200

    giving subForm presidence

---
+++ shesha-reactjs/src/hooks/useEntityProperties.ts
@@ -0,0 +1,29 @@
+import { useContext, useState, useEffect } from 'react';
+import { SubFormContext } from '@/providers/subForm/contexts';
+import { asPropertiesArray, IPropertyMetadata } from '@/interfaces/metadata';
+import { useForm, useMetadataDispatcher } from '@/providers';
+import { IGetMetadataPayload } from '@/providers/metadataDispatcher/contexts';
+
+// Move Promise creation to a separate function
+async function fetchMetadata(dataType: string, getMetadata: Function, entityType: string) {
+    const meta = await getMetadata({ dataType, modelType: entityType });
+    return asPropertiesArray(meta?.properties, []);
+}
+
+export function useEntityProperties({ dataType }:Partial<Omit<IGetMetadataPayload,'modelType'>>): IPropertyMetadata[] {
+    const { getMetadata } = useMetadataDispatcher();
+    const {formSettings: subFormSettings } = useContext(SubFormContext);
+    const {formSettings}=useForm()
+    const [properties, setProperties] = useState<IPropertyMetadata[]>([]);
+
+    useEffect(() => {
+        const entityType = subFormSettings?.modelType || formSettings?.modelType;
+
+        // Call the separate function and handle the Promise in useEffect
+        fetchMetadata(dataType, getMetadata, entityType)
+            .then(properties => setProperties(properties));
+
+    }, [dataType, getMetadata, formSettings]);
+
+    return properties;
+}
