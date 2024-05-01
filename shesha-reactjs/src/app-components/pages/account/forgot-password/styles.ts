e70aa0d4f1eb48fdf7107f3ee2fdd2ff3756a3c2
Alex Stephens <alex.stephens@boxfusion.io>
2024-05-01 15:55:29 +0300

    minor fix

--- shesha-reactjs/src/app-components/pages/account/forgot-password/styles.ts
+++ shesha-reactjs/src/app-components/pages/account/forgot-password/styles.ts
@@ -2,14 +2,25 @@ import UnAuthedAccountPageLayout from '@/components/unAuthedAccountPageLayout';
 import { Modal } from 'antd';
 import styled from 'styled-components';
 
-export const ForgotPasswordPage = styled(UnAuthedAccountPageLayout)`
+/**
+ * There was an error 
+ * [!] (plugin typescript) RollupError: @rollup/plugin-typescript
+ * TS4023: Exported variable 'ForgotPasswordPage' has or is using name 'IStyledComponentBase' from external module "D:/a/1/s/shesha-reactjs/node_modules/styled-components/dist/types" but cannot be named.
+ * src/app-components/pages/account/forgot-password/styles.ts (5:14)
+ * 
+ * found a solution
+ * https://stackoverflow.com/questions/43900035/ts4023-exported-variable-x-has-or-is-using-name-y-from-external-module-but
+ * 
+ */
+
+export const ForgotPasswordPage: any = styled(UnAuthedAccountPageLayout)`
   .back-to-login-btn {
     text-align: center;
   }
 `;
 
 // The style below should be outside as the modal will not be nested within this page
-export const VerifyOtpModal = styled(Modal)`
+export const VerifyOtpModal: any = styled(Modal)`
   .ant-alert {
     margin-bottom: 10px;
 
