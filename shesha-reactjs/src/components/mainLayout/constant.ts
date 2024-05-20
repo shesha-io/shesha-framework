0ab39138dc721e4a117c5c0f9665d9717d088f05
Luke Elabor <Lukeybooi@gmail.com>
2024-05-20 14:24:02 +0200

    refactor: modified module & name of HEADER_CONFIGURATION

--- shesha-reactjs/src/components/mainLayout/constant.ts
+++ shesha-reactjs/src/components/mainLayout/constant.ts
@@ -3,5 +3,5 @@ import { IPersistedFormProps } from '@/providers';
 export const SIDEBAR_COLLAPSE = 'SIDEBAR_COLLAPSE';
 
 //#region Fixed Configuration Names
-export const HEADER_CONFIGURATION: IPersistedFormProps = { name: 'header-configuration', module: 'StarterTemplate' };
+export const HEADER_CONFIGURATION: IPersistedFormProps = { name: 'header', module: 'Shesha' };
 //#endregion
