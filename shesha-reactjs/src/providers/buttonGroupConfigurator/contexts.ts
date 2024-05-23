9a3e250b8d2dea2767031894e2d3d60f40073393
James Baloyi <james_baloyi@yahoo.com>
2024-05-23 16:49:37 +0200

    updated branch to avoid build issues

--- shesha-reactjs/src/providers/buttonGroupConfigurator/contexts.ts
+++ shesha-reactjs/src/providers/buttonGroupConfigurator/contexts.ts
@@ -18,7 +18,7 @@ export interface IButtonGroupConfiguratorStateContext {
   form: ConfigurableFormInstance | null | undefined;
   selectedItemId?: string;
   buttonCount: number;
-  groupCount: number,
+  groupCount: number;
   readOnly: boolean;
 }
 
