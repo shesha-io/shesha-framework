865fc9ff005252092643b6974ee4c584f93f33fa
Alex Stephens <alex.stephens@boxfusion.io>
2024-05-29 11:52:50 +0300

    parentFormValues variable log as undefined when utilised #1570 If a form has a tab and button components configured, upon saving a 415 error gets displayed #1528 If setFormData is used onInitialized, the value does not get appended onto the specified field in the form #1519 GetAuditTrail endpoint parameters are not working as expected #1511

---
+++ shesha-core/src/Shesha.Application/EntityHistory/EntityHistoryResultRequestDto.cs
@@ -0,0 +1,17 @@
+﻿using Abp.Application.Services.Dto;
+using Shesha.Application.Services.Dto;
+using System.Collections.Generic;
+
+namespace Shesha.EntityHistory
+{
+    /// <summary>
+    /// Filtered, pages and sorted request DTO
+    /// </summary>
+    public class EntityHistoryResultRequestDto : PagedAndSortedResultRequestDto, IPagedAndSortedResultRequest
+    {
+        /// <summary>
+        /// Quick search string. Is used to search entities by text
+        /// </summary>
+        public string QuickSearch { get; set; }
+    }
+}
