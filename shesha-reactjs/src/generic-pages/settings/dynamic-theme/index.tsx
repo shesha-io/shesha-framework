16738de0b2bf15a741effc7ea4bcba4cf3f6ff25
James Baloyi <james_baloyi@yahoo.com>
2024-06-19 11:50:34 +0200

    theme page enhances cleanup

--- shesha-reactjs/src/generic-pages/settings/dynamic-theme/index.tsx
+++ shesha-reactjs/src/generic-pages/settings/dynamic-theme/index.tsx
@@ -5,19 +5,23 @@ import AlertsExample from './alertsExamples';
 import FormExample from './form';
 import ThemeParameters from './parameters';
 import TextsExample from './textsExample';
+import { useStyles } from './styles/styles';
 
 export interface IConfigurableThemePageProps { }
 
 export const ConfigurableThemePage: FC<IConfigurableThemePageProps> = () => {
+  const { styles } = useStyles();
+
   return (
     <Page title="Customize theme">
       <Row gutter={16}>
         <Col xs={24} sm={24} md={8} lg={6} xl={6} xxl={6}>
           <CollapsiblePanel collapsible="disabled"
             header={
-              <Typography.Text type='secondary' style={{ fontSize: 18, fontWeight: 700 }}>Theme Parameters</Typography.Text>
+              <Typography.Text type='secondary' className={styles.themeHeader}>Theme Parameters</Typography.Text>
             }
-            style={{ height: 'calc(100vh - 120px)', overflowY: 'auto', scrollbarWidth: 'none' }}>
+            className={styles.themeParameters}
+          >
             <Alert type="info" message="You can modify the values by selecting the colour block" showIcon />
             <ThemeParameters />
           </CollapsiblePanel>
@@ -25,9 +29,9 @@ export const ConfigurableThemePage: FC<IConfigurableThemePageProps> = () => {
 
         <Col xs={24} sm={24} md={16} lg={18} xl={18} xxl={18}>
           <CollapsiblePanel
-            style={{ height: 'calc(100vh - 120px)', overflowY: 'auto', scrollbarWidth: 'none' }}
+            className={styles.themeParameters}
             header={
-              <Typography.Text type='secondary' style={{ fontSize: 18, fontWeight: 700 }}>Results</Typography.Text>
+              <Typography.Text type='secondary' className={styles.themeHeader}>Results</Typography.Text>
             }
           >
             <SectionSeparator title="Alerts" />
