f613d55d3e827b184e9eb8b54f08055b167281a3
Thulasizwe <tnkambule980115@gmail.com>
2024-07-05 12:52:40 +0200

    filter component rename

--- shesha-reactjs/src/designer-components/dataTable/advancedFilterButton/advancedFilterButton.tsx
+++ shesha-reactjs/src/designer-components/dataTable/advancedFilterButton/advancedFilterButton.tsx
@@ -8,66 +8,75 @@ import * as Icons from '@ant-design/icons';
 import { getStyle } from '@/providers/form/utils';
 
 export const AdvancedFilterButton: FC<IButtonComponentProps> = (props) => {
-    const {
-        isInProgress: { isFiltering },
-        setIsInProgressFlag, tableFilter
-    } = useDataTableStore();
+  const {
+    isInProgress: { isFiltering },
+    setIsInProgressFlag,
+    tableFilter,
+  } = useDataTableStore();
 
-    const [icon, setIcon] = useState(null);
-    const { data: formData } = useFormData();
-    const localStyle = getStyle(props.style, formData);
-    const { styles } = useStyles();
-    const startFilteringColumns = () => setIsInProgressFlag({ isFiltering: true, isSelectingColumns: false });
+  const [icon, setIcon] = useState(null);
+  const { data: formData } = useFormData();
+  const localStyle = getStyle(props.style, formData);
+  const { styles } = useStyles();
+  const startFilteringColumns = () => setIsInProgressFlag({ isFiltering: true, isSelectingColumns: false });
 
-    const filterColumns = tableFilter?.map((filter) => filter.columnId);
-    const hasFilters = filterColumns?.length > 0 || isFiltering;
+  const filterColumns = tableFilter?.map((filter) => filter.columnId);
+  const hasFilters = filterColumns?.length > 0 || isFiltering;
 
-    const buttonStyle = {
-        ...localStyle,
-        ...{ color: props.buttonType !== "primary" && !props.danger ? styles.primaryColor : "" },
-        border: hasFilters ? `1px solid ${styles.primaryColor}` : 'none',
-    };
+  const buttonStyle = {
+    ...localStyle,
+    ...{ color: props.buttonType !== 'primary' && !props.danger ? styles.primaryColor : '' },
+    border: hasFilters ? `1px solid ${styles.primaryColor}` : 'none',
+  };
 
-    const splitByCapitalLetters = (str) => {
-        return str?.replace(/([a-z])([A-Z])/g, '$1 $2').replace(/([A-Z])([A-Z][a-z])/g, '$1 $2').split(" ");
-    };
+  const splitByCapitalLetters = (str: string) => {
+    return str
+      ?.replace(/([a-z])([A-Z])/g, '$1 $2')
+      .replace(/([A-Z])([A-Z][a-z])/g, '$1 $2')
+      .split(' ');
+  };
 
-    const customIcon = () => {
-        if (props.icon) {
-            const splitIconName = splitByCapitalLetters(props.icon);
-            splitIconName.pop();
-            splitIconName.push(hasFilters ? 'Filled' : 'Outlined');
-            const iconName = splitIconName.join('');
+  const customIcon = () => {
+    if (props.icon) {
+      const splitIconName = splitByCapitalLetters(props.icon);
+      splitIconName.pop();
+      splitIconName.push(hasFilters ? 'Filled' : 'Outlined');
+      const iconName = splitIconName.join('');
 
-            return Icons[iconName];
-        };
+      return Icons[iconName];
+    }
 
-        return null;
-    };
+    return null;
+  };
 
-    useEffect(() => {
-        setIcon(customIcon());
-    }, [props.icon, hasFilters]);
+  useEffect(() => {
+    setIcon(customIcon());
+  }, [props.icon, hasFilters]);
 
-    const IconComponent = icon;
-    const defaultIcon = hasFilters ? <FilterFilled /> : <FilterOutlined />;
-    const filterIcon = icon ? <IconComponent /> : defaultIcon;
+  const IconComponent = icon;
+  const defaultIcon = hasFilters ? <FilterFilled /> : <FilterOutlined />;
+  const filterIcon = icon ? <IconComponent /> : defaultIcon;
 
-    return (
-        <Badge count={tableFilter?.length} color={isFiltering || props.readOnly ? styles.disabledColor : styles.primaryColor} size='small' title={filterColumns?.join("  ")}>
-            <Button
-                type={props.buttonType}
-                title={filterColumns?.join("  ")}
-                onClick={startFilteringColumns}
-                className={styles.button}
-                danger={props.danger}
-                disabled={props.readOnly || isFiltering}
-                icon={filterIcon}
-                size={props.size}
-                style={isFiltering || props.readOnly ? {} : { ...buttonStyle }}
-            >
-                {props.label}
-            </Button>
-        </Badge>
-    );
+  return (
+    <Badge
+      count={tableFilter?.length}
+      color={isFiltering || props.readOnly ? styles.disabledColor : styles.primaryColor}
+      size="small"
+      title={filterColumns?.join('  ')}
+    >
+      <Button
+        type={props.buttonType}
+        title={filterColumns?.join('  ')}
+        onClick={startFilteringColumns}
+        className={styles.button}
+        danger={props.danger}
+        disabled={props.readOnly || isFiltering}
+        icon={filterIcon}
+        size={props.size}
+        style={isFiltering || props.readOnly ? {} : { ...buttonStyle }}
+      >
+        {props.label}
+      </Button>
+    </Badge>
+  );
 };
--- shesha-reactjs/src/designer-components/dataTable/advancedFilterButton/advancedFilterButtonComponent.tsx
+++ shesha-reactjs/src/designer-components/dataTable/advancedFilterButton/advancedFilterButtonComponent.tsx
@@ -8,30 +8,30 @@ import { IButtonComponentProps } from '@/designer-components/button/interfaces';
 import { Show } from '@/components';
 import { Tooltip } from 'antd';
 
-const FilterComponent: IToolboxComponent<IButtonComponentProps> = {
+const AdvancedFilterButtonComponent: IToolboxComponent<IButtonComponentProps> = {
   type: 'datatable.filter',
   name: 'Table Filter',
   icon: <FilterOutlined />,
-  Factory: ({ model }) => {
-
-    return model.hidden ? null : <div>
-      <AdvancedFilterButton {...model} />
-      <Show when={Boolean(model.tooltip?.trim())}>
-        <Tooltip title={model.tooltip}>
-          <QuestionCircleOutlined className='tooltip-question-icon' size={14} color='gray' />
-        </Tooltip>
-      </Show>
-    </div>;
-  },
+  Factory: ({ model }) =>
+    model.hidden ? null : (
+      <div>
+        <AdvancedFilterButton {...model} />
+        <Show when={Boolean(model.tooltip?.trim())}>
+          <Tooltip title={model.tooltip}>
+            <QuestionCircleOutlined className="tooltip-question-icon" size={14} color="gray" />
+          </Tooltip>
+        </Show>
+      </div>
+    ),
   initModel: (model) => {
     return {
       ...model,
       buttonType: 'link',
-      label: "",
+      label: '',
     };
   },
   settingsFormMarkup: (context) => getSettings(context),
   validateSettings: (model) => validateConfigurableComponentSettings(getSettings(model), model),
 };
 
-export default FilterComponent;
+export default AdvancedFilterButtonComponent;
