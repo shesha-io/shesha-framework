ad42b6ef81403ba0d6a225c2a5e20d0e07a29d57
Tshepo-Mahlangu-boxfusion <159532803+Tshepo-Mahlangu-boxfusion@users.noreply.github.com>
2024-07-10 17:08:27 +0200

    chore: update organisation entities and removal of the mentioned entites
    - Added properties to Organisation and OrganisationBase entities
    - Deleted OrganisationUnit and ShaRoleAppointedUnit entities
    - Added necessary migrations for the changes

---
+++ shesha-core/src/Shesha.Core/Migrations/M20240710033000.cs
@@ -0,0 +1,37 @@
+﻿using FluentMigrator;
+using NUglify;
+using Shesha.FluentMigrator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shesha.Migrations
+{
+    [Migration(20240710033000)]
+    public class M20240710033000 : Migration
+    {
+        public override void Up()
+        {
+            Alter.Table("Core_Organisations").AddColumn("ContactName").AsString().Nullable();
+            Alter.Table("Core_Organisations").AddColumn("ContactRole").AsString().Nullable();
+            Alter.Table("Core_Organisations").AddColumn("OrderIndex").AsInt64().Nullable();
+
+            this.Shesha().ReferenceListCreate("Shesha.Core", "OrganisationStatus")
+            .SetDescription("Organisation Status") // set desctiption
+                .SetNoSelectionValue(1);
+
+            Alter.Table("Core_Organisations").AddColumn("StatusLkp").AsInt64().Nullable();
+
+            Delete.Column("OrganisationUnit").FromTable("Core_ShaRoleAppointments");
+             Delete.Column("VatRegistrationNo").FromTable("Core_Organisations");
+             Delete.Column("CompanyRegistrationNo").FromTable("Core_Organisations");
+        }
+        public override void Down()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
