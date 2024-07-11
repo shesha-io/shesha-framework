8d3ecb958cfccc395e3d0d88281c7f22c463963b
Tshepo-Mahlangu-boxfusion <159532803+Tshepo-Mahlangu-boxfusion@users.noreply.github.com>
2024-07-11 10:38:45 +0200

    chore: update organisation entities and removal of the mentioned entites
    - Added properties to Organisation and OrganisationBase entities
    - Deleted OrganisationUnit and ShaRoleAppointedUnit entities
    - Added necessary migrations for the changes

---
+++ shesha-core/src/Shesha.Core/Migrations/M20240711103300.cs
@@ -0,0 +1,42 @@
+﻿using FluentMigrator;
+using NUglify;
+using Shesha.FluentMigrator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shesha.Migrations
+{
+    [Migration(20240711103300)]
+    public class M20240711103300 : Migration
+    {
+        public override void Up()
+        {
+            Alter.Table("Core_Organisations").AddColumn("ContactName").AsString().Nullable();
+            Alter.Table("Core_Organisations").AddColumn("ContactRole").AsString().Nullable();
+            Alter.Table("Core_Organisations").AddColumn("OrderIndex").AsInt64().Nullable();
+
+            this.Shesha().ReferenceListCreate("Shesha.Core", "OrganisationStatus")
+            .SetDescription("Organisation Status") // set desctiption
+                .SetNoSelectionValue(1);
+
+            Alter.Table("Core_Organisations").AddColumn("StatusLkp").AsInt64().Nullable();
+            Delete.Column("VatRegistrationNo").FromTable("Core_Organisations");
+            Delete.Column("CompanyRegistrationNo").FromTable("Core_Organisations");
+
+
+            Delete.ForeignKey("FK_Core_ShaRoleAppointments_OrganisationUnitId_Core_Organisations_Id").OnTable("Core_ShaRoleAppointments");
+            Delete.Index("IX_Core_ShaRoleAppointments_OrganisationUnitId").OnTable("Core_ShaRoleAppointments");
+            Delete.Column("OrganisationUnitId").FromTable("Core_ShaRoleAppointments");
+
+
+        }
+        public override void Down()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
