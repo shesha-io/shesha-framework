1198160eca3d5b147d7a3a4be8609064aa8d0a5c
Luke Elabor <Lukeybooi@gmail.com>
2024-07-16 14:21:24 +0200

    chore: added HEADER_PUB_PORTAL_CONFIGURATION

--- shesha-reactjs/src/components/mainLayout/constant.ts
+++ shesha-reactjs/src/components/mainLayout/constant.ts
@@ -4,4 +4,5 @@ export const SIDEBAR_COLLAPSE = 'SIDEBAR_COLLAPSE';
 
 //#region Fixed Configuration Names
 export const HEADER_CONFIGURATION: IPersistedFormProps = { name: 'header', module: 'Shesha' };
+export const HEADER_PUB_PORTAL_CONFIGURATION: IPersistedFormProps = { name: 'header-public-portal', module: 'Shesha' };
 //#endregion
