bda2937e223a4273162dcecd95b9dd8159ab1f26
Alex Stephens <alex.stephens@boxfusion.io>
2024-07-25 14:46:14 +0300

    Side Menu: Update Visibility property to Hidden property #1591

--- shesha-reactjs/src/components/configurableSidebarMenu/configurator/sidebarListItem.tsx
+++ shesha-reactjs/src/components/configurableSidebarMenu/configurator/sidebarListItem.tsx
@@ -4,6 +4,7 @@ import { QuestionCircleOutlined } from '@ant-design/icons';
 import ShaIcon, { IconType } from '@/components/shaIcon';
 import { ISidebarMenuItem } from '@/interfaces/sidebar';
 import { useStyles } from '@/components/listEditor/styles/styles';
+import { getActualModel, useAvailableConstantsData } from '@/index';
 
 const { Text } = Typography;
 
@@ -13,22 +14,24 @@ export interface ISidebarMenuItemProps {
 
 export const SidebarListItem: FC<ISidebarMenuItemProps> = ({ item }) => {
   const { styles } = useStyles();
+  const allData = useAvailableConstantsData();
+  const actialItem = getActualModel(item, allData);
 
   return (
     <>
-      {item.itemType === 'button' && (
+      {actialItem.itemType === 'button' && (
         <>
-          {item.icon && <ShaIcon iconName={item.icon as IconType} />}
-          <span className={styles.listItemName}>{item.title}</span>
-          {item.tooltip && (
-            <Tooltip title={item.tooltip}>
+          {actialItem.icon && <ShaIcon iconName={actialItem.icon as IconType} />}
+          <span className={styles.listItemName}>{actialItem.title}</span>
+          {actialItem.tooltip && (
+            <Tooltip title={actialItem.tooltip}>
               <QuestionCircleOutlined className={styles.helpIcon} />
             </Tooltip>
           )}
         </>
       )}
-      {item.itemType === 'divider' && (<Text type="secondary">— divider —</Text>)}
-      {/* {isDynamicItem(item) && (<DynamicGroupDetails {...item} />)} */}
+      {actialItem.itemType === 'divider' && (<Text type="secondary">— divider —</Text>)}
+      {/* {isDynamicItem(actialItem) && (<DynamicGroupDetails {...actialItem} />)} */}
     </>
   );
 };
