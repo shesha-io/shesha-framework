d95fed6a135c5fdf89c8aa7c7f89edcddc5a030f
Thulasizwe <tnkambule980115@gmail.com>
2024-09-11 11:32:04 +0200

    remove log

--- shesha-reactjs/src/components/buttonGroupConfigurator/properties.tsx
+++ shesha-reactjs/src/components/buttonGroupConfigurator/properties.tsx
@@ -1,14 +1,14 @@
-import React, { FC, useMemo } from 'react';
-import { Empty } from 'antd';
+import React, { FC, useMemo, useRef } from 'react';
+import { Empty, Form } from 'antd';
 import itemSettingsJson from './itemSettings.json';
-import { getSettings } from '@/designer-components/button/settingsForm';
 import itemGroupSettingsJson from './itemGroupSettings.json';
 import { FormMarkup } from '@/providers/form/models';
 import { useDebouncedCallback } from 'use-debounce';
+import { ConfigurableFormInstance } from '@/providers/form/contexts';
 import { SourceFilesFolderProvider } from '@/providers/sourceFileManager/sourcesFolderProvider';
-import { ConfigurableForm } from '@/components/configurableForm';
 import { ButtonGroupItemProps } from '@/providers';
 import { sheshaStyles } from '@/styles';
+import { ConfigurableForm } from '../configurableForm';
 
 export interface IButtonGroupPropertiesProps {
   item?: ButtonGroupItemProps;
@@ -17,6 +17,10 @@ export interface IButtonGroupPropertiesProps {
 }
 
 export const ButtonGroupProperties: FC<IButtonGroupPropertiesProps> = ({ item, onChange, readOnly }) => {
+  const [form] = Form.useForm();
+
+  const formRef = useRef<ConfigurableFormInstance>(null);
+
   const debouncedSave = useDebouncedCallback(
     values => {
       onChange?.({ ...item, ...values });
@@ -40,11 +44,12 @@ export const ButtonGroupProperties: FC<IButtonGroupPropertiesProps> = ({ item, o
       <SourceFilesFolderProvider folder={`button-${item.id}`}>
         <ConfigurableForm
           //key={selectedItemId} // rerender for each item to initialize all controls
+          formRef={formRef}
           labelCol={{ span: 24 }}
           wrapperCol={{ span: 24 }}
           mode={readOnly ? 'readonly' : 'edit'}
           markup={markup}
-          cacheKey={item.itemType}
+          form={form}
           initialValues={item}
           onValuesChange={debouncedSave}
           className={sheshaStyles.verticalSettingsClass}
