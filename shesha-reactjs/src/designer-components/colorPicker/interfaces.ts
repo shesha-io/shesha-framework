f678f9613ab88d3861c16a1f8f7c2932ca87eceb
Thulasizwe <tnkambule980115@gmail.com>
2024-09-25 09:14:45 +0200

    colorPicker interface update

--- shesha-reactjs/src/designer-components/colorPicker/interfaces.ts
+++ shesha-reactjs/src/designer-components/colorPicker/interfaces.ts
@@ -2,6 +2,7 @@ import { IConfigurableFormComponent } from '@/providers/form/models';
 
 export interface IColorPickerComponentProps extends IConfigurableFormComponent {
     title?: string;
+    allowClear?: boolean;
     showText?: boolean;
     disabledAlpha?: boolean;
 }
