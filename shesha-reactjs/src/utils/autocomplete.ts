5bcb422f0da0aed99360c40078f16e28593e9171
Robert Marshall <Robert.Marshall@Boxfusion.io>
2024-09-26 10:18:47 +0200

    [fix] Autocomplete util- type check to incude string and object and arrays of object or string

--- shesha-reactjs/src/utils/autocomplete.ts
+++ shesha-reactjs/src/utils/autocomplete.ts
@@ -13,7 +13,7 @@ interface AutocompleteReturn {
   loading: boolean;
 }
 
-export type AutocompleteValueType = string | object | object[];
+export type AutocompleteValueType = string | string[] | object | object[];
 
 export interface IAutocompleteProps {
   entityType: string;
