6cc07a03404968c82ebd402ebd78ff9610fc4fcf
Omolemo Lethuloe <Omolemo.Lethuloe@boxfusion.io>
2024-10-23 15:19:18 +0200

    Shesha update: 0.39.3

--- shesha-starter/publicportal/src/models/accessToken.d.ts
+++
@@ -1,9 +0,0 @@
-export interface IAccessToken {
-  accessToken?: string | null;
-  encryptedAccessToken?: string | null;
-  expireInSeconds?: number;
-  expireOn?: string;
-  userId?: number;
-  personId?: string | null;
-  deviceName?: string | null;
-}
