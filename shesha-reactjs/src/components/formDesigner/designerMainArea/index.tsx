4389352f3380ada1a66063632a39856bda4cfdd3
teboho <tebohomakhalemele@outlook.com>
2024-11-21 13:35:03 +0200

    chore: revert styling on the designer

--- shesha-reactjs/src/components/formDesigner/designerMainArea/index.tsx
+++ shesha-reactjs/src/components/formDesigner/designerMainArea/index.tsx
@@ -1,11 +1,11 @@
 import { ComponentPropertiesTitle } from '../componentPropertiesTitle';
 import ParentProvider from '@/providers/parentProvider';
-import React, { FC, useEffect } from 'react';
+import React, { FC } from 'react';
 import Toolbox from '../toolbox';
 import { ConfigurableFormRenderer, SidebarContainer } from '@/components';
 import { DebugPanel } from '../debugPanel';
 import { MetadataProvider, useCanvas, useForm } from '@/providers';
-import { useFormDesignerActions, useFormDesignerState } from '@/providers/formDesigner';
+import { useFormDesignerState } from '@/providers/formDesigner';
 import { useStyles } from '../styles/styles';
 import { ComponentPropertiesPanel } from '../componentPropertiesPanel';
 import ConditionalWrap from '@/components/conditionalWrapper';
@@ -16,57 +16,55 @@ export interface IDesignerMainAreaProps {
 
 export const DesignerMainArea: FC<IDesignerMainAreaProps> = () => {
     const { isDebug, readOnly } = useFormDesignerState();
-    const { setSelectedComponent } = useFormDesignerActions();
     const { form, formMode, formSettings } = useForm();
     const { designerWidth, zoom } = useCanvas();
     const { styles } = useStyles();
 
-    useEffect(() => {
-        if (formMode !== 'designer') {
-            setSelectedComponent(null);
-        }
-    }, [formMode]);
-
     return (
-        <ConditionalWrap
-            condition={formMode === 'designer'}
-            wrap={(children) => (
-                <SidebarContainer
-                    leftSidebarProps={
-                        readOnly
-                            ? null
-                            : {
-                                title: 'Builder Widgets',
-                                content: () => <Toolbox />,
-                                placeholder: 'Builder Widgets',
-                            }
-                    }
-                    rightSidebarProps={{
-                        title: () => <ComponentPropertiesTitle />,
-                        content: () => <ComponentPropertiesPanel />,
-                        placeholder: 'Properties',
-                    }}
-                >
-                    {children}
-                </SidebarContainer>
-            )}
-        >
-            <div className={`${styles.mainArea} ${formMode === 'designer' ? '' : styles.previewBorderTop10}`}>
+        <div className={styles.mainArea} style={{
+            borderTop: '1px solid #d3d3d3',
+        }}>
+            <ConditionalWrap
+                condition={formMode === 'designer'}
+                wrap={(children) => (
+                    <SidebarContainer
+                        leftSidebarProps={
+                            readOnly
+                                ? null
+                                : {
+                                    title: 'Builder Widgets',
+                                    content: () => <Toolbox />,
+                                    placeholder: 'Builder Widgets',
+                                }
+                        }
+                        rightSidebarProps={{
+                            title: () => <ComponentPropertiesTitle />,
+                            content: () => <ComponentPropertiesPanel />,
+                            placeholder: 'Properties',
+                        }}
+                    >
+                        <div className={styles.mainArea}>
+                            {children}
+                        </div>
+                    </SidebarContainer>
+                )}
+            >
                 <div style={{ width: designerWidth, zoom: `${zoom}%`, overflow: 'auto', margin: '0 auto' }}>
                     <ConditionalWrap
                         condition={Boolean(formSettings?.modelType)}
                         wrap={(children) => (<MetadataProvider modelType={formSettings?.modelType}>{children}</MetadataProvider>)}
                     >
-                        <ParentProvider model={{}} formMode={formMode}>
-                            <ConfigurableFormRenderer form={form} className={undefined}  >
+                        <ParentProvider model={{}} formMode='designer'>
+                            <ConfigurableFormRenderer form={form} className={formMode === 'designer' ? styles.designerWorkArea : undefined}  >
                                 {isDebug && (
                                     <DebugPanel formData={form.getFieldValue([])} />
                                 )}
                             </ConfigurableFormRenderer>
                         </ParentProvider>
                     </ConditionalWrap>
+
                 </div>
-            </div>
-        </ConditionalWrap>
+            </ConditionalWrap>
+        </div>
     );
 };
