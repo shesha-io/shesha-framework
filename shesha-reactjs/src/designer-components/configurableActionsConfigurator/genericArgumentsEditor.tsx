0b3505c3b2d01d05578b1c159a227b8f954df61d
Tshepo-Mahlangu-boxfusion <159532803+Tshepo-Mahlangu-boxfusion@users.noreply.github.com>
2024-12-05 11:32:52 +0200

    fix: fixed the layout of the arguments editor

--- shesha-reactjs/src/designer-components/configurableActionsConfigurator/genericArgumentsEditor.tsx
+++ shesha-reactjs/src/designer-components/configurableActionsConfigurator/genericArgumentsEditor.tsx
@@ -28,6 +28,7 @@ function GenericArgumentsEditor<TModel extends IConfigurableActionArguments>({
 
   return (
     <ConfigurableForm
+      layout='vertical'
       labelCol={{ span: 24 }}
       wrapperCol={{ span: 24 }}
       mode={readOnly ? 'readonly' : 'edit'}
