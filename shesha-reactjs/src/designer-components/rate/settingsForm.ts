4346416b7947df6b40f5415ae6e8a3033639a59b
teboho <tebohomakhalemele@outlook.com>
2024-12-10 11:24:30 +0200

    chore: rate maxRating label in count property setting

--- shesha-reactjs/src/designer-components/rate/settingsForm.ts
+++ shesha-reactjs/src/designer-components/rate/settingsForm.ts
@@ -49,7 +49,7 @@ export const getSettings = (data: IRateProps) => {
                     type: 'number',
                     id: nanoid(),
                     propertyName: 'count',
-                    label: 'Count',
+                    label: 'Max Rating',
                   },
                     {
                       type: 'iconPicker',
