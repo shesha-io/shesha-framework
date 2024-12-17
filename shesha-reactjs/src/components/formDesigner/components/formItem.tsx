490c9c71361b0355744884cc32c21951ce9003c5
teboho <tebohomakhalemele@outlook.com>
2024-12-17 15:50:36 +0200

    chore: tabbed properties panel for Alert

--- shesha-reactjs/src/components/formDesigner/components/formItem.tsx
+++ shesha-reactjs/src/components/formDesigner/components/formItem.tsx
@@ -125,7 +125,7 @@ const ConfigurableFormItem: FC<IConfigurableFormItemProps> = ({
     rules: model.hidden ? [] : getValidationRules(model, { formData }),
     labelCol: layout?.labelCol,
     wrapperCol: hideLabel ? { span: 24 } : layout?.wrapperCol,
-    layout: model.layout,
+    // layout: model.layout,
     name: model.context ? undefined : getFieldNameFromExpression(propName),
   };
 
