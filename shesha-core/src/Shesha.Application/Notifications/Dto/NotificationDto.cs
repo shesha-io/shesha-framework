03853cc0f970c15713a66ee7840b6099bdd81cb0
Tshepo-Mahlangu-boxfusion <159532803+Tshepo-Mahlangu-boxfusion@users.noreply.github.com>
2025-01-16 20:27:13 +0200

    Chore: Creation of bulk and direct methods to test
    (cherry picked from commit 2f4a2e0e95c130e7e7941afb115465e29d30da4a)

---
+++ shesha-core/src/Shesha.Application/Notifications/Dto/NotificationDto.cs
@@ -0,0 +1,67 @@
+﻿using Abp.Notifications;
+using Shesha.Domain;
+using Shesha.EntityReferences;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shesha.Notifications.Dto
+{
+    public class NotificationDto
+    {
+        public NotificationTypeConfig type { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public NotificationChannelConfig channel { get; set; } = null;
+        /// <summary>
+        /// 
+        /// </summary>
+        public long priority { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public Person recipient { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public GenericEntityReference triggeringEntity { get; set; } = null;
+    }
+
+    public class BulkNotificationDto
+    {
+        public NotificationTypeConfig type { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+
+        public NotificationChannelConfig channel { get; set; } = null;
+        /// <summary>
+        /// 
+        /// </summary>
+        public long priority { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public List<Person> recipients { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public GenericEntityReference triggeringEntity { get; set; } = null;
+    }
+
+    public class TestData : NotificationData
+    {
+        public string subject { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string name { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string body { get; set; }
+    }
+}
--- shesha-core/src/Shesha.Application/Notifications/NotificationAppService.cs
+++ shesha-core/src/Shesha.Application/Notifications/NotificationAppService.cs
@@ -22,20 +22,100 @@ namespace Shesha.Notifications
     public class NotificationAppService: SheshaAppServiceBase
     {
         private readonly INotificationSender _notificationService;
+        private readonly IRepository<NotificationChannelConfig, Guid> _notificationChannelRepository;
+        private readonly IRepository<Person, Guid> _personRepository;
+        private readonly IRepository<NotificationTypeConfig, Guid> _notificationTypeRepository;
+        private readonly IStoredFileService _storedFileService;
 
-        public NotificationAppService(INotificationSender notificationService)
+        public NotificationAppService(INotificationSender notificationService, IRepository<NotificationChannelConfig, Guid> notificationChannelRepository, IRepository<Person, Guid> personRepository, IRepository<NotificationTypeConfig, Guid> notificationType, IStoredFileService storedFileService)
         {
+            _notificationChannelRepository = notificationChannelRepository;
             _notificationService = notificationService;
+            _personRepository = personRepository;
+            _notificationTypeRepository = notificationType;
+            _storedFileService = storedFileService;
         }
 
-        public async Task PublishAsync(NotificationTypeConfig type, long priority, NotificationData data, Person recipient, GenericEntityReference triggeringEntity = null)
+        public async Task TestNotificationAsync(NotificationDto notification)
         {
+            // Fetch notification type details
+            var type = await _notificationTypeRepository.FirstOrDefaultAsync(notification.type.Id);
+            if (type == null)
+                throw new ArgumentException($"Notification type with ID {notification.type.Id} does not exist.");
+            // Get recipient details
+            var recipient = await _personRepository.FirstOrDefaultAsync(notification.recipient.Id);
             if (recipient == null)
-                throw new Exception($"{nameof(recipient)} must not be null");
+                throw new ArgumentException($"Recipient with ID {notification.recipient.Id} does not exist.");
+            // Get channel details
+            var channel = notification.channel != null  ? await _notificationChannelRepository.FirstOrDefaultAsync(notification.channel.Id) : null;
+            // Prepare notification data
+            var data = new TestData()
+            {
+                name = recipient.FullName,
+                subject = type.Name,
+                body = type.Description
+            };
+
+            var getAttachments = await _storedFileService.GetAttachmentsAsync(recipient.Id, "Shesha.Domain.Person");
+            var attachments = getAttachments.Select(x => new NotificationAttachmentDto()
+            {
+                FileName = x.FileName,
+                StoredFileId = x.Id,
+            }).ToList();
 
             var sender = await GetCurrentPersonAsync();
+            if (sender == null)
+                throw new InvalidOperationException("Current person could not be determined. Ensure the user is logged in.");
 
-            await _notificationService.SendNotification(type, sender, recipient, data, (RefListNotificationPriority)priority, null, triggeringEntity);
+            await _notificationService.SendNotification(
+                type,
+                sender,
+                recipient,
+                data,
+                (RefListNotificationPriority)notification.priority,
+                attachments,
+                null,
+                channel
+            );
+        }
+
+        public async Task BulkPublishAsync (BulkNotificationDto notification)
+        {
+            if(notification.recipients == null || notification.recipients.Count == 0)
+                throw new ArgumentException("Recipients must not be null or empty.");
+            // Fetch notification type details
+            var type = await _notificationTypeRepository.FirstOrDefaultAsync(notification.type.Id);
+            if (type == null)
+                throw new ArgumentException($"Notification type with ID {notification.type.Id} does not exist.");
+            // Get channel details
+            var channel = notification.channel != null ? await _notificationChannelRepository.FirstOrDefaultAsync(notification.channel.Id) : null;
+            // Prepare notification data
+            var data = new TestData()
+            {
+                name = "Test Name",
+            };
+            // Get the current person
+            var sender = await GetCurrentPersonAsync();
+            if (sender == null)
+                throw new InvalidOperationException("Current person could not be determined. Ensure the user is logged in.");
+            // Send notification to each recipient
+            foreach (var reciever in notification.recipients)
+            {
+                var recipient = await _personRepository.FirstOrDefaultAsync(reciever.Id);
+                if (recipient == null)
+                    throw new Exception($"{nameof(recipient)} must not be null");
+
+                await _notificationService.SendNotification(
+                    type,
+                    sender,
+                    recipient, 
+                    data,
+                    (RefListNotificationPriority)notification.priority,
+                    null,
+                    null,
+                    channel
+                 );
+            }
         }
     }
 }
