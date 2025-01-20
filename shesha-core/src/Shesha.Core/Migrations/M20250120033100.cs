ad6b10e98d6f4b39c801df572af3b35e04f83c21
Tshepo-Mahlangu-boxfusion <159532803+Tshepo-Mahlangu-boxfusion@users.noreply.github.com>
2025-01-20 19:08:29 +0200

    chore: rename the property name from CanOtpOut to CanOptOut

---
+++ shesha-core/src/Shesha.Core/Migrations/M20250120033100.cs
@@ -0,0 +1,37 @@
+﻿using EasyNetQ;
+using FluentMigrator;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shesha.Migrations
+{
+    [Migration(20250120033100)]
+    public class M20250120033100 : Migration
+    {
+        public override void Up()
+        {
+            //fix the foreign key constraint
+            IfDatabase("SqlServer").Execute.Sql(@"
+                ALTER TABLE Core_NotificationMessageAttachments 
+                DROP CONSTRAINT FK_Core_NotificationMessageAttachments_MessageId_Core_NotificationMessages_Id;
+
+                ALTER TABLE Core_NotificationMessageAttachments 
+                ADD CONSTRAINT FK_Core_NotificationMessageAttachments_MessageId_Core_NewNotificationMessages_Id
+                FOREIGN KEY (MessageId) REFERENCES Core_NotificationMessages(Id);
+            ");
+
+            //rename the column
+            Rename.Column("Core_CanOtpOut").OnTable("Core_NotificationTypeConfigs").To("Core_CanOptOut");
+        }
+        public override void Down()
+        {
+
+
+        }
+    }
+ 
+}
