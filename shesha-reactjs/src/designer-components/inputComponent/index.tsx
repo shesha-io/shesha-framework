6abcd57c3c44032d781cdac4f3b81118476a3fd6
teboho <tebohomakhalemele@outlook.com>
2025-01-20 11:29:47 +0200

    en: tabbed properties for sizable columns

--- shesha-reactjs/src/designer-components/inputComponent/index.tsx
+++ shesha-reactjs/src/designer-components/inputComponent/index.tsx
@@ -26,6 +26,7 @@ import { QueryBuilder } from '../queryBuilder/queryBuilder';
 import { ColumnsConfig } from '../dataTable/table/columnsEditor/columnsConfig';
 import { DynamicActionsConfigurator } from '../dynamicActionsConfigurator/configurator';
 import ColumnsList from '../columns/columnsList';
+import SizableColumnsList from '../sizableColumns/sizableColumnList';
 
 export const InputComponent: FC<ISettingsInputProps> = (props) => {
     const icons = require('@ant-design/icons');
@@ -166,8 +167,10 @@ export const InputComponent: FC<ISettingsInputProps> = (props) => {
             return <ColumnsConfig size={size} />;
         case 'columnsList':
             return <ColumnsList {...props} readOnly={readOnly} />;
+        case 'sizableColumnsConfig':
+            return <SizableColumnsList {...props} readOnly={readOnly} />;
         case 'editableTagGroupProps':
-            return <EditableTagGroup  value={value} defaultValue={props?.defaultValue} onChange={onChange} readOnly={props.readOnly} />;
+            return <EditableTagGroup value={value} defaultValue={props?.defaultValue} onChange={onChange} readOnly={props.readOnly} />;
         case 'propertyAutocomplete':
             return <PropertyAutocomplete {...props} style={props.style as any} readOnly={readOnly} id="contextPropertyAutocomplete" />;
         case 'contextPropertyAutocomplete':
@@ -180,7 +183,7 @@ export const InputComponent: FC<ISettingsInputProps> = (props) => {
                 onChange={onChange}
             />;
         case 'labelValueEditor': 
-         return <LabelValueEditor {...props} exposedVariables={codeEditorProps.exposedVariables} />;
+            return <LabelValueEditor {...props} exposedVariables={codeEditorProps.exposedVariables} />;
         case 'permissions':
             return <PermissionAutocomplete value={value} readOnly={readOnly} onChange={onChange} size={size} />;
         case 'multiColorPicker':
