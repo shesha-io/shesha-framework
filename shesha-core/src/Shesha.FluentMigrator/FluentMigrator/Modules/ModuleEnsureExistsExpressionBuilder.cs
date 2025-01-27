4bb0964a00a6ec1c2dc3bdcc6fdd35fcfea4abbc
IvanIlyichev <123535566+IvanIlyichev@users.noreply.github.com>
2025-01-27 17:19:45 +0300

    added migrator helper for modules
    (cherry picked from commit 016af502b9d20b649eeaa7bdf9fe23ab035ced9e)

---
+++ shesha-core/src/Shesha.FluentMigrator/FluentMigrator/Modules/ModuleEnsureExistsExpressionBuilder.cs
@@ -0,0 +1,15 @@
+﻿using FluentMigrator.Builders;
+using FluentMigrator.Infrastructure;
+
+namespace Shesha.FluentMigrator.ReferenceLists
+{
+    public class ModuleEnsureExistsExpressionBuilder : ExpressionBuilderBase<ModuleEnsureExistsExpression>, IModuleEnsureExistsExpressionSyntax
+    {
+        private readonly IMigrationContext _context;
+
+        public ModuleEnsureExistsExpressionBuilder(ModuleEnsureExistsExpression expression, IMigrationContext context) : base(expression)
+        {
+            _context = context;
+        }
+    }
+}
