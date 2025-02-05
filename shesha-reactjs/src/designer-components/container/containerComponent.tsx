d023ba5211b47ca39b99f347b1f2d9bc822cc7c8
IvanIlyichev <123535566+IvanIlyichev@users.noreply.github.com>
2025-02-05 12:22:44 +0300

    container component: fixed wrong styles of border and shadow

--- shesha-reactjs/src/designer-components/container/containerComponent.tsx
+++ shesha-reactjs/src/designer-components/container/containerComponent.tsx
@@ -12,7 +12,27 @@ import { migrateFormApi } from '../_common-migrations/migrateFormApi1';
 import { isValidGuid } from '@/components/formDesigner/components/utils';
 import { CSSProperties } from 'styled-components';
 import { toSizeCssProp } from '@/utils/form';
-import { useTheme } from 'antd-style';
+import { removeUndefinedProperties } from '@/utils/array';
+import { ShadowStyleType } from './interfaces';
+
+const basicShadow: CSSProperties = {
+  boxShadow: "0px 2px 4px 0px rgba(0,0,0,.15)"
+};
+
+const invertedShadow: CSSProperties = {
+  boxShadow: "0px -2px 4px 0px rgba(0,0,0,.15)"
+};
+
+const getShadowStyle = (shadowType?: ShadowStyleType): object=> {
+  switch (shadowType) {
+    case 'below':
+      return basicShadow;
+    case 'above':
+      return invertedShadow;
+    default:
+      return undefined;
+  }
+};
 
 const ContainerComponent: IToolboxComponent<IContainerComponentProps> = {
   type: 'container',
@@ -23,13 +43,12 @@ const ContainerComponent: IToolboxComponent<IContainerComponentProps> = {
     const { data: formData } = useFormData();
     const { globalState } = useGlobalState();
     const { backendUrl, httpHeaders } = useSheshaApplication();
-    const [ fileUrl, setFileUrl] = useState(null);
-    const theme = useTheme();
+    const [fileUrl, setFileUrl] = useState(null);
 
     useEffect(() => {
       if (model.backgroundDataSource === 'storedFileId' && model.backgroundStoredFileId) {
         fetch(`${backendUrl}/api/StoredFile/Download?id=${model.backgroundStoredFileId}`,
-          {headers: {...httpHeaders, "Content-Type": "application/octet-stream"}})
+          { headers: { ...httpHeaders, "Content-Type": "application/octet-stream" } })
           .then((response) => {
             return response.blob();
           })
@@ -46,26 +65,26 @@ const ContainerComponent: IToolboxComponent<IContainerComponentProps> = {
     if (model.hidden) return null;
 
     const flexAndGridStyles: ICommonContainerProps = {
-      display: model?.display,
-      flexDirection: model?.flexDirection,
-      direction: model?.direction,
-      justifyContent: model?.justifyContent,
-      alignItems: model?.alignItems,
-      alignSelf: model?.alignSelf,
-      justifyItems: model?.justifyItems,
-      textJustify: model?.textJustify,
-      justifySelf: model?.justifySelf,
-      noDefaultStyling: model?.noDefaultStyling,
-      gridColumnsCount: model?.gridColumnsCount,
-      flexWrap: model?.flexWrap,
-      gap: model?.gap,
+      display: model.display,
+      flexDirection: model.flexDirection,
+      direction: model.direction,
+      justifyContent: model.justifyContent,
+      alignItems: model.alignItems,
+      alignSelf: model.alignSelf,
+      justifyItems: model.justifyItems,
+      textJustify: model.textJustify,
+      justifySelf: model.justifySelf,
+      noDefaultStyling: model.noDefaultStyling,
+      gridColumnsCount: model.gridColumnsCount,
+      flexWrap: model.flexWrap,
+      gap: model.gap,
     };
 
     const widthStyles: CSSProperties = {
       width: toSizeCssProp(model.width),
       minWidth: toSizeCssProp(model.minWidth),
       maxWidth: toSizeCssProp(model.maxWidth),
-      overflow: model?.overflow,
+      overflow: model.overflow,
     };
 
     const heightStyles: CSSProperties = {
@@ -75,9 +94,9 @@ const ContainerComponent: IToolboxComponent<IContainerComponentProps> = {
     };
 
     const borderStyles: CSSProperties = {
-      borderWidth: toSizeCssProp(model.borderWidth) || 0,
-      borderColor: model.borderColor || theme.colorBorder,
-      borderStyle: model.borderStyle || 'solid',
+      borderWidth: toSizeCssProp(model.borderWidth),
+      borderColor: model.borderColor,
+      borderStyle: model.borderStyle,
       borderRadius: toSizeCssProp(model.borderRadius),
     };
 
@@ -89,21 +108,22 @@ const ContainerComponent: IToolboxComponent<IContainerComponentProps> = {
           ? model.backgroundUrl
           : '';
 
-    const backgroundStyles: CSSProperties = model?.backgroundType === 'image' && val
-      ? { backgroundImage: `url(${val})`, backgroundSize: model?.backgroundCover, backgroundRepeat: model?.backgroundRepeat }
-      : model?.backgroundType === 'color'
+    const backgroundStyles: CSSProperties = model.backgroundType === 'image' && val
+      ? { backgroundImage: `url(${val})`, backgroundSize: model.backgroundCover, backgroundRepeat: model.backgroundRepeat }
+      : model.backgroundType === 'color'
         ? { background: model?.backgroundColor }
         : {};
 
-    const basicShadow: CSSProperties = {
-      boxShadow: "0px 2px 4px 0px rgba(0,0,0,.15)"
-    };
+    const renderShadow = getShadowStyle(model.shadowStyle);
 
-    const invertedShadow: CSSProperties = {
-      boxShadow: "0px -2px 4px 0px rgba(0,0,0,.15)"
-    };
-
-    const renderShadow = model?.shadowStyle !== 'none' ? model?.shadowStyle === 'below' ? basicShadow : invertedShadow : '';
+    const finalStyle = removeUndefinedProperties({
+      ...widthStyles,
+      ...heightStyles,
+      ...borderStyles,
+      ...backgroundStyles,
+      ...renderShadow,
+      ...getStyle(model?.style, formData)
+    });
 
     return (
       <ParentProvider model={model}>
@@ -111,14 +131,7 @@ const ContainerComponent: IToolboxComponent<IContainerComponentProps> = {
           containerId={model.id}
           className={model.className}
           wrapperStyle={getLayoutStyle({ ...model, style: model?.wrapperStyle }, { data: formData, globalState })}
-          style={{
-            ...widthStyles,
-            ...heightStyles,
-            ...borderStyles,
-            ...backgroundStyles,
-            ...renderShadow,
-            ...getStyle(model?.style, formData)
-          }}
+          style={finalStyle}
           dynamicComponents={model?.isDynamic ? model?.components : []}
           {...flexAndGridStyles}
         />
@@ -141,30 +154,30 @@ const ContainerComponent: IToolboxComponent<IContainerComponentProps> = {
       .add<IContainerComponentProps>(2, (prev) => migrateVisibility(prev))
       .add<IContainerComponentProps>(3, (prev) => ({ ...migrateFormApi.properties(prev) }))
       .add<IContainerComponentProps>(4, (prev) => (
-        { 
+        {
           ...prev,
           backgroundDataSource: prev.backgroundDataSource ?? prev['dataSource'],
           backgroundBase64: prev.backgroundBase64 ?? prev['base64'],
           backgroundStoredFileId: prev.backgroundStoredFileId ?? prev['storedFileId'],
         }))
-        .add<IContainerComponentProps>(5, (prev) => {
-          const styles = {
-            style: prev.style,
-            wrapperStyle: prev.wrapperStyle,
-            className: prev.className,
-            stylingBox: prev.stylingBox,
-            width: prev.width,
-            height: prev.height,
-            minWidth: prev.minWidth,
-            minHeight: prev.minHeight,
-            maxHeight: prev.maxHeight,
-            maxWidth: prev.maxWidth
-          };
-          return { ...prev, desktop: {...styles}, tablet: {...styles}, mobile: {...styles} };
-        })
-        .add<IContainerComponentProps>(6, (prev) => {
-          return {...prev, shadowStyle: 'none'};
-        })
+      .add<IContainerComponentProps>(5, (prev) => {
+        const styles = {
+          style: prev.style,
+          wrapperStyle: prev.wrapperStyle,
+          className: prev.className,
+          stylingBox: prev.stylingBox,
+          width: prev.width,
+          height: prev.height,
+          minWidth: prev.minWidth,
+          minHeight: prev.minHeight,
+          maxHeight: prev.maxHeight,
+          maxWidth: prev.maxWidth
+        };
+        return { ...prev, desktop: { ...styles }, tablet: { ...styles }, mobile: { ...styles } };
+      })
+      .add<IContainerComponentProps>(6, (prev) => {
+        return { ...prev, shadowStyle: 'none' };
+      })
   ,
 };
 
