70d0b5531173e382ff3c88a75d8d13d899fd8839
teboho <13440702+teboho@users.noreply.github.com>
2025-02-21 17:07:09 +0200

    Update settingsForm.ts

--- shesha-reactjs/src/designer-components/entityReference/settingsForm.ts
+++ shesha-reactjs/src/designer-components/entityReference/settingsForm.ts
@@ -347,7 +347,7 @@ export const getSettings = (data: IEntityReferenceControlProps) => {
                   hidden: false,
                   dataSourceType: 'url',
                   validate: { required: true },
-                  dataSourceUrl: '/api/services/app/Metadata/EntityTypeAutocomplete',
+                  dataSourceUrl: '/api/services/app/Api/Endpoints',
                   settingsValidationErrors: [],
                   useRawValues: true,
                   width: '100%'
