cce4b2ce3bdfc66f7d640556a5c6280c54739a97
James Baloyi <james_baloyi@yahoo.com>
2025-03-01 10:03:16 +0200

    Revert "Properties panel code editor"

--- shesha-reactjs/src/designer-components/container/settingsForm.ts
+++ shesha-reactjs/src/designer-components/container/settingsForm.ts
@@ -110,7 +110,6 @@ export const getSettings = (data) => {
                             label: 'Position',
                             parentId: 'positionCollapsiblePanel',
                             inputType: 'dropdown',
-                            defaultValue: 'relative',
                             description: 'The position CSS property sets how an element is positioned in a document. The top, right, bottom, and left properties determine the final location of positioned elements.',
                             validate: {
                               required: true,
