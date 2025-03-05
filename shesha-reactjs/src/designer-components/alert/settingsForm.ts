41863cf1cf0dcf8354f1a5eb5b0f6fbd59c8da60
teboho <tebohomakhalemele@outlook.com>
2025-03-05 23:07:23 +0200

    minor change rm space

--- shesha-reactjs/src/designer-components/alert/settingsForm.ts
+++ shesha-reactjs/src/designer-components/alert/settingsForm.ts
@@ -208,7 +208,6 @@ export const getSettings = (data: IAlertComponentProps) => {
                     ]
                   }
                 })
-
                 .toJson()
             ]
           },
