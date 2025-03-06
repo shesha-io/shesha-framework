018cc8808f7e4a1afb782d476781b130de81b6bc
teboho <tebohomakhalemele@outlook.com>
2025-03-06 14:15:15 +0200

    show lable for style

--- shesha-reactjs/src/designer-components/textArea/settingsForm.ts
+++ shesha-reactjs/src/designer-components/textArea/settingsForm.ts
@@ -797,7 +797,7 @@ export const getSettings = (data: any) => {
                               id: nanoid(),
                               inputType: 'codeEditor',
                               propertyName: 'style',
-                              hideLabel: true,
+                              hideLabel: false,
                               label: 'Style',
                               description: 'A script that returns the style of the element as an object. This should conform to CSSProperties',
                             })
