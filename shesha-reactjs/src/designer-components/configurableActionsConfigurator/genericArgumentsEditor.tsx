c8c6a14818eaeeabe8ddc123efd11bd29eec977c
teboho <tebohomakhalemele@outlook.com>
2025-03-18 12:14:29 +0200

    build fix

--- shesha-reactjs/src/designer-components/configurableActionsConfigurator/genericArgumentsEditor.tsx
+++ shesha-reactjs/src/designer-components/configurableActionsConfigurator/genericArgumentsEditor.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { ConfigurableForm } from '@/components';
 import { FormMarkup } from '@/providers/form/models';
 import { IConfigurableActionArguments } from '@/interfaces/configurableAction';
