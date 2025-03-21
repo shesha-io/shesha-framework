43ef4a6f7892806bae2a2c36a8a52ec2bebca096
IvanIlyichev <ivan.Ilyichev@shesha.io>
2025-03-21 11:20:08 +0300

    changed StringHelper.IsValidEmail() to use EmailAddressAttribute

--- shesha-core/src/Shesha.Framework/Utilities/StringHelper.cs
+++ shesha-core/src/Shesha.Framework/Utilities/StringHelper.cs
@@ -3,6 +3,7 @@ using HtmlAgilityPack;
 using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
+using System.ComponentModel.DataAnnotations;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
@@ -873,14 +874,8 @@ namespace Shesha.Utilities
             if (string.IsNullOrWhiteSpace(inputEmail))
                 return false;
 
-            string strRegex = @"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}" +
-                              @"\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\" +
-                              @".)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$";
-            Regex re = new Regex(strRegex);
-            if (re.IsMatch(inputEmail))
-                return (true);
-            else
-                return (false);
+            var emailAttribute = new EmailAddressAttribute();
+            return emailAttribute.IsValid(inputEmail);
         }
 
         [DebuggerStepThrough]
