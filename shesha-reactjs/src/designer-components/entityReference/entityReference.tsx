27f84fc87b484a348fc148336329715387b76dd0
teboho <tebohomakhalemele@outlook.com>
2025-03-26 23:32:24 +0200

    ensure the stylingbox is read

--- shesha-reactjs/src/designer-components/entityReference/entityReference.tsx
+++ shesha-reactjs/src/designer-components/entityReference/entityReference.tsx
@@ -4,7 +4,11 @@ import ConfigurableFormItem from '@/components/formDesigner/components/formItem'
 import { LinkExternalOutlined } from '@/icons/linkExternalOutlined';
 import { IToolboxComponent } from '@/interfaces';
 import { IConfigurableFormComponent } from '@/providers/form/models';
-import { migratePropertyName, migrateCustomFunctions, migrateReadOnly } from '@/designer-components/_common-migrations/migrateSettings';
+import {
+  migratePropertyName,
+  migrateCustomFunctions,
+  migrateReadOnly,
+} from '@/designer-components/_common-migrations/migrateSettings';
 import { isEntityReferencePropertyMetadata } from '@/interfaces/metadata';
 import { migrateVisibility } from '@/designer-components/_common-migrations/migrateVisibility';
 import { migrateNavigateAction } from '../_common-migrations/migrate-navigate-action';
@@ -12,6 +16,8 @@ import { migrateFormApi } from '../_common-migrations/migrateFormApi1';
 import { getSettings } from './settingsForm';
 import { validateConfigurableComponentSettings } from '@/formDesignerUtils';
 import { migratePrevStyles } from '../_common-migrations/migrateStyles';
+import { removeUndefinedProps } from '@/utils/object';
+import { getStyle, pickStyleFromModel } from '@/index';
 
 export type IActionParameters = [{ key: string; value: string }];
 
@@ -26,47 +32,62 @@ const EntityReferenceComponent: IToolboxComponent<IEntityReferenceControlProps> 
   Factory: ({ model: passedModel }) => {
     const { style, hidden, readOnly, ...model } = passedModel;
 
-    if (hidden)
-      return null;
+    if (hidden) return null;
+
+    const jsStyle = getStyle(passedModel.style, passedModel);
+
+    const styling = JSON.parse(model.stylingBox || '{}');
+    const stylingBoxAsCSS = pickStyleFromModel(styling);
+    const additionalStyles = JSON.stringify(
+      removeUndefinedProps({
+        ...jsStyle,
+        ...stylingBoxAsCSS,
+      })
+    );
+    const additionalStylesWithoutQuotes = additionalStyles
+      .replace(/"([^"]+)":/g, '$1:') // Remove quotes from keys
+      .replace(/'([^']+)':/g, '$1:'); // Remove quotes from keys
+    const finalStyle = `return ${additionalStylesWithoutQuotes}`;
 
     return (
       <ConfigurableFormItem model={model}>
         {(value) => {
-          return <EntityReference {...model} value={value} style={style} />;
+          return <EntityReference {...model} value={value} style={finalStyle} />;
         }}
       </ConfigurableFormItem>
     );
   },
   settingsFormMarkup: (data) => getSettings(data),
   validateSettings: (model) => validateConfigurableComponentSettings(getSettings(model), model),
-  migrator: m => m
-    .add<IEntityReferenceControlProps>(0, prev => {
-      return {
+  migrator: (m) =>
+    m
+      .add<IEntityReferenceControlProps>(0, (prev) => {
+        return {
+          ...prev,
+          formSelectionMode: 'name',
+          entityReferenceType: 'Quickview',
+          quickviewWidth: 600,
+          displayProperty: '',
+          handleFail: false,
+          handleSuccess: false,
+          style: prev.style,
+          stylingBox: prev.stylingBox,
+        };
+      })
+      .add<IEntityReferenceControlProps>(1, (prev) => migratePropertyName(migrateCustomFunctions(prev)))
+      .add<IEntityReferenceControlProps>(2, (prev) => migrateVisibility(prev))
+      .add<IEntityReferenceControlProps>(3, (prev) => ({
         ...prev,
-        formSelectionMode: 'name',
-        entityReferenceType: 'Quickview',
-        quickviewWidth: 600,
-        displayProperty: '',
-        handleFail: false,
-        handleSuccess: false
-      };
-    })
-    .add<IEntityReferenceControlProps>(1, (prev) => migratePropertyName(migrateCustomFunctions(prev)))
-    .add<IEntityReferenceControlProps>(2, (prev) => migrateVisibility(prev))
-    .add<IEntityReferenceControlProps>(3, (prev) => ({
-      ...prev,
-      onSuccess: migrateNavigateAction(prev.onSuccess),
-      onFail: migrateNavigateAction(prev.onFail),
-    }))
-    .add<IEntityReferenceControlProps>(4, (prev) => migrateReadOnly(prev, 'editable'))
-    .add<IEntityReferenceControlProps>(5, (prev, context) => ({
-      ...prev,
-      footerButtons: context.isNew
-        ? 'default'
-        : prev.footerButtons ?? prev.showModalFooter ? 'default' : 'none',
-    }))
-    .add<IEntityReferenceControlProps>(6, (prev) => ({ ...migrateFormApi.eventsAndProperties(prev) }))
-    .add<IEntityReferenceControlProps>(7, (prev) => ({ ...migratePrevStyles(prev) })),
+        onSuccess: migrateNavigateAction(prev.onSuccess),
+        onFail: migrateNavigateAction(prev.onFail),
+      }))
+      .add<IEntityReferenceControlProps>(4, (prev) => migrateReadOnly(prev, 'editable'))
+      .add<IEntityReferenceControlProps>(5, (prev, context) => ({
+        ...prev,
+        footerButtons: context.isNew ? 'default' : (prev.footerButtons ?? prev.showModalFooter) ? 'default' : 'none',
+      }))
+      .add<IEntityReferenceControlProps>(6, (prev) => ({ ...migrateFormApi.eventsAndProperties(prev) }))
+      .add<IEntityReferenceControlProps>(7, (prev) => ({ ...migratePrevStyles(prev) })),
   linkToModelMetadata: (model, propMetadata): IEntityReferenceControlProps => {
     return {
       ...model,
