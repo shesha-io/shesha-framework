73833cbb8ac188e132bdaa24113ad4184ed53537
Thulasizwe Nkambule <tnkambule980115@gmail.com>
2025-04-16 11:28:35 +0200

    fix: addressed issues #3144 and #3152

--- shesha-reactjs/src/components/dataTable/sortingConfigurator/index.tsx
+++ shesha-reactjs/src/components/dataTable/sortingConfigurator/index.tsx
@@ -1,67 +1,59 @@
 import { Space, Select } from 'antd';
 import { ListEditor, PropertyAutocomplete } from '@/components/index';
 import { ColumnSorting, GroupingItem as SortingItem } from '@/providers/dataTable/interfaces';
-import { MetadataProvider } from '@/providers/index';
 import React, { FC } from 'react';
 import { getNanoId } from '@/utils/uuid';
-import ConditionalWrap from '@/components/conditionalWrapper';
 
 const { Option } = Select;
 
 export interface ISortingEditorProps {
     value?: SortingItem[];
     onChange: (newValue?: SortingItem[]) => void;
-    modelType?: string;
     readOnly?: boolean;
     maxItemsCount?: number;
 }
 
 export const SortingEditor: FC<ISortingEditorProps> = (props) => {
-    const { value, onChange, modelType, readOnly: editorReadOnly, maxItemsCount } = props;
+    const { value, onChange, readOnly: editorReadOnly, maxItemsCount } = props;
     return (
-        <ConditionalWrap
-            condition={Boolean(modelType)}
-            wrap={content => <MetadataProvider modelType={modelType}>{content}</MetadataProvider>}
+        <ListEditor<SortingItem>
+            value={value}
+            onChange={onChange}
+            initNewItem={(_items) => ({ id: getNanoId(), propertyName: '', sorting: 'asc' })}
+            readOnly={editorReadOnly}
+            maxItemsCount={maxItemsCount}
         >
-            <ListEditor<SortingItem>
-                value={value}
-                onChange={onChange}
-                initNewItem={(_items) => ({ id: getNanoId(), propertyName: '', sorting: 'asc' })}
-                readOnly={editorReadOnly}
-                maxItemsCount={maxItemsCount}
-            >
-                {({ item, itemOnChange, readOnly }) => {
-                    return (
-                        <div>
-                            <Space.Compact style={{ width: '100%' }}>
-                                <PropertyAutocomplete
-                                    style={{ width: 'calc(100% - 120px)' }}
-                                    mode='single'
-                                    value={item.propertyName}
-                                    onChange={(value) => {
-                                        if (!Array.isArray(value))
-                                            itemOnChange({ ...item, propertyName: value }, undefined);
-                                    }}
-                                    autoFillProps={false}
-                                    size='small'
-                                    readOnly={readOnly}
-                                />
-                                <Select<ColumnSorting>
-                                    value={item.sorting}
-                                    onChange={(value) => {
-                                        itemOnChange({ ...item, sorting: value }, undefined);
-                                    }}
-                                    style={{ width: '120px' }}
-                                    size='small'
-                                    disabled={readOnly}
-                                >
-                                    <Option value="asc">Ascending</Option>
-                                    <Option value="desc">Descending</Option>
-                                </Select>
-                            </Space.Compact>
-                        </div>);
-                }}
-            </ListEditor>
-        </ConditionalWrap>
+            {({ item, itemOnChange, readOnly }) => {
+                return (
+                    <div>
+                        <Space.Compact style={{ width: '100%' }}>
+                            <PropertyAutocomplete
+                                style={{ width: 'calc(100% - 120px)' }}
+                                mode='single'
+                                value={item.propertyName}
+                                onChange={(value) => {
+                                    if (!Array.isArray(value))
+                                        itemOnChange({ ...item, propertyName: value }, undefined);
+                                }}
+                                autoFillProps={false}
+                                size='small'
+                                readOnly={readOnly}
+                            />
+                            <Select<ColumnSorting>
+                                value={item.sorting}
+                                onChange={(value) => {
+                                    itemOnChange({ ...item, sorting: value }, undefined);
+                                }}
+                                style={{ width: '120px' }}
+                                size='small'
+                                disabled={readOnly}
+                            >
+                                <Option value="asc">Ascending</Option>
+                                <Option value="desc">Descending</Option>
+                            </Select>
+                        </Space.Compact>
+                    </div>);
+            }}
+        </ListEditor>
     );
 };
