b039bbbd9d8117ebd3124c9be1332c01a860ec49
Tshepo <tshepo1103@gmail.com>
2025-05-06 13:16:38 +0200

    chore: remove the default color so that it uses primary color

--- shesha-reactjs/src/designer-components/entityReference/utils.ts
+++ shesha-reactjs/src/designer-components/entityReference/utils.ts
@@ -3,7 +3,7 @@ import { IStyleType } from "@/index";
 export const defaultStyles = (): IStyleType => {
     return {
         background: { type: 'color', color: '#fff' },
-        font: { weight: '400', size: 14, color: '#000', type: 'Segoe UI' },
+        font: { weight: '400', size: 14, type: 'Segoe UI' },
         border: {
             border: {
                 all: { width: '1px', style: 'none', color: '#d9d9d9' },
