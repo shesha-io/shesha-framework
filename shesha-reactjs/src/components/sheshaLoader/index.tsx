cff4216da6e68eee3198bd897415b46b6d473db5
Teboho <tebohomakhalemele@outlook.com>
2025-05-14 20:53:19 +0200

    update the loader

--- shesha-reactjs/src/components/sheshaLoader/index.tsx
+++ shesha-reactjs/src/components/sheshaLoader/index.tsx
@@ -1,14 +1,18 @@
-import { Flex } from 'antd';
+import { Flex, Typography } from 'antd';
 import React, { FC } from 'react';
+import Image from 'next/image';
+import './styles.css';
+
+const { Title } = Typography;
 
 interface ISheshaLoader {
   message?: string;
 }
 
-const SheshaLoader: FC<ISheshaLoader> = ({ message = 'Initializing...' }) => (
+const SheshaLoader: FC<ISheshaLoader> = ({ message = 'Initializing' }) => (
   <Flex vertical justify="center" align="center" style={{ height: '100vh' }}>
-    <img src={`/images/SheshaLoadingAnimation.gif`} alt="Shesha Loading Animation" />
-    <div>{message}</div>
+    <Image src={`/images/SheshaLoadingAnimation.gif`} alt="Shesha Loading Animation" width={200} height={200} />
+    <Title className="loading" level={3}>{message}</Title>
   </Flex>
 );
 
