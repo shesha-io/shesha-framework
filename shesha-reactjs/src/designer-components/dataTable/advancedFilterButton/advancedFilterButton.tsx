49d1fad565e1a78b147463c4eb3dd921d3ca103f
Tshepo <tshepo1103@gmail.com>
2025-05-16 16:48:13 +0200

    chore: fix the formating

--- shesha-reactjs/src/designer-components/dataTable/advancedFilterButton/advancedFilterButton.tsx
+++ shesha-reactjs/src/designer-components/dataTable/advancedFilterButton/advancedFilterButton.tsx
@@ -29,8 +29,8 @@ export const AdvancedFilterButton: FC<IButtonComponentProps> = (props) => {
   const buttonStyle = {
     ...localStyle,
     ...{ color: props.buttonType !== 'primary' && !props.danger ? styles.primaryColor : '' },
-    padding: "3px",
-    border: props.buttonType === 'link' ? 'none' : (hasFilters ? `1px solid ${styles.primaryColor}` : 'none'),
+    padding: '3px',
+    border: props.buttonType === 'link' ? 'none' : hasFilters ? `1px solid ${styles.primaryColor}` : 'none',
     ...props.styles,
   };
 
@@ -81,12 +81,15 @@ export const AdvancedFilterButton: FC<IButtonComponentProps> = (props) => {
           disabled={props.readOnly || isFiltering}
           icon={filterIcon}
           size={props.size}
-          style={isFiltering || props.readOnly ? { ...buttonStyle, opacity: 0.5, border: props.buttonType === 'link' ? 'none' : buttonStyle.border } : { ...buttonStyle }}
+          style={
+            isFiltering || props.readOnly
+              ? { ...buttonStyle, opacity: 0.5, border: props.buttonType === 'link' ? 'none' : buttonStyle.border }
+              : { ...buttonStyle }
+          }
         >
           {props.label}
         </Button>
       </Badge>
     </span>
-
   );
 };
