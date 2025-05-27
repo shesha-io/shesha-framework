b9ad2537820baf8196b4d90720e3e3c561da7bde
Tshepo <tshepo1103@gmail.com>
2025-05-27 15:59:08 +0200

    chore: changing the notes to use the category

--- shesha-reactjs/src/designer-components/notes/notesComponent.tsx
+++ shesha-reactjs/src/designer-components/notes/notesComponent.tsx
@@ -19,6 +19,8 @@ export interface INotesProps extends IConfigurableFormComponent {
   savePlacement?: 'left' | 'right';
   autoSize?: boolean;
   allowDelete?: boolean;
+  onCreated?: string; 
+  category?: string;
 }
 
 const settingsForm = settingsFormJson as FormMarkup;
@@ -37,7 +39,7 @@ const NotesComponent: IToolboxComponent<INotesProps> = {
     if (model.hidden) return null;
 
     return (
-      <NotesProvider ownerId={ownerId} ownerType={model.ownerType} uniqueIdentifier={model.componentName}>
+      <NotesProvider ownerId={ownerId} ownerType={model?.ownerType} category={model?.category}>
         <NotesRenderer
           showCommentBox={!model.readOnly}
           buttonPostion={model?.savePlacement}
