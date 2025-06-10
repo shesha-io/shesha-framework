720c956bb1ce9babf6456ebfff754991df12fce5
James Baloyi <james_baloyi@yahoo.com>
2025-06-10 17:15:35 +0200

    update settings file

--- shesha-reactjs/src/designer-components/dataTable/table/tableSettings.ts
+++ shesha-reactjs/src/designer-components/dataTable/table/tableSettings.ts
@@ -160,6 +160,7 @@ export const getSettings = (data: ITableComponentProps) => {
                                     propertyName: 'canEditInline',
                                     label: 'Can Edit Inline',
                                     inputType: 'dropdown',
+                                    jsSetting: true,
                                     parentId: crudTabId,
                                     dropdownOptions: [
                                         { value: 'yes', label: 'Yes' },
@@ -227,7 +228,7 @@ export const getSettings = (data: ITableComponentProps) => {
                                             id: nanoid(),
                                             propertyName: 'customUpdateUrl',
                                             label: 'Custom Update URL',
-                                            type: 'textField',
+                                            type: 'endpointsAutocomplete',
                                             parentId: crudTabId,
                                         }
                                     ]
@@ -241,6 +242,7 @@ export const getSettings = (data: ITableComponentProps) => {
                                             propertyName: 'canAddInline',
                                             label: 'Can Add Inline',
                                             type: 'dropdown',
+                                            jsSetting: true,
                                             parentId: crudTabId,
                                             dropdownOptions: [
                                                 { value: 'yes', label: 'Yes' },
@@ -309,7 +311,7 @@ export const getSettings = (data: ITableComponentProps) => {
                                             id: nanoid(),
                                             propertyName: 'customCreateUrl',
                                             label: 'Custom Create URL',
-                                            type: 'textField',
+                                            type: 'endpointsAutocomplete',
                                             parentId: crudTabId,
                                         }
                                     ]
@@ -348,6 +350,7 @@ export const getSettings = (data: ITableComponentProps) => {
                                     label: 'Can Delete Inline',
                                     inputType: 'dropdown',
                                     parentId: crudTabId,
+                                    jsSetting: true,
                                     dropdownOptions: [
                                         { value: 'yes', label: 'Yes' },
                                         { value: 'no', label: 'No' },
@@ -380,7 +383,7 @@ export const getSettings = (data: ITableComponentProps) => {
                                             id: nanoid(),
                                             propertyName: 'customDeleteUrl',
                                             label: 'Custom Delete URL',
-                                            type: 'textField',
+                                            type: 'endpointsAutocomplete',
                                             parentId: crudTabId,
                                         }
                                     ]
