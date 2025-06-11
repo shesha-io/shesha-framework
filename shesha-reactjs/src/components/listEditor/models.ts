98dddca2b413bb11d390d79fadf117827a58a439
Teboho <tebohomakhalemele@outlook.com>
2025-06-11 10:12:55 +0200

    ListEditorWithPropertiesPanel real time (preview) changes unified algo update

--- shesha-reactjs/src/components/listEditor/models.ts
+++ shesha-reactjs/src/components/listEditor/models.ts
@@ -3,10 +3,12 @@ import { ItemInterface } from "react-sortablejs";
 export type ListMode = 'create' | 'read' | 'update';
 
 export interface ListItem {
-    
+    itemType?: 'item' | 'group';
+    itemSubType?: 'button' | 'item' | 'divider';
 }
 
 export interface ListItemWithId extends ListItem {
+    childItems?: ListItemWithId[];
     id: string;
 }
 
