a9b27b571453b3db5aad0873fc504f4a02977adb
Alex Stephens <alex.stephens@boxfusion.io>
2025-06-26 15:02:11 +0300

    Form settings update #3084 (import)

--- shesha-reactjs/src/providers/configurationItemsLoader/index.tsx
+++ shesha-reactjs/src/providers/configurationItemsLoader/index.tsx
@@ -133,9 +133,11 @@ const ConfigurationItemsLoaderProvider: FC<PropsWithChildren<IConfigurationItems
       settings: markupWithSettings?.formSettings,
     };
 
-    const access = (result.settings.access ?? dto.access) as any; // there can be string for some old forms
-    result.settings.access = ((access && typeof(access) === 'string') ? parseInt(access, 10) : access) ?? 3;
-    result.settings.permissions = result.settings.permissions ?? dto.permissions ?? [];
+    if (result.settings) {
+      const access = (result.settings.access ?? dto?.access) as any; // there can be string for some old forms
+      result.settings.access = ((access && typeof(access) === 'string') ? parseInt(access, 10) : access) ?? 3;
+      result.settings.permissions = result.settings.permissions ?? dto?.permissions ?? [];
+    }
 
     return result;
   };
