f45450564630c4191bf4414d9be0bd9a94b1452f
Thulasizwe Nkambule <tnkambule980115@gmail.com>
2025-06-26 07:46:57 +0200

    fix: selected listeditor item

--- shesha-reactjs/src/components/listEditor/models.ts
+++ shesha-reactjs/src/components/listEditor/models.ts
@@ -3,6 +3,7 @@ import { ItemInterface } from "react-sortablejs";
 export type ListMode = 'create' | 'read' | 'update';
 
 export interface ListItem {
+    id: string;
     itemType?: 'item' | 'group';
     itemSubType?: 'button' | 'item' | 'divider' | 'dynamic' | 'separator' | 'line';
 }
