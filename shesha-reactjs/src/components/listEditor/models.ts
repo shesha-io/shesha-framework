4d48a069cf3d2d011aa0eb576fbfaef6d48a6a0a
Thulasizwe Nkambule <tnkambule980115@gmail.com>
2025-06-26 09:02:34 +0200

    fix: optional id

--- shesha-reactjs/src/components/listEditor/models.ts
+++ shesha-reactjs/src/components/listEditor/models.ts
@@ -3,7 +3,7 @@ import { ItemInterface } from "react-sortablejs";
 export type ListMode = 'create' | 'read' | 'update';
 
 export interface ListItem {
-    id: string;
+    id?: string;
     itemType?: 'item' | 'group';
     itemSubType?: 'button' | 'item' | 'divider' | 'dynamic' | 'separator' | 'line';
 }
