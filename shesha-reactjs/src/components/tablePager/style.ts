c9c649f8b8166bfeaea71275c80534d1a37a781a
Thulasizwe Nkambule <tnkambule980115@gmail.com>
2025-06-30 16:13:03 +0200

    fix: pagination dropdown

--- shesha-reactjs/src/components/tablePager/style.ts
+++ shesha-reactjs/src/components/tablePager/style.ts
@@ -24,23 +24,40 @@ export const useStyles = createStyles(({ css, cx }, { style }) => {
             .ant-pagination-next button {
             font-size: ${style?.fontSize} !important;
             }
+        }
+    `);
 
-            .ant-select {
-             --ant-color-text : ${style?.color} !important;
+    const dropdown = cx("sha-dropdown", css`
+        
+        // height: calc(${style?.fontSize} * 1.5);
+
+        .ant-select-selection-item {
+         height: calc(${style?.fontSize} * 1.5);
+         display: flex;
+         align-items: center;
+        }
+
+        * {
+            --ant-color-text : ${style?.color} !important;
             --ant-font-size : ${style?.fontSize} !important;
-            --ant-font-family : ${style?.fontFamily} !important; 
-                height: calc(${style?.fontSize} * 1.5);
+            --ant-font-family : ${style?.fontFamily} !important;
+            font-weight: ${style?.fontWeight} !important;
+            font-family: ${style?.fontFamily} !important;
+        }
+    `);
 
-                .ant-select-selection-item {
-                    height: calc(${style?.fontSize} * 1.5);
-                    line-height: calc(${style?.fontSize} * 1.5) !important;
-                    font-weight: ${style?.fontWeight} !important;
-                }
-            }
+    const popup = cx("sha-popup", css`
+        .ant-select-item-option-content {
+            font-size: ${style?.fontSize} !important;
+            font-family: ${style?.fontFamily} !important;
+            color: ${style?.color} !important;
+            font-weight: ${style?.fontWeight} !important;
         }
     `);
 
     return {
         pager,
+        dropdown,
+        popup,
     };
 });
