0921a7b1e50e42b432a2c535ef7c51fc2c73c4b4
James Baloyi <james_baloyi@yahoo.com>
2025-07-28 11:26:40 +0200

    on row save logic

--- shesha-reactjs/src/designer-components/dataTable/table/tableSettings.ts
+++ shesha-reactjs/src/designer-components/dataTable/table/tableSettings.ts
@@ -331,6 +331,7 @@ export const getSettings = (data: ITableComponentProps) => {
                                         }
                                     ]
                                 })
+
                                 .addSettingsInputRow({
                                     id: nanoid(),
                                     hidden: { _code: 'return getSettingValue(data?.canAddInline) === "no";', _mode: 'code', _value: false } as any,
@@ -347,16 +348,21 @@ export const getSettings = (data: ITableComponentProps) => {
                                         }
                                     ]
                                 })
-                                .addSettingsInput({
+                                .addSettingsInputRow({
                                     id: nanoid(),
-                                    propertyName: 'onRowSave',
-                                    label: 'On Row Save',
-                                    inputType: 'codeEditor',
-                                    parentId: crudTabId,
-                                    tooltip: 'Custom business logic to be executed on saving of new/updated row (e.g. custom validation / calculations). This handler should return an object or a Promise<object>.',
                                     hidden: { _code: 'return getSettingValue(data?.canAddInline) === "no" && getSettingValue(data?.canEditInline) === "no";', _mode: 'code', _value: false } as any,
-                                    description: 'Allows custom business logic to be executed on saving of new/updated row (e.g. custom validation / calculations).',
-                                    exposedVariables: ROW_SAVE_EXPOSED_VARIABLES,
+                                    inputs: [
+                                        {
+                                            id: nanoid(),
+                                            propertyName: 'onNewRowSave',
+                                            label: 'On Row Save',
+                                            type: 'codeEditor',
+                                            parentId: crudTabId,
+                                            tooltip: 'Custom business logic to be executed on saving of new/updated row (e.g. custom validation / calculations). This handler should return an object or a Promise<object>.',
+                                            description: 'Allows custom business logic to be executed on saving of new/updated row (e.g. custom validation / calculations).',
+                                            exposedVariables: NEW_ROW_EXPOSED_VARIABLES,
+                                        }
+                                    ]
                                 })
                                 .addSettingsInput({
                                     id: nanoid(),
