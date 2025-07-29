e00260bb4cd8fff6e0b9b04a23b5be010318b875
HackGenesis <derik.sadiki@boxfusion.io>
2025-07-29 14:54:17 +0200

    add gap on grouping

--- shesha-reactjs/src/components/dataList/index.tsx
+++ shesha-reactjs/src/components/dataList/index.tsx
@@ -436,7 +436,10 @@ export const DataList: FC<Partial<IDataListProps>> = ({
       ...(orientation === 'horizontal' && { flexShrink: 0 }),
       ...(orientation === 'wrap' && showBorder && {
         border: '1px solid #d3d3d3',
-        borderRadius: '8px'
+        borderRadius: '8px',
+      }),
+      ...(orientation !== 'wrap'  &&  {
+        marginTop: gap !== undefined ? (typeof gap === 'number' ? `${gap}px` : gap) : '0px',
       }),
     };
 
