918541f4d4d610f3b03c0ae224dad3af2c184255
HackGenesis <derik.sadiki@boxfusion.io>
2025-08-01 16:06:19 +0200

    fix style

--- shesha-reactjs/src/components/dataTable/sortingConfigurator/index.tsx
+++ shesha-reactjs/src/components/dataTable/sortingConfigurator/index.tsx
@@ -1,7 +1,7 @@
 import { Space, Select } from 'antd';
 import { ListEditor, PropertyAutocomplete } from '@/components/index';
-import { GroupingItem as SortingItem } from '@/providers/dataTable/interfaces';
-import React, { FC, useLayoutEffect, useRef, useState } from 'react';
+import { ColumnSorting, GroupingItem as SortingItem } from '@/providers/dataTable/interfaces';
+import React, { FC } from 'react';
 import { getNanoId } from '@/utils/uuid';
 import { ListItem } from '@/components/listEditor/models';
 
@@ -15,26 +15,6 @@ export interface ISortingEditorProps {
 }
 
 export const SortingEditor: FC<ISortingEditorProps> = (props) => {
-
-
-    const containerRef = useRef<HTMLDivElement>(null);
-    const [compactMode, setCompactMode] = useState(false);
-  
-    // Detect when container is too narrow
-    useLayoutEffect(() => {
-      const resizeObserver = new ResizeObserver((entries) => {
-        for (const entry of entries) {
-          const width = entry.contentRect.width;
-          setCompactMode(width < 140); 
-        }
-      });
-      if (containerRef.current) {
-        resizeObserver.observe(containerRef.current);
-      }
-      return () => resizeObserver.disconnect();
-    }, []);
-  
-
     const { value, onChange, readOnly: editorReadOnly, maxItemsCount } = props;
     return (
         <ListEditor<SortingItem & ListItem>
@@ -46,42 +26,34 @@ export const SortingEditor: FC<ISortingEditorProps> = (props) => {
         >
             {({ item, itemOnChange, readOnly }) => {
                 return (
-                    <div ref={containerRef}>
-                       <Space.Compact
-                        style={{
-                            width: '100%',
-                            display: compactMode ? 'grid' : 'flex',
-                            gridTemplateColumns: compactMode ? '1fr 1fr' : undefined,
-                        }}
-                        >
-                        <PropertyAutocomplete
-                            style={{ width: '100%', minWidth: 30 }}
-                            mode="single"
-                            value={item.propertyName}
-                            onChange={(value) => {
-                            if (!Array.isArray(value))
-                                itemOnChange({ ...item, propertyName: value }, undefined);
-                            }}
-                            autoFillProps={false}
-                            size="small"
-                            readOnly={readOnly}
-                        />
-
-                        <Select
-                            value={item.sorting}
-                            onChange={(value) =>
-                            itemOnChange({ ...item, sorting: value }, undefined)
-                            }
-                            style={{ minWidth: 70, width: '100%' }}
-                            size="small"
-                            disabled={readOnly}
-                        >
-                            <Option value="asc">{'Ascending'}</Option>
-                            <Option value="desc">{'Descending'}</Option>
-                        </Select>
+                    <div>
+                        <Space.Compact style={{ width: '100%' }}>
+                            <PropertyAutocomplete
+                                style={{ width: 'calc(100% - 120px)' }}
+                                mode='single'
+                                value={item.propertyName}
+                                onChange={(value) => {
+                                    if (!Array.isArray(value))
+                                        itemOnChange({ ...item, propertyName: value }, undefined);
+                                }}
+                                autoFillProps={false}
+                                size='small'
+                                readOnly={readOnly}
+                            />
+                            <Select<ColumnSorting>
+                                value={item.sorting}
+                                onChange={(value) => {
+                                    itemOnChange({ ...item, sorting: value }, undefined);
+                                }}
+                                style={{ width: '100%' }}
+                                size='small'
+                                disabled={readOnly}
+                            >
+                                <Option value="asc">Ascending</Option>
+                                <Option value="desc">Descending</Option>
+                            </Select>
                         </Space.Compact>
-
-                  </div>);
+                    </div>);
             }}
         </ListEditor>
     );
