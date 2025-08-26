df40211ae981271c6a1f4b9004af286a4e7b99ef
HackGenesis <derik.sadiki@boxfusion.io>
2025-08-26 16:14:37 +0200

    use phone size on pagination

--- shesha-reactjs/src/components/tablePager/index.tsx
+++ shesha-reactjs/src/components/tablePager/index.tsx
@@ -1,6 +1,6 @@
 import React, { CSSProperties, FC } from 'react';
 import { useMediaQuery } from 'react-responsive';
-import { DESKTOP_SIZE_QUERY, PHONE_SIZE_QUERY } from '@/shesha-constants/media-queries';
+import {PHONE_SIZE_QUERY } from '@/shesha-constants/media-queries';
 import { useDataTable } from '@/providers';
 import TablePaging from './tablePaging';
 import TableNoPaging from './tableNoPaging';
