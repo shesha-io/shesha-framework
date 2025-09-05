71536dfbde6484a31959e4dc5005957557d6dfdb
IvanIlyichev <ivan.Ilyichev@shesha.io>
2025-09-05 16:21:43 +0300

    chore: fixed build

--- shesha-core/test/Shesha.Tests/SheshaTestModule.cs
+++ shesha-core/test/Shesha.Tests/SheshaTestModule.cs
@@ -40,9 +40,10 @@ namespace Shesha.Tests
         )]
     public class SheshaTestModule : AbpModule
     {
-        public SheshaTestModule(SheshaNHibernateModule nhModule)
+        public SheshaTestModule(SheshaNHibernateModule nhModule, SheshaFrameworkModule frwkModule)
         {
             nhModule.SkipDbSeed = false;    // Set to false to apply DB Migration files on start up
+            frwkModule.SkipAppWarmUp = true;
         }
 
         public override void PreInitialize()
