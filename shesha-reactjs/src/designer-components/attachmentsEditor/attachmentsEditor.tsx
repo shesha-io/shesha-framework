29b89aeee35fb9da7cf3fbb8682c679510d16732
czwe-01 <tnkambule980115@gmail.com>
2025-09-16 09:25:24 +0200

    include pageContext

--- shesha-reactjs/src/designer-components/attachmentsEditor/attachmentsEditor.tsx
+++ shesha-reactjs/src/designer-components/attachmentsEditor/attachmentsEditor.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 import { CustomFile } from '@/components';
 import ConfigurableFormItem from '@/components/formDesigner/components/formItem';
 import { IToolboxComponent } from '@/interfaces';
-import { IStyleType, useForm, useFormData, useGlobalState, useHttpClient, useSheshaApplication } from '@/providers';
+import { IStyleType, useDataContextManagerActions, useForm, useFormData, useGlobalState, useHttpClient, useSheshaApplication } from '@/providers';
 import { IConfigurableFormComponent, IInputStyles } from '@/providers/form/models';
 import {
   evaluateValue,
@@ -60,7 +60,7 @@ const AttachmentsEditor: IToolboxComponent<IAttachmentsEditorProps> = {
     const { data } = useFormData();
     const { globalState, setState: setGlobalState } = useGlobalState();
     const { message } = App.useApp();
-
+    const pageContext = useDataContextManagerActions(false)?.getPageContext();
     const ownerId = evaluateValue(`${model.ownerId}`, { data: data, globalState });
 
     const enabled = !model.readOnly;
@@ -82,7 +82,8 @@ const AttachmentsEditor: IToolboxComponent<IAttachmentsEditorProps> = {
               http: httpClient,
               message,
               moment,
-              setGlobalState
+              setGlobalState,
+              pageContext
             });
           };
 
