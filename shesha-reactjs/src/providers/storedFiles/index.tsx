5655978a2d54153911a04ae2b9784dbbde7c967f
czwe-01 <tnkambule980115@gmail.com>
2025-09-25 06:45:37 +0200

    cleanup

--- shesha-reactjs/src/providers/storedFiles/index.tsx
+++ shesha-reactjs/src/providers/storedFiles/index.tsx
@@ -40,6 +40,7 @@ import {
 } from './contexts';
 import { storedFilesReducer } from './reducer';
 import { App } from 'antd';
+import { isAjaxSuccessResponse } from '@/interfaces/ajaxResponse';
 import { removeFile, updateAllFilesDownloaded, updateDownloadedAFile } from './utils';
 export interface IStoredFilesProviderProps {
   ownerId: string;
@@ -87,7 +88,9 @@ const StoredFilesProvider: FC<PropsWithChildren<IStoredFilesProviderProps>> = ({
   const { addItem: addDelayedUpdate, removeItem: removeDelayedUpdate } = useDelayedUpdate(false) ?? {};
 
   const {
+    loading: isFetchingFileList,
     refetch: fetchFileListHttp,
+    data: fileListResponse,
   } = useGet<IAjaxResponse<IStoredFile[]>>({
     path: filesListEndpoint.url,
     queryParams: {
@@ -123,6 +126,19 @@ const StoredFilesProvider: FC<PropsWithChildren<IStoredFilesProviderProps>> = ({
         });
     }
   }, [ownerId, ownerType, filesCategory, propertyName]);
+
+  useEffect(() => {
+    if (!isFetchingFileList) {
+      if (isAjaxSuccessResponse(fileListResponse)) {
+        const { result } = fileListResponse;
+        const fileList = filesReducer(result as IStoredFile[]);
+
+        dispatch(fetchFileListSuccessAction(fileList));
+      } else {
+        dispatch(fetchFileListErrorAction());
+      }
+    }
+  }, [isFetchingFileList, fileListResponse]);
 
   //#region Register signal r events
   useEffect(() => {
