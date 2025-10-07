68b8a9f402c8f92bd3377239bf0f63708bbcfd81
WillemBarendKruger <willem.kruger11@gmail.com>
2025-10-07 15:57:54 +0200

    add phone number input component with interfaces, settings, and toolbox integration

---
+++ shesha-reactjs/src/designer-components/phoneNumber/interface.ts
@@ -0,0 +1,28 @@
+import { IConfigurableFormComponent, IInputStyles } from '@/providers/form/models';
+import { CSSProperties } from 'styled-components';
+
+export type ValueFormat = 'fullNumber' | 'object';
+
+export interface IPhoneNumberValue {
+    number: string;
+    dialCode: string;
+    countryCode: string;
+}
+
+export interface IPhoneNumberInputComponentProps extends IConfigurableFormComponent, IInputStyles {
+    desktop?: CSSProperties;
+    tablet?: CSSProperties;
+    mobile?: CSSProperties;
+    placeholder?: string;
+    valueFormat?: ValueFormat;
+    country?: string;
+    defaultCountry?: string;
+    allowClear?: boolean;
+    enableArrow?: boolean;
+    disableParentheses?: boolean;
+    onlyCountries?: string[];
+    excludeCountries?: string[];
+    preferredCountries?: string[];
+    searchNotFound?: string;
+    onChangeCustom?: string;
+}
