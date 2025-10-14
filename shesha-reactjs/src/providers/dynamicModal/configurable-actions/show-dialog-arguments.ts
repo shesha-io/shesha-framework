577cb290ff30c72c3108fdd275186bdde09afccb
HackGenesis <derik.sadiki@boxfusion.io>
2025-10-14 16:26:00 +0200

    add hide close button

--- shesha-reactjs/src/providers/dynamicModal/configurable-actions/show-dialog-arguments.ts
+++ shesha-reactjs/src/providers/dynamicModal/configurable-actions/show-dialog-arguments.ts
@@ -152,6 +152,14 @@ export const getSettings = (): FormMarkupWithSettings => {
                   jsSetting: true,
                 },
                 {
+                  type: 'switch',
+                  id: nanoid(),
+                  propertyName: 'showCloseIcon',
+                  label: 'Show Close Button',
+                  defaultValue: true,
+                  jsSetting: true,
+                },
+                {
                   type: 'buttonGroupConfigurator',
                   id: nanoid(),
                   propertyName: 'buttons',
