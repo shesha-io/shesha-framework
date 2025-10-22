0851a9f3dc04043890eb1592d9c7d60432d879ea
Teboho <tebohomakhalemele@outlook.com>
2025-10-22 12:45:24 +0200

    refactor: enhance permission handling in GetCurrentLoginInfoAsync method and define System Administrator role so that the user who's currently logged if admin has all permissions

--- shesha-core/src/Shesha.Web.Core/Authorization/RoleNames.cs
+++ shesha-core/src/Shesha.Web.Core/Authorization/RoleNames.cs
@@ -1,13 +1,17 @@
+using System;
+
 namespace Boxfusion.Authorization
 {
     /// <summary>
     /// SheshaWebCore Role Names
     /// </summary>
+    [Obsolete("Use Shesha.Authorization.Roles.StaticRoleNames instead to avoid circular dependencies")]
     public static class RoleNames
     {
         /// <summary>
         /// Data Administrator
         /// </summary>
+        [Obsolete("Use Shesha.Authorization.Roles.StaticRoleNames.SystemAdministrator instead")]
         public const string SystemAdministrator = "System Administrator";
     }
 }
