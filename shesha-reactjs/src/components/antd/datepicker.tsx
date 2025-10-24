3cd7e3e0418fce98be39adff5f0c9ae8bc1d88b3
WillemBarendKruger <willem.kruger11@gmail.com>
2025-10-24 08:30:04 +0200

    removed unused import

--- shesha-reactjs/src/components/antd/datepicker.tsx
+++ shesha-reactjs/src/components/antd/datepicker.tsx
@@ -1,7 +1,6 @@
 import { DatePicker } from 'antd';
 import type { Moment } from 'moment';
 import momentGenerateConfig from 'rc-picker/lib/generate/moment';
-import type generatePicker from 'antd/es/date-picker/generatePicker';
 
 const MomentDatePicker = DatePicker.generatePicker<Moment>(momentGenerateConfig);
 
