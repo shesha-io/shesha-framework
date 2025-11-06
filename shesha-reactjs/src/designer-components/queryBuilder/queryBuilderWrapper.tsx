7c2d16be4ae7d224095a288f1f4fae714f4ee5af
HackGenesis <derik.sadiki@boxfusion.io>
2025-11-06 09:47:15 +0200

    uodate metadata

--- shesha-reactjs/src/designer-components/queryBuilder/queryBuilderWrapper.tsx
+++ shesha-reactjs/src/designer-components/queryBuilder/queryBuilderWrapper.tsx
@@ -2,7 +2,8 @@ import React, { FC, PropsWithChildren } from 'react';
 import { QueryBuilderProvider, useMetadata } from '@/providers';
 
 export const QueryBuilderWrapper: FC<PropsWithChildren<{}>> = ({ children }) => {
-  const { metadata } = useMetadata(true);
+  const metadataContext = useMetadata(false);
+  const metadata = metadataContext?.metadata;
   return (
     <QueryBuilderProvider id="QueryBuilderWrapper" metadata={metadata}>
       {children}
