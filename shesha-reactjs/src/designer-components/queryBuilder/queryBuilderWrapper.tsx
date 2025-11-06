e90690c8492e8b144760ef5c672984b80ffd11c8
HackGenesis <derik.sadiki@boxfusion.io>
2025-11-06 09:56:32 +0200

    handle errors

--- shesha-reactjs/src/designer-components/queryBuilder/queryBuilderWrapper.tsx
+++ shesha-reactjs/src/designer-components/queryBuilder/queryBuilderWrapper.tsx
@@ -4,6 +4,11 @@ import { QueryBuilderProvider, useMetadata } from '@/providers';
 export const QueryBuilderWrapper: FC<PropsWithChildren<{}>> = ({ children }) => {
   const metadataContext = useMetadata(false);
   const metadata = metadataContext?.metadata;
+
+  if (!metadata) {
+    return <>{children}</>;
+  }
+
   return (
     <QueryBuilderProvider id="QueryBuilderWrapper" metadata={metadata}>
       {children}
