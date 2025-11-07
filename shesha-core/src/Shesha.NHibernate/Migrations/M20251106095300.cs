60c8efdff4d50ca510d50fa3b7cb2077a084bca5
Tshepo <tshepo1103@gmail.com>
2025-11-07 09:54:37 +0200

    refactor: Remove unused MoveFks method and streamline foreign key creation

--- shesha-core/src/Shesha.NHibernate/Migrations/M20251106095300.cs
+++ shesha-core/src/Shesha.NHibernate/Migrations/M20251106095300.cs
@@ -11,11 +11,6 @@ namespace Shesha.Migrations
     [Migration(20251106095300)]
     public class M20251106095300 : OneWayMigration
     {
-        private void MoveFks(string oldTable, string newTable)
-        {
-            this.Shesha().MoveForeignKeys(oldTable, null, "Id", newTable, "frwk", "id");
-        }
-
         public override void Up()
         {
             Create.Table("stored_file_version_downloads").InSchema("frwk")
@@ -24,40 +19,38 @@ namespace Shesha.Migrations
                 .WithTenantIdAsNullable(SnakeCaseDbObjectNames.Instance).Indexed()
                 .WithColumn("file_version_id").AsGuid().Nullable().Indexed();
 
+            Create.ForeignKey("fk_stored_file_version_downloads_file_version_id")
+                   .FromTable("stored_file_version_downloads").InSchema("frwk").ForeignColumn("file_version_id")
+                   .ToTable("stored_file_versions").InSchema("frwk").PrimaryColumn("id");
+
             if (Schema.Table("Frwk_StoredFileVersionDownloads").Exists())
             {
                 Execute.Sql(@"insert into frwk.stored_file_version_downloads
-                                (id
-                                ,creation_time
-                                ,creator_user_id
-                                ,last_modification_time
-                                ,last_modifier_user_id
-                                ,is_deleted
-                                ,deletion_time
-                                ,deleter_user_id
-                                ,tenant_id
-                                ,file_version_id)
-                            select
-                                ""Id""
-                                ,""CreationTime""
-                                ,""CreatorUserId""
-                                ,""LastModificationTime""
-                                ,""LastModifierUserId""
-                                ,""IsDeleted""
-                                ,""DeletionTime""
-                                ,""DeleterUserId""
-                                ,NULL  
-                                ,""FileVersionId""
-                            from ""Frwk_StoredFileVersionDownloads""
-                            ");
-
-                MoveFks("Frwk_StoredFileVersionDownloads", "stored_file_version_downloads");
+                        (id
+                        ,creation_time
+                        ,creator_user_id
+                        ,last_modification_time
+                        ,last_modifier_user_id
+                        ,is_deleted
+                        ,deletion_time
+                        ,deleter_user_id
+                        ,tenant_id
+                        ,file_version_id)
+                    select
+                        ""Id""
+                        ,""CreationTime""
+                        ,""CreatorUserId""
+                        ,""LastModificationTime""
+                        ,""LastModifierUserId""
+                        ,""IsDeleted""
+                        ,""DeletionTime""
+                        ,""DeleterUserId""
+                        ,NULL  
+                        ,""FileVersionId""
+                    from ""Frwk_StoredFileVersionDownloads""
+                ");
 
                 Delete.Table("Frwk_StoredFileVersionDownloads");
-
-                Create.ForeignKey("fk_stored_file_version_downloads_file_version_id")
-                    .FromTable("stored_file_version_downloads").InSchema("frwk").ForeignColumn("file_version_id")
-                    .ToTable("stored_file_versions").InSchema("frwk").PrimaryColumn("id");
             }
         }
     }
