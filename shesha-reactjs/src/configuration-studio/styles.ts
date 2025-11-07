494e984326a740c85bc9b45a4ee1b2a007e61411
czwe-01 <tnkambule980115@gmail.com>
2025-11-07 14:49:45 +0200

    fix: update tooltip container for DragWrapper and remove overflow from styles

--- shesha-reactjs/src/configuration-studio/styles.ts
+++ shesha-reactjs/src/configuration-studio/styles.ts
@@ -22,9 +22,6 @@ export const useStyles = createStyles(({ css, cx, token, prefixCls, iconPrefixCl
   const headerHeight = 60;
 
   const configStudio = cx("sha-config-studio", css`
-        body {
-            overflow: hidden;
-        }
 
         .${csHeader} {
             height: ${headerHeight}px;
