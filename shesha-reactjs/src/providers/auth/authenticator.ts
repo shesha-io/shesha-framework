008d8609a9bb68836d8ffe693ed3c309c10d1800
IvanIlyichev <ivan.Ilyichev@shesha.io>
2025-11-12 12:10:49 +0300

    chore: set default token expiration to 2 hours for functional test site
    (cherry picked from commit f1743e548e17d18a05fff4fcb1f5f4e018a2029e)
    
    # Conflicts:
    #	shesha-functional-tests/backend/src/Boxfusion.SheshaFunctionalTests.Web.Host/appsettings.json

--- shesha-reactjs/src/providers/auth/authenticator.ts
+++ shesha-reactjs/src/providers/auth/authenticator.ts
@@ -252,21 +252,25 @@ export class Authenticator implements IAuthenticator {
   #startTokenExpirationTimer = (expireOn: string | undefined) => {
     this.#clearTokenExpirationTimer();
 
-    const expirationDate = expireOn && new Date(expireOn);
-    const timeUntilExpiration = expirationDate.getTime() - Date.now();
-    if (timeUntilExpiration > 0) {
-      this.#timer = setTimeout(() => {
-        this.#clearAccessToken();
-        this.#updateState('waiting', null, null);
-        this.#redirect(this.#unauthorizedRedirectUrl);
-        this.#onTokenExpired?.();
-        this.#timer = undefined;
-      }, timeUntilExpiration);
+    if (expireOn) {
+      const expirationDate = expireOn && new Date(expireOn);
+      const timeUntilExpiration = expirationDate.getTime() - Date.now();
+      if (timeUntilExpiration > 0) {
+        this.#timer = setTimeout(() => {
+          this.#clearAccessToken();
+          this.#loginInfo = null;
+          this.#updateState('waiting', null, null);
+          this.#redirect(this.#unauthorizedRedirectUrl);
+          this.#onTokenExpired?.();
+          this.#timer = undefined;
+        }, timeUntilExpiration);
+      }
     }
   };
   #clearTokenExpirationTimer = () => {
     if (this.#timer) {
-      clearInterval(this.#timer);
+      clearTimeout(this.#timer);
+      this.#timer = undefined;
     }
   };
 
