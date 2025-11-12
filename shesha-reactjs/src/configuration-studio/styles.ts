1c0abf0fa32d21dc18967887ddce86803f30a6da
czwe-01 <tnkambule980115@gmail.com>
2025-11-12 09:24:15 +0200

    rm unrelated style

--- shesha-reactjs/src/configuration-studio/styles.ts
+++ shesha-reactjs/src/configuration-studio/styles.ts
@@ -52,7 +52,6 @@ export const useStyles = createStyles(({ css, cx, token, prefixCls, iconPrefixCl
         }
         .${csContent}{
             background-color: ${token.colorBgContainer};
-            height: calc(100vh - 55px);
         }
         .${csTreeArea}{
             height: calc(100vh - ${headerHeight}px);
