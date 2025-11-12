f81ea619d5636495ce6ab6b2035a9663606a7c7d
IvanIlyichev <ivan.Ilyichev@shesha.io>
2025-11-12 11:19:12 +0300

    feat: configurable JWT token expiration
    - implemented configurable expiration using `Authentication:JwtBearer:ExpirationSeconds` key
    - implemented auto redirect to login page on expiration on front-end

--- shesha-core/src/Shesha.Application/Authorization/TokenAuthController.cs
+++ shesha-core/src/Shesha.Application/Authorization/TokenAuthController.cs
@@ -4,6 +4,7 @@ using Abp.Domain.Repositories;
 using Abp.MultiTenancy;
 using Abp.Runtime.Security;
 using Abp.UI;
+using DocumentFormat.OpenXml.Spreadsheet;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -101,9 +102,11 @@ namespace Shesha.Authorization
 
         private async Task<AuthenticateResultModel> GetAuthenticateResultAsync(ShaLoginResult<User> loginResult, string imei) 
         {
-            var accessToken = CreateAccessToken(CreateJwtClaims(loginResult.Identity));
-
+            var validFrom = DateTime.UtcNow;
+            var expiresOn = validFrom.Add(_configuration.Expiration);
             var expireInSeconds = (int)_configuration.Expiration.TotalSeconds;
+
+            var accessToken = CreateAccessToken(CreateJwtClaims(loginResult.Identity), validFrom, expiresOn);
 
             var personId = loginResult?.User != null
                 ? await _personRepository.GetAll()
@@ -121,7 +124,7 @@ namespace Shesha.Authorization
                 AccessToken = accessToken,
                 EncryptedAccessToken = GetEncryptedAccessToken(accessToken),
                 ExpireInSeconds = expireInSeconds,
-                ExpireOn = DateTime.Now.AddSeconds(expireInSeconds),
+                ExpireOn = expiresOn,
                 UserId = loginResult.User.Id,
                 PersonId = personId,
                 DeviceName = device?.Name,
@@ -309,16 +312,21 @@ namespace Shesha.Authorization
             }
         }
 
-        private string CreateAccessToken(IEnumerable<Claim> claims, TimeSpan? expiration = null)
+        private string CreateAccessToken(IEnumerable<Claim> claims) 
         {
-            var now = DateTime.UtcNow;
+            var validFrom = DateTime.UtcNow;
+            var expiresOn = validFrom.Add(_configuration.Expiration);
+            return CreateAccessToken(claims);
+        }
 
+        private string CreateAccessToken(IEnumerable<Claim> claims, DateTime validFrom, DateTime? expiresOn = null)
+        {
             var jwtSecurityToken = new JwtSecurityToken(
                 issuer: _configuration.Issuer,
                 audience: _configuration.Audience,
                 claims: claims,
-                notBefore: now,
-                expires: now.Add(expiration ?? _configuration.Expiration),
+                notBefore: validFrom,
+                expires: expiresOn,
                 signingCredentials: _configuration.SigningCredentials
             );
 
