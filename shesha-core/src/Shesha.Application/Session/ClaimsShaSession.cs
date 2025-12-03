cfece2769a0aa26d81cc5d9c68f4288141298264
IvanIlyichev <ivan.Ilyichev@shesha.io>
2025-12-03 18:26:33 +0300

    feat: implemented custom session
    (cherry picked from commit 58520bdb32772f72e7e90e2bd3191f75819e5a41)
    
    # Conflicts:
    #	shesha-reactjs/src/providers/auth/authenticator.ts
    #	shesha-reactjs/src/providers/auth/models.ts
    #	shesha-reactjs/src/providers/sheshaApplication/configurable-actions/execute-script.ts
    (cherry picked from commit d385c077be63027f15f61346ea78e359ad49557a)
    
    # Conflicts:
    #	shesha-reactjs/src/providers/auth/authenticator.ts
    #	shesha-reactjs/src/providers/auth/models.ts

---
+++ shesha-core/src/Shesha.Application/Session/ClaimsShaSession.cs
@@ -0,0 +1,47 @@
+﻿using Abp.Configuration.Startup;
+using Abp.Domain.Repositories;
+using Abp.MultiTenancy;
+using Abp.Runtime;
+using Abp.Runtime.Session;
+using Shesha.Domain;
+using Shesha.Extensions;
+using Shesha.Utilities;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shesha.Session
+{
+    /// <summary>
+    /// Implements <see cref="IShaSession"/> to get session properties from current claims.
+    /// </summary>
+    public class ClaimsShaSession : ClaimsAbpSession, IShaSession
+    {
+        protected IRepository<Person, Guid> PersonRepository;
+
+        public ClaimsShaSession(IPrincipalAccessor principalAccessor, 
+            IMultiTenancyConfig multiTenancy, 
+            ITenantResolver tenantResolver, 
+            IAmbientScopeProvider<SessionOverride> sessionOverrideScopeProvider,
+            IRepository<Person, Guid> personRepository) : base(principalAccessor, multiTenancy, tenantResolver, sessionOverrideScopeProvider)
+        {
+            PersonRepository = personRepository;
+        }
+
+        public virtual Guid? PersonId 
+        {
+            get
+            {
+                var personIdClaim = PrincipalAccessor.Principal?.Claims.FirstOrDefault(c => c.Type == ShaClaimTypes.PersonId);
+                return !string.IsNullOrWhiteSpace(personIdClaim?.Value)
+                    ? personIdClaim.Value.ToGuidOrNull()
+                    : null;
+            }
+        }
+
+        public Task<Person> GetCurrentPersonAsync()
+        {
+            return PersonRepository.GetAll().FirstOrDefaultAsync(p => p.User != null && p.User.Id == this.GetUserId());
+        }
+    }
+}
