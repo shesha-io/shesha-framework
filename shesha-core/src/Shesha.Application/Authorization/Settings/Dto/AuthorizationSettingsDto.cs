ab4077576d930a9b6316b6abdb1e8b8f545b25d6
Ntuthuko Msane <ntuthukomsane@gmail.com>
2025-12-09 11:27:43 +0200

    authorization dto

--- shesha-core/src/Shesha.Application/Authorization/Settings/Dto/AuthorizationSettingsDto.cs
+++ shesha-core/src/Shesha.Application/Authorization/Settings/Dto/AuthorizationSettingsDto.cs
@@ -1,9 +1,4 @@
-using System;
 using System.ComponentModel.DataAnnotations;
-using Shesha.Configuration.Security.Frontend;
-using Shesha.EntityReferences;
-
-namespace Shesha.Authorization.Settings.Dto
 
 namespace Shesha.Authorization.Settings.Dto
 {
