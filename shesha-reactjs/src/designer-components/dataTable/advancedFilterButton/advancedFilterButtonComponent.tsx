836394a4ed6ee69044a9e283f3c86571fd148b00
Teboho <tebohomakhalemele@outlook.com>
2025-12-09 14:36:40 +0200

    Update editMode logic in AdvancedFilterButtonComponent to depend on context.isNew

--- shesha-reactjs/src/designer-components/dataTable/advancedFilterButton/advancedFilterButtonComponent.tsx
+++ shesha-reactjs/src/designer-components/dataTable/advancedFilterButton/advancedFilterButtonComponent.tsx
@@ -85,7 +85,7 @@ const AdvancedFilterButtonComponent: IToolboxComponent<IButtonComponentProps> = 
     m
       .add<IButtonComponentProps>(3, (prev) => migrateReadOnly(prev, 'inherited'))
       .add<IButtonComponentProps>(4, (prev) => ({ ...migratePrevStyles(prev, defaultStyles(prev)) }))
-      .add<IButtonComponentProps>(5, (prev) => ({ ...prev, editMode: "editable"})),
+      .add<IButtonComponentProps>(5, (prev, context) => ({ ...prev, editMode: context.isNew ? 'editable' : prev.editMode })),
 };
 
 export default AdvancedFilterButtonComponent;
