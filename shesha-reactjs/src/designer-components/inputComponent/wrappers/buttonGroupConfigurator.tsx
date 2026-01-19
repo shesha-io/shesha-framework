0f254bf2d8c9958131d7f3106a698dfa0db390c8
czwe-01 <tnkambule980115@gmail.com>
2026-01-19 14:32:57 +0200

    Enhance ButtonGroupConfiguratorWrapper to accept additional props: buttonText, buttonTextReadOnly, and title for improved customization.

--- shesha-reactjs/src/designer-components/inputComponent/wrappers/buttonGroupConfigurator.tsx
+++ shesha-reactjs/src/designer-components/inputComponent/wrappers/buttonGroupConfigurator.tsx
@@ -3,13 +3,16 @@ import React, { FC } from 'react';
 import { ButtonGroupConfigurator } from '@/components';
 
 export const ButtonGroupConfiguratorWrapper: FC<IButtonGroupConfiguratorSettingsInputProps> = (props) => {
-  const { value, readOnly, size, onChange } = props;
+  const { value, readOnly, size, onChange, buttonText, buttonTextReadOnly, title } = props;
   return (
     <ButtonGroupConfigurator
       value={value}
       onChange={onChange}
       readOnly={readOnly}
       size={size}
+      buttonText={buttonText}
+      buttonTextReadOnly={buttonTextReadOnly}
+      title={title}
     />
   );
 };
