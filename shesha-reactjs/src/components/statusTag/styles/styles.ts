8e9d6436a778e35f809f2b1bdc0c5a9dd99b273e
Lihlumise Njani <lihlumisenjani@outlook.com>
2026-01-20 11:32:04 +0200

    style: remove left and right margins on status tag component

--- shesha-reactjs/src/components/statusTag/styles/styles.ts
+++ shesha-reactjs/src/components/statusTag/styles/styles.ts
@@ -1,13 +1,11 @@
 import { createStyles } from '@/styles';
-import { sheshaStyles } from '@/styles';
 
 export const useStyles = createStyles(({ css, cx }) => {
    const shaStatusTag = cx("sha-status-tag", css`
     text-transform: uppercase;
     text-align: center;
     margin: 3px 0;
-    align-self: center;
-    margin: 0 ${sheshaStyles.paddingMD}px !important;   
+    align-self: center;  
   `); 
   return {
     shaStatusTag,
