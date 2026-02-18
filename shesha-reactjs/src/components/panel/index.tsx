3f6e3b2fa9ccb2ce6cc15152cbf9998df4645564
czwe-01 <tnkambule980115@gmail.com>
2026-02-18 08:44:14 +0200

    refactor: remove console log from CollapsiblePanel for cleaner code
    - Eliminated the console log statement for body styles in CollapsiblePanel to enhance code cleanliness and reduce unnecessary debugging output.

--- shesha-reactjs/src/components/panel/index.tsx
+++ shesha-reactjs/src/components/panel/index.tsx
@@ -86,7 +86,6 @@ export const CollapsiblePanel: FC<Omit<ICollapsiblePanelProps, 'radiusLeft' | 'r
   const { styles } = useStyles({ bodyStyle, headerStyle, ghost, isSimpleDesign, noContentPadding, hideWhenEmpty, hideCollapseContent, accentStyle, overflow: overflowStyle });
   const shaCollapsiblePanelStyle = isSimpleDesign ? styles.shaSimpleDesign : styles.shaCollapsiblePanel;
 
-  console.log("Body styles :: ", bodyStyle);
   return (
     <Collapse
       defaultActiveKey={collapsedByDefault ? [] : ['1']}
