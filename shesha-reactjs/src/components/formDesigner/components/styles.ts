1e246fa7003d906d5224081501257ad7449938f6
czwe-01 <tnkambule980115@gmail.com>
2026-02-18 10:33:44 +0200

    refactor: update height property in useStyles for consistent layout
    - Added height: 100% to the useStyles definition to ensure consistent component sizing within the form designer.
    - This change enhances the layout control and visual consistency across form components.

--- shesha-reactjs/src/components/formDesigner/components/styles.ts
+++ shesha-reactjs/src/components/formDesigner/components/styles.ts
@@ -31,6 +31,7 @@ export const useStyles = createStyles(({ css, cx }, { layout, hasLabel, noLabelA
                 min-height : 0px !important;
                 flex: 1;
                 width: 100%;
+                height: 100%;
 
                 .ant-form-item-control-input-content {
                     ${preserveDimensionsInDesigner ? 'height: auto' : 'height: 100%'};
