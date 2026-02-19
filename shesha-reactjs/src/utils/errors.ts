ffc6af3ff66c7b0b16553f9039cffac6fb068923
James Baloyi <james_baloyi@yahoo.com>
2026-02-19 16:15:44 +0200

    Fix: OnBeforeRowReorder validation fails and shows generic framework error instead of custom message (resolves #4512)

--- shesha-reactjs/src/utils/errors.ts
+++ shesha-reactjs/src/utils/errors.ts
@@ -20,6 +20,18 @@ export interface ISheshaErrorCause {
   errors?: IModelValidation;
 }
 
+
+export class RowReorderValidationError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = 'RowReorderValidationError';
+  }
+
+  static isRowReorderValidationError(error: Error): error is RowReorderValidationError {
+    return error instanceof RowReorderValidationError;
+  }
+}
+
 /**
  * Shesha Error class
  */
