162e0702809522276a20f3dd033836683c21f66a
czwe-01 <tnkambule980115@gmail.com>
2026-02-20 13:20:03 +0200

    backend url for testing in shesha cloud

--- shesha-reactjs/src/app/layout.tsx
+++ shesha-reactjs/src/app/layout.tsx
@@ -11,7 +11,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }): JSX.Element {
   noStore();
-  const backendUrl = process.env.BACKEND_URL ?? 'http://localhost:21021';
+  // const backendUrl = process.env.BACKEND_URL ?? 'http://localhost:21021';
+  const backendUrl = 'https://linux-functional-api-test.azurewebsites.net';
 
   return (
     <html lang="en">
