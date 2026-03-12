dc3b348a70697d2dd1b7145941078c263270e7b4
HackGenesis <derik.sadiki@boxfusion.io>
2026-03-12 15:37:56 +0200

    fix shadows

--- shesha-reactjs/src/designer-components/horizontalMenu/utils.ts
+++ shesha-reactjs/src/designer-components/horizontalMenu/utils.ts
@@ -1,6 +1,6 @@
 import { IStyleType } from "@/index";
 
-export const defaultStyles = (): IStyleType => {
+export const defaultStyles = () => {
   return {
     background: { type: 'color' },
     font: { weight: '400', size: 14, color: '#000', type: 'Segoe UI' },
@@ -23,14 +23,8 @@ export const defaultStyles = (): IStyleType => {
       blurRadius: 0,
       spreadRadius: 0,
     },
-    menuItemShadow: {
-      color: '#000000',
-      offsetX: 0,
-      offsetY: 0,
-      blurRadius: 0,
-      spreadRadius: 0,
-    },
     overflow: 'dropdown',
     dimensions: { width: '500px', height: '55px', minHeight: '0px', maxHeight: 'auto', minWidth: '0px', maxWidth: 'auto' },
-  };
+    menuOverflow: 'dropdown',
+  } as any;
 };
