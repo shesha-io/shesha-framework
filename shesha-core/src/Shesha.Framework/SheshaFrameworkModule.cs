4b42648abfc1b24088380f6f53493e552e08b89b
AlexStepantsov <125558744+AlexStepantsov@users.noreply.github.com>
2026-03-20 00:23:48 +0300

    Merge pull request #4640 from ktsapo/hotfix/4615/default-endpoint-access-authenticated
    security: change default endpoint access from AllowAnonymous to AnyAuthenticated

--- shesha-core/src/Shesha.Framework/SheshaFrameworkModule.cs
+++ shesha-core/src/Shesha.Framework/SheshaFrameworkModule.cs
@@ -142,7 +142,7 @@ namespace Shesha
                     OtpCooldownSeconds = 60,
                     UseResetPasswordViaSecurityQuestions = true,
                     ResetPasswordViaSecurityQuestionsNumQuestionsAllowed = 3,
-                    DefaultEndpointAccess = Domain.Enums.RefListPermissionedAccess.AllowAnonymous
+                    DefaultEndpointAccess = Domain.Enums.RefListPermissionedAccess.AnyAuthenticated
                 });
             });
 
