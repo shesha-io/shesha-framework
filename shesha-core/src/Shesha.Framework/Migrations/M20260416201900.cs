2722893e22667213259f43f729145e3e0e0d0e14
Alex Stephens <alex.stephens@boxfusion.io>
2026-04-16 21:25:00 +0300

    Error occurs when saving admin user details #4803

---
+++ shesha-core/src/Shesha.Framework/Migrations/M20260416201900.cs
@@ -0,0 +1,15 @@
+﻿using FluentMigrator;
+using Shesha.FluentMigrator;
+
+namespace Shesha.Migrations
+{
+    [Migration(20260416201900)]
+    public class M20260416201900 : OneWayMigration
+    {
+        public override void Up()
+        {
+            // fill required data (AbpUser.NormalizedEmailAddress has [Required] attribute)
+            Execute.Sql("update \"AbpUsers\" set \"EmailAddress\" = '', \"NormalizedEmailAddress\" = ' ' where \"EmailAddress\" is null");
+        }
+    }
+}
