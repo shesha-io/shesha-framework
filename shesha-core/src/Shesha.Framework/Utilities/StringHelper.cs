233dde340ce5547d97cfb2231f820f836900fab8
Robert-John Marshall <robert.marshall@boxfusion.io>
2026-04-17 07:50:12 +0200

    [fix] send email validate address - allow address with .afrca extenion and + in the mailbox name

--- shesha-core/src/Shesha.Framework/Utilities/StringHelper.cs
+++ shesha-core/src/Shesha.Framework/Utilities/StringHelper.cs
@@ -866,14 +866,12 @@ namespace Shesha.Utilities
 
         public static bool IsValidEmail(this string inputEmail)
         {
-            string strRegex = @"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}" +
+            string strRegex = @"^([a-zA-Z0-9_\-\.\+]+)@((\[[0-9]{1,3}" +
                               @"\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\" +
-                              @".)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$";
+                              @".)+))([a-zA-Z]{2,}|[0-9]{1,3})(\]?)$";
             Regex re = new Regex(strRegex);
-            if (re.IsMatch(inputEmail))
-                return (true);
-            else
-                return (false);
+            var normalised = Regex.Replace(inputEmail ?? "", @"\s+", "");
+            return re.IsMatch(normalised);
         }
 
         [DebuggerStepThrough]
