5876ac9a69f55e8bf86a6b9f8316ebb1497ccfd9
KatlehoMadaba <101579604+KatlehoMadaba@users.noreply.github.com>
2026-05-05 16:05:34 +0200

    feat: add configuration-studio page

---
+++ shesha-starter/frontend/src/app/configuration-studio/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import { ConfigurationStudio, PageWithLayout } from "@shesha-io/reactjs";
+import React from "react";
+
+const Page: PageWithLayout = () => {
+  return <ConfigurationStudio />;
+};
+
+export default Page;
