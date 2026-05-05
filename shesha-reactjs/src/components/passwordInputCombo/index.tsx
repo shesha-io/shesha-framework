cf53994de9dc8ae26698da5fd166b8e7201ebd00
James Baloyi <james_baloyi@yahoo.com>
2026-05-05 13:12:56 +0200

    move password suppression after props spread

--- shesha-reactjs/src/components/passwordInputCombo/index.tsx
+++ shesha-reactjs/src/components/passwordInputCombo/index.tsx
@@ -64,7 +64,7 @@ const PasswordInputCombo: FC<IProps> = ({
         trigger="focus"
       >
         <FormItem {...formItemProps} {...passwordValidations(newPassword, errorMessage, passwordLength)}>
-          <Password autoComplete="new-password" {...inputProps} placeholder={placeholder} value={newPassword} onChange={onPasswordChange} style={style} />
+          <Password {...inputProps} placeholder={placeholder} autoComplete="new-password" value={newPassword} onChange={onPasswordChange} style={style} />
         </FormItem>
       </Popover>
 
@@ -79,8 +79,8 @@ const PasswordInputCombo: FC<IProps> = ({
           {...confirmPasswordValidations(newPassword, repeatPassword, errorMessage)}
         >
           <Password
-            autoComplete="new-password"
             {...inputProps}
+            autoComplete="new-password"
             placeholder={confirmPlaceholder}
             value={repeatPassword}
             style={style}
