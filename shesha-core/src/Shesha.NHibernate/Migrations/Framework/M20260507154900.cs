9be6ff70a0a2503bfc58b304334db0f716d1f03d
Wellington Kahondo <Wellington.Kahondo@boxfusion.io>
2026-05-07 16:01:40 +0200

    Added a migration to extend the Recipient column size

---
+++ shesha-core/src/Shesha.NHibernate/Migrations/Framework/M20260507154900.cs
@@ -0,0 +1,13 @@
+﻿using FluentMigrator;
+
+namespace Shesha.Migrations.Framework
+{
+    [Migration(20260507154900), MsSqlOnly]
+    public class M20260507154900 : AutoReversingMigration
+    {
+        public override void Up()
+        {
+            Alter.Table("Frwk_OtpAuditItems").AlterColumn("RecipientId").AsString(100).Nullable();
+        }
+    }
+}
