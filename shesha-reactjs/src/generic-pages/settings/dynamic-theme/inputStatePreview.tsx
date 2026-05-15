bb557cb77a48a93bcd8f0892c836341f70077752
czwe-01 <tnkambule980115@gmail.com>
2026-05-15 09:33:07 +0200

    refactor: improve input state preview layout and formatting
    - Adjusted wrapperCol span in InputStatesPreview to align with theme layout settings for better responsiveness.
    - Enhanced formatting of Form.Item components for improved readability and consistency in the input state presentation.

--- shesha-reactjs/src/generic-pages/settings/dynamic-theme/inputStatePreview.tsx
+++ shesha-reactjs/src/generic-pages/settings/dynamic-theme/inputStatePreview.tsx
@@ -1,28 +1,42 @@
-import React, { FC } from 'react';
-import { useTheme } from '@/providers';
-import { Form, FormItemProps, Input, Space } from 'antd';
+import React, { FC } from "react";
+import { useTheme } from "@/providers";
+import { Form, FormItemProps, Input, Space } from "antd";
 
 const InputStatesPreview: FC = () => {
   const { theme } = useTheme();
 
+  const componentSpan = theme.layout === "vertical" ? 24 : theme.componentSpan;
   const commonProps: FormItemProps = {
     layout: theme.layout,
     labelAlign: theme.labelAlign,
     labelCol: { span: theme.labelSpan },
+    wrapperCol: { span: componentSpan },
   };
 
   return (
     <Space>
-      <Form.Item {...commonProps} label="Failed" validateStatus="error" help="Please complete before submission">
+      <Form.Item
+        {...commonProps}
+        label="Failed"
+        validateStatus="error"
+        help="Please complete before submission"
+      >
         <Input placeholder="Placeholder Text" />
       </Form.Item>
-      <Form.Item {...commonProps} validateStatus="warning" label="Warning">
-        <Input placeholder="Warning Message" prefix={<span style={{ color: '#faad14' }}>⚠</span>} />
+      <Form.Item {...commonProps} label="Warning">
+        <Input
+          placeholder="Warning Message"
+          prefix={<span style={{ color: "#faad14" }}>⚠</span>}
+        />
       </Form.Item>
-      <Form.Item {...commonProps} label="Validating" validateStatus="validating" help="Please wait while we validate your input">
+      <Form.Item
+        {...commonProps}
+        label="Validating"
+        help="Please wait while we validate your input"
+      >
         <Input placeholder="Placeholder Text" />
       </Form.Item>
-      <Form.Item {...commonProps} label="Success" validateStatus="success">
+      <Form.Item {...commonProps} label="Success">
         <Input placeholder="Successful Input" />
       </Form.Item>
     </Space>
