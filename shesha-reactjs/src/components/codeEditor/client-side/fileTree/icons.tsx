c51dca57875fa4dc15352f5c9b1a216d12064e01
James Baloyi <james_baloyi@yahoo.com>
2026-05-25 14:23:40 +0200

    revet icons SiCss3

--- shesha-reactjs/src/components/codeEditor/client-side/fileTree/icons.tsx
+++ shesha-reactjs/src/components/codeEditor/client-side/fileTree/icons.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { SiHtml5, SiCss, SiJavascript, SiTypescript, SiJson } from "react-icons/si";
+import { SiHtml5, SiCss3, SiJavascript, SiTypescript, SiJson } from "react-icons/si";
 import { FcFolder, FcOpenedFolder, FcPicture, FcFile } from "react-icons/fc";
 import { AiFillFileText } from "react-icons/ai";
 import { AntdTreeNodeAttribute } from 'antd/lib/tree';
@@ -15,7 +15,7 @@ export const getNodeIcon = (nodeProps: AntdTreeNodeAttribute): JSX.Element => {
     case "jsx": return <SiJavascript color="#fbcb38" />;
     case "ts": return <SiTypescript color="#378baa" />;
     case "tsx": return <SiTypescript color="#378baa" />;
-    case "css": return <SiCss color="purple" />;
+    case "css": return <SiCss3 color="purple" />;
     case "json": return <SiJson color="#5656e6" />;
     case "html": return <SiHtml5 color="#e04e2c" />;
     case "png": return <FcPicture />;
