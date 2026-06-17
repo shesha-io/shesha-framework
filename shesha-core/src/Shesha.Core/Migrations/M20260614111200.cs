ca4d8ff04e9e185b371291caec3ef6bf87e72aac
tshepo <tshepo1103@gmail.com>
2026-06-17 11:41:37 +0200

    Add migration to move DefaultPriority from NotificationChannelConfigs to NotificationTypeConfigs

--- shesha-core/src/Shesha.Core/Migrations/M20260614111200.cs
+++ shesha-core/src/Shesha.Core/Migrations/M20260614111200.cs
@@ -3,14 +3,14 @@ using Shesha.FluentMigrator;
 
 namespace Shesha.Migrations
 {
-    [Migration(20260614100000)]
-    public class M20260614100000 : OneWayMigration
+    [Migration(20260614111200)]
+    public class M20260614111200 : OneWayMigration
     {
         public override void Up()
         {
             // Move DefaultPriority from NotificationChannelConfigs to NotificationTypeConfigs
             Alter.Table("Core_NotificationTypeConfigs")
-                .AddColumn("DefaultPriorityLkp").AsInt64().Nullable();
+                .AddColumn("Core_DefaultPriorityLkp").AsInt64().Nullable();
 
             Delete.Column("DefaultPriorityLkp").FromTable("Core_NotificationChannelConfigs");
         }
