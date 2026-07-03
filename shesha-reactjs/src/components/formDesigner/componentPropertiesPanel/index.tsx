cc93e86eb1d6155e3acea2dda7453d8c1bdc2621
Nhlakanipho Masilela <nhlakanipho.masilela@boxfusion.io>
2026-07-03 09:20:52 +0200

    refactor: remove unused import of isNullOrWhiteSpace in ComponentPropertiesPanel

--- shesha-reactjs/src/components/formDesigner/componentPropertiesPanel/index.tsx
+++ shesha-reactjs/src/components/formDesigner/componentPropertiesPanel/index.tsx
@@ -1,7 +1,6 @@
 import React, { FC, useCallback } from 'react';
 import { Empty } from 'antd';
 import { useFormDesigner, useFormDesignerReadOnly, useFormDesignerSelectedComponentId } from '@/providers/formDesigner';
-import { isNullOrWhiteSpace } from '@/utils/nullables';
 
 const ComponentPropertiesPanelInner: FC = () => {
   const formDesigner = useFormDesigner();
