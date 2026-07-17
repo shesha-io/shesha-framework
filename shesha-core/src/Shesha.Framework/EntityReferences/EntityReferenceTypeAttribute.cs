50ae239600e6ff23362573765212512879541592
Alex Stephens <alex.stephens@boxfusion.io>
2026-07-17 16:31:58 +0300

    Index Table Advanced Filter: Reference List and Entity Reference filters don't bind for custom DTO models #5132

---
+++ shesha-core/src/Shesha.Framework/EntityReferences/EntityReferenceTypeAttribute.cs
@@ -0,0 +1,24 @@
+﻿using Shesha.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shesha.EntityReferences
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class EntityReferenceTypeAttribute: Attribute
+    {
+        public Type EntityType { get; set; }
+
+        public EntityReferenceTypeAttribute() { }
+
+        public EntityReferenceTypeAttribute(Type entityType)
+        {
+            if (!entityType.IsEntityType())
+                throw new ArgumentException($"{entityType.Name} is not an entity type", nameof(entityType));
+            EntityType = entityType;
+        }
+    }
+}
