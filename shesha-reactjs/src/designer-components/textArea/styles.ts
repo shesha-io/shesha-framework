001031227a6430aae3478cbda146020c77caa4d8
czwe-01 <tnkambule980115@gmail.com>
2026-07-23 15:11:37 +0200

    feat: add TextAreaApi type and enhance TextArea component with new settings and styles

--- shesha-reactjs/src/designer-components/textArea/styles.ts
+++ shesha-reactjs/src/designer-components/textArea/styles.ts
@@ -1,28 +1,21 @@
 import { createStyles } from '@/styles';
-import { CSSProperties } from 'react';
+import { ITextAreaComponentProps } from './interfaces';
+import { backgroundStyles, borderStyles, dimensionsStyles, fontStyles, paddingStyles, shadowStyles } from '../_common/styles/utils';
 
-type StylesArgs = {
-  fontWeight: string | undefined;
-  fontFamily: string | undefined;
-  textAlign: string | undefined;
-  color: string | undefined;
-  fontSize: CSSProperties['fontSize'] | undefined;
-};
+export const useStyles = createStyles(({ css, cx }, model: ITextAreaComponentProps) => {
+  const textArea = cx('sha-text-area', css`
+      ${borderStyles(model.border)}
+      ${backgroundStyles(model.background)}
+      ${shadowStyles(model.shadow)}
+      ${paddingStyles(model.stylingBoxJson)}
+      ${dimensionsStyles(model.dimensions)}
 
-type StylesResponse = {
-  textArea: string;
-};
-
-export const useStyles = createStyles<StylesArgs, StylesResponse>(({ css, cx }, { fontWeight, fontFamily, textAlign, color, fontSize }) => {
-  const textArea = cx("sha-text-area", css`
-        .ant-input {
-          ${fontWeight ? `font-weight: ${fontWeight};` : ''}
-          ${fontFamily ? `font-family: ${fontFamily};` : ''}
-          ${textAlign ? `text-align: ${textAlign};` : ''}
-          ${color ? `color: ${color};` : ''}
-          ${fontSize ? `font-size: ${fontSize};` : ''}
-        }
+      &.ant-input,
+      .ant-input {
+        ${fontStyles(model.font)}
+      }
   `);
+
   return {
     textArea,
   };
--- shesha-reactjs/src/designer-components/textArea/textArea.tsx
+++ shesha-reactjs/src/designer-components/textArea/textArea.tsx
@@ -1,8 +1,8 @@
 import { IInputStyles } from '@/providers/form/models';
 import { FontColorsOutlined } from '@ant-design/icons';
 import { Input } from 'antd';
-import { TextAreaProps } from 'antd/lib/input';
-import React, { CSSProperties, FocusEventHandler, ReactNode } from 'react';
+import { TextAreaProps, TextAreaRef } from 'antd/lib/input/TextArea';
+import React, { FocusEventHandler, ReactNode, useEffect, useRef } from 'react';
 import { validateConfigurableComponentSettings } from '@/providers/form/utils';
 import { DataTypes, StringFormats } from '@/interfaces/dataTypes';
 import { ITextAreaComponentProps, TextAreaComponentDefinition } from './interfaces';
@@ -12,17 +12,23 @@ import {
   migratePropertyName,
   migrateCustomFunctions,
   migrateReadOnly,
+  migrateHiddenToVisible,
 } from '@/designer-components/_common-migrations/migrateSettings';
 import { migrateVisibility } from '@/designer-components/_common-migrations/migrateVisibility';
+import { migratePermissionsToVisiblePermissions } from '../_common-migrations/migratePermissionsToVisiblePermissions';
 import { migrateFormApi } from '../_common-migrations/migrateFormApi1';
-import { toSizeCssProp } from '@/utils/form';
-import { removeUndefinedProps } from '@/utils/object';
 import { getSettings } from './settingsForm';
 import { migratePrevStyles } from '../_common-migrations/migrateStyles';
 import { defaultStyles } from './utils';
 import { useStyles } from './styles';
 import { getOverflowStyle } from '../_settings/utils/overflow/util';
+import { getComponentEvents } from '../_common/events';
+import { useComponentApi } from '@/providers/componentApi/provider';
+import { TextAreaApi } from '@/componentsApi/componentApi';
+import { useEffectOnce } from '@/hooks/useEffectOnce';
 import { isDefined } from '@/utils/nullables';
+
+import apiCode from "../../componentsApi/componentApi.ts?raw";
 
 interface IJsonTextAreaProps {
   value?: object;
@@ -47,6 +53,7 @@ const JsonTextArea: React.FC<IJsonTextAreaProps> = (props) => {
 type TextAreaValueType = string | object | undefined;
 
 const TextAreaComponent: TextAreaComponentDefinition = {
+  allowInherit: true,
   type: 'textArea',
   name: 'Text Area',
   isInput: true,
@@ -57,29 +64,20 @@ const TextAreaComponent: TextAreaComponentDefinition = {
   dataTypeSupported: ({ dataType, dataFormat }) =>
     dataType === DataTypes.string && dataFormat === StringFormats.multiline,
   Factory: ({ model }) => {
-    const { styles } = useStyles({
-      fontWeight: model.font?.weight,
-      fontFamily: model.font?.type,
-      textAlign: model.allStyles?.fullStyle.textAlign,
-      color: model.allStyles?.fullStyle.color,
-      fontSize: model.allStyles?.fullStyle.fontSize,
-    });
+    const componentApi = useComponentApi();
+    const inputRef = useRef<TextAreaRef>(null);
+    useEffect(() => {
+      componentApi?.updateApi<TextAreaApi>({
+        id: model.id,
+        componentName: model.componentName ?? "",
+        level: 3,
+        typeDefinition: { typeName: 'TextAreaApi', files: [{ content: apiCode, fileName: 'apis/componentApi.ts' }] },
+        api: { focus: () => inputRef.current?.focus() },
+      });
+    }, [componentApi, model.componentName, model.id]);
+    useEffectOnce(() => () => componentApi?.removeApi(model.id));
 
-    const additionalStyles: CSSProperties = removeUndefinedProps({
-      height: toSizeCssProp(model.height),
-      width: toSizeCssProp(model.width),
-      borderWidth: model.hideBorder ? 0 : model.borderSize,
-      borderRadius: model.borderRadius,
-      borderStyle: model.borderType,
-      borderColor: model.borderColor,
-      backgroundColor: model.backgroundColor,
-      color: model.fontColor,
-      fontWeight: model.fontWeight,
-      fontSize: model.fontSize,
-      ...model.allStyles?.appearanceStyle,
-    });
-
-    const finalStyle = removeUndefinedProps({ ...additionalStyles, ...model.allStyles?.jsStyle });
+    const { styles } = useStyles(model);
 
     const textAreaProps: TextAreaProps = {
       className: `sha-text-area ${styles.textArea}`,
@@ -89,11 +87,8 @@ const TextAreaComponent: TextAreaComponentDefinition = {
       maxLength: model.validate?.maxLength,
       size: model.size,
       style: {
-        ...finalStyle,
         ...getOverflowStyle(true, false),
-        ...((!finalStyle.marginBottom || finalStyle.marginBottom === '0px' || finalStyle.marginBottom === 0 || finalStyle.marginBottom === '0')
-          ? { marginBottom: model.showCount ? '4px' : '0px' }
-          : {}),
+        ...(isDefined(model.styleJson) ? model.styleJson : {}),
       },
       spellCheck: model.spellCheck ?? false,
     };
@@ -122,9 +117,9 @@ const TextAreaComponent: TextAreaComponentDefinition = {
                 color: maxLength && currentLength > maxLength ? '#ff4d4f' : '#8c8c8c',
                 marginTop: '0px',
                 marginBottom: '0px',
-                width: model.allStyles?.dimensionsStyles.width,
-                minWidth: model.allStyles?.dimensionsStyles.minWidth,
-                maxWidth: model.allStyles?.dimensionsStyles.maxWidth,
+                width: model.dimensions?.width,
+                minWidth: model.dimensions?.minWidth,
+                maxWidth: model.dimensions?.maxWidth,
               }}
               >
                 {currentLength}
@@ -132,14 +127,6 @@ const TextAreaComponent: TextAreaComponentDefinition = {
               </div>
             );
           };
-
-          const finalStyle = !model.enableStyleOnReadonly && model.readOnly
-            ? {
-              ...model.allStyles?.fontStyles,
-              ...model.allStyles?.dimensionsStyles,
-              ...getOverflowStyle(true, false),
-            }
-            : { ...model.allStyles?.fullStyle, ...getOverflowStyle(true, false) };
 
           return (
             <>
@@ -153,23 +140,26 @@ const TextAreaComponent: TextAreaComponentDefinition = {
                   />
                   {renderCharCounter()}
                 </>
-              ) : model.readOnly ? ( // no need to show counter in read only mode
-                <ReadOnlyDisplayFormItem value={value} style={{ padding: 8, ...finalStyle }} type="textArea" />
+              ) : model.readOnly === true ? ( // no need to show counter in read only mode
+                <ReadOnlyDisplayFormItem
+                  value={value}
+                  type="textArea"
+                  enableFullStyle={model.enableStyleOnReadonly}
+                  style={{ padding: 8, ...getOverflowStyle(true, false), ...model.styleJson }}
+                  styleValue={model}
+                />
               ) : (
                 <>
                   <Input.TextArea
+                    ref={inputRef}
                     rows={2}
                     {...textAreaProps}
-                    disabled={model.readOnly}
                     value={value ?? ""}
-
-                    // TODO EVENTS
                     onChange={(event) => {
                       ctx?.handleEvent(event, { value: event.currentTarget.value }, model.onChangeCustom);
                       onChange(event.currentTarget.value);
                     }}
-                    onFocus={(event) => ctx?.handleEvent(event, { value }, model.onFocusCustom)}
-                    onBlur={(event) => ctx?.handleEvent(event, { value }, model.onBlurCustom)}
+                    {...getComponentEvents<TextAreaValueType>(model, ['onFocus', 'onBlur', 'onClick', 'onMouseEnter', 'onKeyDown'], ctx, value, DataTypes.string)}
                   />
                   {renderCharCounter()}
                 </>
@@ -191,13 +181,18 @@ const TextAreaComponent: TextAreaComponentDefinition = {
 
     return textAreaModel;
   },
+  settingsFormMarkup: getSettings,
+  validateSettings: (model) => validateConfigurableComponentSettings(getSettings, model),
+  getDefaultStyles: () => defaultStyles(),
   migrator: (m) =>
     m
       .add<ITextAreaComponentProps>(0, (prev) => migratePropertyName(migrateCustomFunctions(prev)))
       .add<ITextAreaComponentProps>(1, (prev) => migrateVisibility(prev))
       .add<ITextAreaComponentProps>(2, (prev) => migrateReadOnly(prev))
       .add<ITextAreaComponentProps>(3, (prev) => ({ ...migrateFormApi.eventsAndProperties(prev) }))
-      .add<ITextAreaComponentProps>(4, (prev) => {
+      .add<ITextAreaComponentProps>(4, (prev, context) => {
+        if (context.isNew === true) return prev;
+
         const styles: IInputStyles = {
           size: prev.size,
           width: prev.width,
@@ -214,14 +209,16 @@ const TextAreaComponent: TextAreaComponentDefinition = {
         };
         return { ...prev, desktop: { ...styles }, tablet: { ...styles }, mobile: { ...styles } };
       })
-      .add<ITextAreaComponentProps>(5, (prev) => ({ ...migratePrevStyles(prev, defaultStyles()) })),
+      .add<ITextAreaComponentProps>(5, (prev, context) => context.isNew === true
+        ? prev
+        : { ...migratePrevStyles(prev, defaultStyles()) })
+      .add<ITextAreaComponentProps>(6, (prev) => migrateHiddenToVisible(prev))
+      .add<ITextAreaComponentProps>(7, (prev) => migratePermissionsToVisiblePermissions(prev)),
   linkToModelMetadata: (model, _): ITextAreaComponentProps => {
     return {
       ...model,
     };
   },
-  settingsFormMarkup: getSettings,
-  validateSettings: (model) => validateConfigurableComponentSettings(getSettings, model),
 };
 
 export default TextAreaComponent;
