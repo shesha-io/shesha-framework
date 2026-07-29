9df4760812610efb5beb3b743fd7d94748885949
czwe-01 <tnkambule980115@gmail.com>
2026-07-29 14:44:53 +0200

    refactor: remove unused migrateStylesToNestedSet export
    The `migrateStylesToNestedSet` export has been removed from the index file as part of ongoing codebase simplification efforts. This change helps streamline the migration logic by eliminating unnecessary exports.

--- shesha-reactjs/src/designer-components/_common-migrations/index.ts
+++ shesha-reactjs/src/designer-components/_common-migrations/index.ts
@@ -8,7 +8,6 @@ export * from './migrateFormLifecycle';
 export * from './migrateGqlCustomEndpoint';
 export * from './migrateSettings';
 export * from './migrateStyles';
-export * from './migrateStylesToNestedSet';
 export * from './migrateUrlDataSource';
 export * from './migrateUseExpression';
 export * from './migrateVisibility';
