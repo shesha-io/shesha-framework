b0c9553c878f3749983348d34caf2c834af0bf88
Alex Stephens <alex.stephens@boxfusion.io>
2026-08-24 15:17:04 +0300

    QuckSearch component refactoring

--- shesha-reactjs/src/components/globalTableFilterBase/index.tsx
+++ shesha-reactjs/src/components/globalTableFilterBase/index.tsx
@@ -11,7 +11,6 @@ export interface IGlobalTableFilterBaseProps {
   performQuickSearch?: ((val: string) => void) | undefined;
   quickSearch: string | undefined;
   size?: SizeType | undefined;
-  block?: boolean | undefined;
   style?: React.CSSProperties | undefined;
 }
 
@@ -20,10 +19,9 @@ export const GlobalTableFilterBase: FC<IGlobalTableFilterBaseProps> = ({
   changeQuickSearch,
   performQuickSearch,
   quickSearch,
-  block = false,
   style,
 }) => {
-  const { styles } = useStyles({ block });
+  const { styles } = useStyles();
   const srcProps: SearchProps = {
     allowClear: true,
     ...searchProps,
@@ -32,10 +30,7 @@ export const GlobalTableFilterBase: FC<IGlobalTableFilterBaseProps> = ({
 
   const onSearch = (
     value: string,
-    event?:
-      | React.MouseEvent<HTMLElement, MouseEvent> |
-      React.ChangeEvent<HTMLInputElement> |
-      React.KeyboardEvent<HTMLInputElement>,
+    event?: | React.MouseEvent<HTMLElement, MouseEvent> | React.ChangeEvent<HTMLInputElement> | React.KeyboardEvent<HTMLInputElement>,
   ): void => {
     event?.stopPropagation();
     event?.preventDefault();
--- shesha-reactjs/src/designer-components/dataTable/quickSearch/interfaces.ts
+++ shesha-reactjs/src/designer-components/dataTable/quickSearch/interfaces.ts
@@ -1,10 +1,6 @@
 import { IConfigurableFormComponent } from '@/providers/form/models';
 import { ComponentDefinition } from '@/interfaces';
-import { IDimensionsValue } from '@/designer-components/_settings/utils/dimensions/interfaces';
 
-export interface IQuickSearchComponentProps extends IConfigurableFormComponent {
-  block?: boolean | undefined;
-  dimensions?: IDimensionsValue | undefined;
-}
+export type IQuickSearchComponentProps = IConfigurableFormComponent;
 
-export type QuickSearchComponentDefinition = ComponentDefinition<"datatable.quickSearch", IQuickSearchComponentProps>;
+export type QuickSearchComponentDefinition = ComponentDefinition<"datatable.quickSearch", IConfigurableFormComponent>;
