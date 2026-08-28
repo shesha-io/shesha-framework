530276bef82a3ad1578ac276a2093f0b9b051cc5
czwe-01 <tnkambule980115@gmail.com>
2026-08-28 12:40:08 +0200

    Enhance Notes Component Styles and Layout
    - Updated the styles in the Notes component to improve text rendering and layout consistency.
    - Introduced a new font style handling mechanism to differentiate between aligned and non-aligned text elements.
    - Adjusted padding values for better spacing in the notes input fields and empty state description.
    - Refactored CSS rules to ensure proper inheritance and application of styles across various note elements.
    
    These changes enhance the visual appearance and usability of the notes component, ensuring a more cohesive user experience.

--- shesha-reactjs/src/designer-components/notes/utils.ts
+++ shesha-reactjs/src/designer-components/notes/utils.ts
@@ -33,8 +33,8 @@ export const defaultStyles = (): IStyleValue => ({
     marginBottom: '0',
     marginLeft: '0',
     paddingTop: '0',
-    paddingRight: '0',
+    paddingRight: '8',
     paddingBottom: '0',
-    paddingLeft: '0',
+    paddingLeft: '8',
   },
 });
