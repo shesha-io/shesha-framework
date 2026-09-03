e79136d0c8cf9735dbeab9f3b4ab9ae586f0b4e7
Alex Stephens <alex.stephens@boxfusion.io>
2026-09-03 12:12:50 +0300

    StyleBox rework

--- shesha-reactjs/src/designer-components/styleBox/components/box.tsx
+++ shesha-reactjs/src/designer-components/styleBox/components/box.tsx
@@ -6,14 +6,13 @@ import { StyleBoxValue } from '../../../providers/form/models';
 import { getStyleBoxValue } from '../utils';
 
 interface IProps {
-  className?: string | undefined;
   readOnly: boolean;
   value: StyleBoxValue | undefined;
   propertyName: string;
   onChange?: ((newValue: StyleBoxValue | undefined) => void) | undefined;
 }
 
-const Box: FC<IProps> = ({ className, onChange, readOnly, value, propertyName }) => {
+const Box: FC<IProps> = ({ onChange, readOnly, value, propertyName }) => {
   const { styles } = useStyles();
 
   // need to store the value locally because internal components may not be rendered and will use the old value
@@ -34,25 +33,39 @@ const Box: FC<IProps> = ({ className, onChange, readOnly, value, propertyName })
   const commonProps = { onChange: onChangeInternal, readOnly, value, propertyName };
 
   return (
-    <div className={classNames(styles.shaStyleBox, className)}>
-      <div className={styles.margin}>
-        <span className={styles.title}>Margin</span>
-
+    <div className={styles.shaStyleBox}>
+      <div className={classNames('sha-style-box-margin', 'sha-style-box-mtb')}>
+        <span className="sha-style-box-text">Margin</span>
         <BoxInput {...commonProps} type="margin" direction="top" />
-        <BoxInput {...commonProps} type="margin" direction="left" />
-        <BoxInput {...commonProps} type="margin" direction="bottom" />
-        <BoxInput {...commonProps} type="margin" direction="right" />
-
-        <div className={styles.padding}>
-          <span className={styles.title}>Padding</span>
-
-          <BoxInput {...commonProps} type="padding" direction="top" />
-          <BoxInput {...commonProps} type="padding" direction="left" />
-          <BoxInput {...commonProps} type="padding" direction="bottom" />
-          <BoxInput {...commonProps} type="padding" direction="right" />
-
-          <div className="center" />
+      </div>
+      <div className="sha-style-box-center">
+        <div className={classNames('sha-style-box-margin', 'sha-style-box-mlr')}>
+          <BoxInput {...commonProps} type="margin" direction="left" />
         </div>
+        <div className="sha-style-box-padding-container">
+          <div className={classNames('sha-style-box-padding', 'sha-style-box-ptb')}>
+            <span className="sha-style-box-text">Padding</span>
+            <BoxInput {...commonProps} type="padding" direction="top" />
+          </div>
+          <div className="sha-style-box-padding-center">
+            <div className={classNames('sha-style-box-padding', 'sha-style-box-plr')}>
+              <BoxInput {...commonProps} type="padding" direction="left" />
+            </div>
+            <div className={classNames('sha-style-box-margin', 'sha-style-box-inner')} />
+            <div className={classNames('sha-style-box-padding', 'sha-style-box-plr')}>
+              <BoxInput {...commonProps} type="padding" direction="right" />
+            </div>
+          </div>
+          <div className={classNames('sha-style-box-padding', 'sha-style-box-ptb')}>
+            <BoxInput {...commonProps} type="padding" direction="bottom" />
+          </div>
+        </div>
+        <div className={classNames('sha-style-box-margin', 'sha-style-box-mlr')}>
+          <BoxInput {...commonProps} type="margin" direction="right" />
+        </div>
+      </div>
+      <div className={classNames('sha-style-box-margin', 'sha-style-box-mtb')}>
+        <BoxInput {...commonProps} type="margin" direction="bottom" />
       </div>
     </div>
   );
