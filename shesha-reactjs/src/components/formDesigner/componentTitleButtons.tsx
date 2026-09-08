d45db23e950cccdb8958fd42da8120b8ae72474c
IvanIlyichev <ivan.mcarthy@boxfusion.io>
2026-09-08 10:12:09 +0300

    feat: added isDebugMode flag to application for features under development

--- shesha-reactjs/src/components/formDesigner/componentTitleButtons.tsx
+++ shesha-reactjs/src/components/formDesigner/componentTitleButtons.tsx
@@ -1,25 +1,43 @@
 import { FC } from 'react';
 import { Button } from 'antd';
-import { CopyOutlined, DeleteFilled } from '@ant-design/icons';
+import { BugOutlined, CopyOutlined, DeleteFilled } from '@ant-design/icons';
 import { useFormDesigner, useFormDesignerReadOnly, useFormDesignerSelectedComponent } from '@/providers/formDesigner';
+import { isDefined } from '@/utils';
+import { useSheshaApplication } from '@/providers';
 
 export const ComponentTitleButtons: FC = ({}) => {
+  const { isDebugMode } = useSheshaApplication();
   const component = useFormDesignerSelectedComponent();
   const readOnly = useFormDesignerReadOnly();
-  const { deleteComponent, duplicateComponent } = useFormDesigner();
+  const { deleteComponent, duplicateComponent, validateComponentAsync } = useFormDesigner();
 
   const onDeleteClick = (): void => {
     if (!readOnly && component)
       deleteComponent({ componentId: component.id });
   };
+
   const onDuplicateClick = (): void => {
     if (!readOnly && component)
       duplicateComponent({ componentId: component.id });
   };
 
+  const onDebugClick = (): void => {
+    if (isDefined(component))
+      void validateComponentAsync(component);
+  };
+
   return component && !readOnly
     ? (
       <>
+        {isDebugMode && (
+          <Button
+            type="text"
+            icon={<BugOutlined />}
+            onClick={onDebugClick}
+            size="small"
+            title="Debug"
+          />
+        )}
         <Button
           type="text"
           icon={<CopyOutlined />}
