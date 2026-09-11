3163e8acd6df5970ec1264df6c3899b8b264d26e
Nhlakanipho Masilela <nhlakanipho.masilela@boxfusion.io>
2026-09-11 07:12:34 +0200

    Add Custom Layout Component and related files

---
+++ shesha-reactjs/src/designer-components/customLayout/customLayoutComponent.tsx
@@ -0,0 +1,45 @@
+import { GroupOutlined } from '@ant-design/icons';
+import ComponentsContainer from '@/components/formDesigner/containers/componentsContainer';
+import ParentProvider from '@/providers/parentProvider/index';
+import { getFullSizeWrapperDesignerStyle } from '@/components/formDesigner/utils/stylingUtils';
+import { useActualContextExecutionNoRefresh } from '@/hooks/formComponentHooks';
+import { getSettings } from './settingsForm';
+import { defaultStyles } from './utils';
+import { useStyles } from './styles';
+import { CustomLayoutComponentDefinition } from './interfaces';
+
+const CustomLayoutComponent: CustomLayoutComponentDefinition = {
+  showInThemeEditor: false,
+  styleGroup: 'common-containers',
+  allowInherit: true,
+  type: 'customLayout',
+  isInput: false,
+  name: 'Custom Layout',
+  icon: <GroupOutlined />,
+  emptyComponents: [],
+  getWrapperStyle: (model) => getFullSizeWrapperDesignerStyle(model),
+  Factory: ({ model }) => {
+    const { styles, cx } = useStyles(model);
+    const wrappedStyleJson = useActualContextExecutionNoRefresh(model.wrapperStyle, undefined, {});
+
+    if (model.hidden === true) return null;
+
+    return (
+      <ParentProvider model={model} name={`CustomLayoutComponent-${model.id}`}>
+        {model.hideLabel !== true && <div className={styles.heading}>{model.label}</div>}
+        <ComponentsContainer
+          containerId={model.id}
+          wrapperStyle={wrappedStyleJson}
+          style={model.styleCss}
+          className={cx(model.className, styles.customLayout)}
+          dynamicComponents={model.isDynamic === true ? model.components : CustomLayoutComponent.emptyComponents}
+        />
+      </ParentProvider>
+    );
+  },
+  initModel: (model) => ({ ...model, label: 'Custom Layout', hideLabel: false, labelAlign: 'left' }),
+  settingsFormMarkup: getSettings,
+  getDefaultStyles: defaultStyles,
+};
+
+export default CustomLayoutComponent;
